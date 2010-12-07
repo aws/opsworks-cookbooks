d6f622595335abe0a91eefb937e8f221da95ff8a
Mathias Meyer <meyer@paperplanes.de>
2010-12-07 12:41:27 +0100

    Bundle into specific directory for the current app.

--- deploy/definitions/scalarium_deploy.rb
+++ deploy/definitions/scalarium_deploy.rb
@@ -55,9 +55,9 @@ define :scalarium_deploy do
     end
 
     before_migrate do
-      if File.exists?("#{release_path}/Gemfile")
+      if deploy[:application_type] == 'rails' and File.exists?("#{release_path}/Gemfile")
         Chef::Log.info("Gemfile detected. Running bundle install.")
-        sudo("cd #{release_path} && bundle install --system --without=test")
+        sudo("cd #{release_path} && bundle install #{node[:etc][:passwd][deploy[:user]][:dir]}/.bundler/#{application} --without=test")
       end
       run_callback_from_file("#{release_path}/deploy/before_migrate.rb")
     end
