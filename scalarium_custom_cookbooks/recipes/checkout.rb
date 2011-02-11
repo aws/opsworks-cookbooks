1b3aab353473c95eefc5da039107eed79fc14c7d
Jonathan Weiss <jw@innerewut.de>
2011-02-11 17:07:21 +0100

    remove usage of @node

--- scalarium_custom_cookbooks/recipes/checkout.rb
+++ scalarium_custom_cookbooks/recipes/checkout.rb
@@ -1,38 +1,38 @@
-ensure_scm_package_installed(@node[:scalarium_custom_cookbooks][:scm][:type])
+ensure_scm_package_installed(node[:scalarium_custom_cookbooks][:scm][:type])
 
-prepare_git_checkouts(:user => @node[:scalarium_custom_cookbooks][:user], 
-                      :group => @node[:scalarium_custom_cookbooks][:group], 
-                      :home => @node[:scalarium_custom_cookbooks][:home], 
-                      :ssh_key => @node[:scalarium_custom_cookbooks][:scm][:ssh_key]) if @node[:scalarium_custom_cookbooks][:scm][:type].to_s == 'git'
+prepare_git_checkouts(:user => node[:scalarium_custom_cookbooks][:user],
+                      :group => node[:scalarium_custom_cookbooks][:group],
+                      :home => node[:scalarium_custom_cookbooks][:home],
+                      :ssh_key => node[:scalarium_custom_cookbooks][:scm][:ssh_key]) if node[:scalarium_custom_cookbooks][:scm][:type].to_s == 'git'
                       
-prepare_svn_checkouts(:user => @node[:scalarium_custom_cookbooks][:user], 
-                      :group => @node[:scalarium_custom_cookbooks][:group], 
-                      :home => @node[:scalarium_custom_cookbooks][:home]) if @node[:scalarium_custom_cookbooks][:scm][:type].to_s == 'svn'
+prepare_svn_checkouts(:user => node[:scalarium_custom_cookbooks][:user],
+                      :group => node[:scalarium_custom_cookbooks][:group],
+                      :home => node[:scalarium_custom_cookbooks][:home]) if node[:scalarium_custom_cookbooks][:scm][:type].to_s == 'svn'
 
 scm "Download Custom Cookbooks" do
-  user @node[:scalarium_custom_cookbooks][:user]
-  group @node[:scalarium_custom_cookbooks][:group]
+  user node[:scalarium_custom_cookbooks][:user]
+  group node[:scalarium_custom_cookbooks][:group]
   
-  case @node[:scalarium_custom_cookbooks][:scm][:type]
+  case node[:scalarium_custom_cookbooks][:scm][:type]
   when 'git'
     provider Chef::Provider::Git
-    enable_submodules @node[:scalarium_custom_cookbooks][:enable_submodules]
+    enable_submodules node[:scalarium_custom_cookbooks][:enable_submodules]
     depth nil
   when 'svn'
     provider Chef::Provider::Subversion
-    svn_username @node[:scalarium_custom_cookbooks][:scm][:user]
-    svn_password @node[:scalarium_custom_cookbooks][:scm][:password]
+    svn_username node[:scalarium_custom_cookbooks][:scm][:user]
+    svn_password node[:scalarium_custom_cookbooks][:scm][:password]
   else
-    raise "unsupported SCM type #{@node[:scalarium_custom_cookbooks][:scm][:type].inspect}"
+    raise "unsupported SCM type #{node[:scalarium_custom_cookbooks][:scm][:type].inspect}"
   end
   
   action :checkout
-  destination @node[:scalarium_custom_cookbooks][:destination]
-  repository @node[:scalarium_custom_cookbooks][:scm][:repository]
-  revision @node[:scalarium_custom_cookbooks][:scm][:revision]
-  user @node[:scalarium_custom_cookbooks][:user]
+  destination node[:scalarium_custom_cookbooks][:destination]
+  repository node[:scalarium_custom_cookbooks][:scm][:repository]
+  revision node[:scalarium_custom_cookbooks][:scm][:revision]
+  user node[:scalarium_custom_cookbooks][:user]
   
   not_if do
-    @node[:scalarium_custom_cookbooks][:scm][:repository].blank? || ::File.directory?(@node[:scalarium_custom_cookbooks][:destination])
+    node[:scalarium_custom_cookbooks][:scm][:repository].blank? || ::File.directory?(node[:scalarium_custom_cookbooks][:destination])
   end
 end
--- scalarium_custom_cookbooks/recipes/default.rb
+++ scalarium_custom_cookbooks/recipes/default.rb
@@ -4,15 +4,15 @@ include_recipe "scalarium_custom_cookbooks::checkout"
 
 ruby_block "load and execute the new cookbooks" do
   block do
-    if ::File.directory?(@node[:scalarium_custom_cookbooks][:destination])
-      Chef::Log.info("Loading custom cookbooks from #{@node[:scalarium_custom_cookbooks][:destination]}")
+    if ::File.directory?(node[:scalarium_custom_cookbooks][:destination])
+      Chef::Log.info("Loading custom cookbooks from #{node[:scalarium_custom_cookbooks][:destination]}")
       
       # add new Cookbooks and keep old
-      Chef::Config.cookbook_path = [Chef::Config.cookbook_path, @node[:scalarium_custom_cookbooks][:destination]].flatten
+      Chef::Config.cookbook_path = [Chef::Config.cookbook_path, node[:scalarium_custom_cookbooks][:destination]].flatten
       
       load_new_cookbooks
 
-      @node[:scalarium_custom_cookbooks][:recipes].each do |r|
+      node[:scalarium_custom_cookbooks][:recipes].each do |r|
         begin
           Chef::Log.info("Executing custom recipe: #{r}")
           include_recipe r
@@ -25,7 +25,7 @@ ruby_block "load and execute the new cookbooks" do
   end
   
   only_if do
-    Chef::Log.info("Loading and executing custom cookbooks") if @node[:scalarium_custom_cookbooks][:enabled]
-    @node[:scalarium_custom_cookbooks][:enabled]
+    Chef::Log.info("Loading and executing custom cookbooks") if node[:scalarium_custom_cookbooks][:enabled]
+    node[:scalarium_custom_cookbooks][:enabled]
   end
 end
