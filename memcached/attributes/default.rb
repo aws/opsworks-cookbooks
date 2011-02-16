f442a1794a2485f5e35a88b3c37299a146756bf7
Jonathan Weiss <jw@innerewut.de>
2011-02-16 17:40:09 +0100

    make memcached max connections configurable

--- memcached/attributes/default.rb
+++ memcached/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:memcached] = {}
 default[:memcached][:memory] = 512
 default[:memcached][:port] = 11211
 default[:memcached][:user] = "nobody"
+default[:memcached][:max_connections] = "4096"
