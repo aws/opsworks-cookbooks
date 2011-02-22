f79983a95f3aa18e9a34d830144844b376bdb9f7
Daniel Hüsch <daniel.huesch@gmail.com>
2011-02-22 15:57:55 +0100

    Only install bundler when explicitly asked to.

--- scalarium_bundler/recipes/default.rb
+++ scalarium_bundler/recipes/default.rb
@@ -1,7 +1,9 @@
-gem_package "Installing Bundler #{node[:scalarium_bundler][:version]}" do
-  gem_binary node[:dependencies][:gem_binary]
-  retries 2
-  package_name "bundler"
-  action :install
-  version node[:scalarium_bundler][:version]
+if node[:scalarium_bundler][:manage_package]
+  gem_package "Installing Bundler #{node[:scalarium_bundler][:version]}" do
+    gem_binary node[:dependencies][:gem_binary]
+    retries 2
+    package_name "bundler"
+    action :install
+    version node[:scalarium_bundler][:version]
+  end
 end
