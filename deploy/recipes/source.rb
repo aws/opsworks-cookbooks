074772bcac81b887dd4d85e4e37dd82a0530c44d
Jonathan Weiss <jw@innerewut.de>
2011-03-15 13:01:59 +0100

    add no longer used recipes that only print a deprication warning

---
+++ deploy/recipes/source.rb
@@ -0,0 +1,2 @@
+Chef::Log.warn("You are including a recipe that is no longer used and depricated: deploy::source")
+Chef::Log.warn("Please use the definition `scalarium_deploy` instead. See http://support.scalarium.com/kb/deployment/how-to-do-a-custom-deployment")
