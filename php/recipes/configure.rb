34574f521b6a27183f1fb5ea9530e75133926671
Jonathan Weiss <jw@innerewut.de>
2011-03-21 10:34:29 +0100

    add lost PHP cookbook

---
+++ php/recipes/configure.rb
@@ -0,0 +1,16 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+
+  # write out scalarium.php
+  template "#{deploy[:deploy_to]}/shared/config/scalarium.php" do
+    cookbook 'php'
+    source 'scalarium.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:database => deploy[:database], :memcached => deploy[:memcached], :roles => node[:scalarium][:roles])
+  end
+end
