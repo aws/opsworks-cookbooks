ce83f0b6149b1a27021f52937e323cde01389b25
Jonathan Weiss <jw@innerewut.de>
2011-03-22 12:57:45 +0100

    fix deploy::user for older recipes

--- deploy/recipes/user.rb
+++ deploy/recipes/user.rb
@@ -1,2 +1,4 @@
 Chef::Log.warn("You are including a recipe that is no longer used and depricated: deploy::user")
 Chef::Log.warn("Please use the definition `scalarium_deploy_user` instead. See http://support.scalarium.com/kb/deployment/how-to-do-a-custom-deployment")
+
+include_recipe "deploy::default"
