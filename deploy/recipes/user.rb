b3a6ec6befc41203abce0db31fe6ebfe951956d2
Jonathan Weiss <jw@innerewut.de>
2011-03-22 13:12:19 +0100

    explicitly create user

--- deploy/recipes/user.rb
+++ deploy/recipes/user.rb
@@ -1,4 +1,10 @@
 Chef::Log.warn("You are including a recipe that is no longer used and depricated: deploy::user")
 Chef::Log.warn("Please use the definition `scalarium_deploy_user` instead. See http://support.scalarium.com/kb/deployment/how-to-do-a-custom-deployment")
 
-include_recipe "deploy::default"
+node[:deploy].each do |application, deploy|
+
+  scalarium_deploy_user do
+    deploy_data deploy
+  end
+
+end
