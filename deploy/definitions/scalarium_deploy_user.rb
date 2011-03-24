d9186868b46af8d8312a09d724bf13164bbf14a1
Jonathan Weiss <jw@innerewut.de>
2011-03-24 19:14:07 +0100

    get rid of user modify hack and set defaults for deploy user shell

--- deploy/definitions/scalarium_deploy_user.rb
+++ deploy/definitions/scalarium_deploy_user.rb
@@ -9,19 +9,6 @@ define :scalarium_deploy_user do
     gid deploy[:group]
     home deploy[:home]
     supports :manage_home => true
-    shell "/bin/zsh"
-    
-    not_if do
-      # do not modify existing deploy user!
-      require 'etc'
-      deploy_user_exists = false
-      Etc.passwd do |user|
-        if user.name == deploy[:user]
-          Chef::Log.info("The deploy user #{deploy[:user]} already exists - skipping create")
-          deploy_user_exists = true
-        end
-      end
-      deploy_user_exists
-    end
+    shell deploy[:shell]
   end
 end
