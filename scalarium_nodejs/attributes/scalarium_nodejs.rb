6289c3bdf21ed4e566ec2e0c4d614a740dea9228
Daniel Hüsch <daniel.huesch@gmail.com>
2011-05-17 18:45:26 +0200

    Add recipes to install node.js and npm.

---
+++ scalarium_nodejs/attributes/scalarium_nodejs.rb
@@ -0,0 +1,7 @@
+default[:scalarium_nodejs] = {}
+default[:scalarium_nodejs][:version] = '0.4.7'
+default[:scalarium_nodejs][:npm_version] = '1.0.6'
+default[:scalarium_nodejs][:pkgrelease] = '1'
+arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
+default[:scalarium_nodejs][:deb] = "nodejs_#{node[:scalarium_nodejs][:version]}-#{node[:scalarium_nodejs][:pkgrelease]}_#{arch}.deb"
+default[:scalarium_nodejs][:deb_url] = "http://peritor-assets.s3.amazonaws.com/#{node[:platform]}/#{node[:platform_version]}/#{node[:scalarium_nodejs][:deb]}"
---
+++ scalarium_nodejs/recipes/npm.rb
@@ -0,0 +1,16 @@
+remote_file "/tmp/npm-#{node[:scalarium_nodejs][:npm_version]}.tgz" do
+  source "http://registry.npmjs.org/npm/-/npm-#{node[:scalarium_nodejs][:npm_version]}.tgz"
+  action :create_if_missing
+end
+
+directory "/tmp/npm-#{node[:scalarium_nodejs][:npm_version]}"
+execute "tar xvfz /tmp/npm-#{node[:scalarium_nodejs][:npm_version]}.tgz -C /tmp/npm-#{node[:scalarium_nodejs][:npm_version]} --strip-components=1"
+
+execute "Install npm #{node[:scalarium_nodejs][:npm_version]}" do
+  cwd "/tmp/npm-#{node[:scalarium_nodejs][:npm_version]}"
+  command 'make install'
+  not_if do
+    ::File.exists?("/usr/local/bin/npm") &&
+    system("/usr/local/bin/npm -v | grep -q '#{node[:scalarium_nodejs][:npm_version]}'")
+  end
+end
