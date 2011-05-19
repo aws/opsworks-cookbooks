67ac43fa401153be90acf4b6fe5085a1d18044d2
Daniel Hüsch <daniel.huesch@gmail.com>
2011-05-19 06:50:40 +0200

    Install global npm dependencies locally into every app root.

--- dependencies/attributes/default.rb
+++ dependencies/attributes/default.rb
@@ -1,5 +1,6 @@
 default[:dependencies][:gems] = {}
 default[:dependencies][:debs] = {}
+default[:dependencies][:npms] = {}
 default[:dependencies][:update_debs] = false
 default[:dependencies][:upgrade_debs] = false
 default[:dependencies][:upgrade_gems] = false
--- deploy/definitions/scalarium_nodejs.rb
+++ deploy/definitions/scalarium_nodejs.rb
@@ -6,6 +6,21 @@ define :scalarium_nodejs do
     action :nothing
   end
 
+  node[:dependencies][:npms].each do |npm, version|
+    execute "/usr/local/bin/npm install #{npm}" do
+      cwd "#{deploy[:deploy_to]}/current"
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/scalarium.js" do
+    cookbook 'scalarium_nodejs'
+    source 'scalarium.js.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:database => deploy[:database], :memcached => deploy[:memcached], :roles => node[:scalarium][:roles])
+  end
+
   template "/etc/monit/conf.d/node_web_app-#{application}.monitrc" do
     source 'node_web_app.monitrc.erb'
     cookbook 'scalarium_nodejs'
