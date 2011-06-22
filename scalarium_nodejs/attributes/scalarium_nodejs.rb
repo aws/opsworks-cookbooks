33ea153be5ded4f3a215a227b3eca1901b937388
Jonathan Weiss <jw@innerewut.de>
2011-06-22 17:52:29 +0200

    update NPM to 1.0.13

--- scalarium_nodejs/attributes/scalarium_nodejs.rb
+++ scalarium_nodejs/attributes/scalarium_nodejs.rb
@@ -1,6 +1,6 @@
 default[:scalarium_nodejs] = {}
 default[:scalarium_nodejs][:version] = '0.4.7'
-default[:scalarium_nodejs][:npm_version] = '1.0.6'
+default[:scalarium_nodejs][:npm_version] = '1.0.13'
 default[:scalarium_nodejs][:pkgrelease] = '1'
 arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
 default[:scalarium_nodejs][:deb] = "nodejs_#{node[:scalarium_nodejs][:version]}-#{node[:scalarium_nodejs][:pkgrelease]}_#{arch}.deb"
