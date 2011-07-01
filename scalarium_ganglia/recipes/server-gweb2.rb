d92a14d707c943f7c79d083d05fa5021f2548cfb
Thomas Metschke <thomas.metschke@gmail.com>
2011-07-01 12:25:48 +0200

    add reports to new UI

--- scalarium_ganglia/recipes/server-gweb2.rb
+++ scalarium_ganglia/recipes/server-gweb2.rb
@@ -11,3 +11,43 @@ execute "Execute make install" do
   command "cd /tmp/ganglia-misc/ganglia-web/ && make install"
 end
 
+remote_file "/var/www/ganglia2/graph.d/mysql_query_report.php" do
+  source "mysql_query_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/apache_report.php" do
+  source "apache_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/apache_worker_report.php" do
+  source "apache_worker_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/passenger_memory_stats_report.php" do
+  source "passenger_memory_stats_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/passenger_status_report.php" do
+  source "passenger_status_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/haproxy_requests_report.php" do
+  source "haproxy_requests_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/nginx_status_report.php" do
+  source "nginx_status_report.php"
+  mode "0644"
+end
+
+remote_file "/var/www/ganglia2/graph.d/apache_response_time_report.php" do
+  source "apache_response_time_report.php"
+  mode "0644"
+end
+
