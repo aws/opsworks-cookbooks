e7d9c5e4197a5e4324bfce5c52fb8c275aed1b24
Jonathan Weiss <jw@innerewut.de>
2011-08-22 16:52:33 +0200

    allow all MySQL hosts

--- deploy/recipes/mysql.rb
+++ deploy/recipes/mysql.rb
@@ -11,26 +11,13 @@ node[:deploy].each do |application, deploy|
       system("#{mysql_command} -e 'SHOW DATABASES' | egrep -e '^#{deploy[:database][:database]}$'")
     end
   end
- 
-  ruby_block "get hosts list" do
-    block do 
-      if Chef::VERSION > "0.9"
-        template = run_context.resource_collection.find(:template => "/tmp/grants.sql")
-      else
-        # this is a bug, and should just be 'resources'
-        template = @collection.resources(:template => "/tmp/grants.sql")
-      end
-      status, stdout, stderr = Chef::Mixin::Command.output_of_command("echo 'select host from db where db=\"#{deploy[:database][:database]}\" and user =\"root\"' | #{mysql_command} --skip-column-names mysql", {})
-      template.variables[:hosts] = stdout.split("\n").delete_if{|host| host == '127.0.0.1' || host == 'localhost'}
-    end
-  end
- 
+
   template "/tmp/grants.sql" do
     source 'grants.sql.erb'
     owner 'root'
     group 'root'
     mode '0600'
-    variables :hosts => [], :settings => deploy[:database], :cluster_clients => node[:mysql][:clients].select{|private_ip| Resolv.getaddress(private_ip) }
+    variables :hosts => [], :settings => deploy[:database]
     cookbook "mysql"
     action :create
   end
