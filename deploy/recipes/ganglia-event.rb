520f72fe7a3f394232c395522eb87c308903b6d8
Thomas Metschke <thomas.metschke@gmail.com>
2011-10-18 16:05:05 +0200

    move events.json.d folder creation to setup of gweb2

--- deploy/recipes/ganglia-event.rb
+++ deploy/recipes/ganglia-event.rb
@@ -1,13 +1,6 @@
 events_dir = node[:ganglia][:datadir] + '/conf/events.json.d/'
 event = events_dir + node[:scalarium][:sent_at].to_s + '_event.json'
 
-directory events_dir do
-  mode '0755'
-  action :create
-  recursive true
-  owner 'www-data'
-end
-
 template event do
   source 'event.json.erb'
   mode '0644'
@@ -17,7 +10,7 @@ end
 
 ruby_block 'Create new events.json file for Ganglia' do
   block do
-    logs = Dir.glob(events_dir + '/*.json').sort
+    logs = Dir.glob(events_dir + '*.json').sort
     logs.each do |log|
       Chef::Log.info("processing #{log}")
     end
