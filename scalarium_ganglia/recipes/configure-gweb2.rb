3bbd37bc67f7076e926776fc1ee6d6fee1f8363c
Thomas Metschke <thomas.metschke@gmail.com>
2011-10-19 13:04:15 +0200

    jetzt aber

--- scalarium_ganglia/recipes/configure-gweb2.rb
+++ scalarium_ganglia/recipes/configure-gweb2.rb
@@ -1,16 +1,16 @@
-instances = []
+instances = {}
 
 node[:scalarium][:roles].each do |role_name, role_config|
   role_config[:instances].each do |instance_name, instance_config|
-    unless instances.include?(instance_name)
-      instances << instance_name
-    end
+    instances[instance_name] ||= []
+    instances[instance_name] << role_name
   end
 end
 
-instances.each do |instance_name|
+instances.keys.each do |instance_name|
   template "#{node[:ganglia][:datadir]}/conf/host_#{instance_name}.json" do
     source 'host_view_json.erb'
     mode '0644'
+    variables({:roles => instances[instance_name]})
   end
 end
