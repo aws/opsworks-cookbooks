8e211ff6b1eaaa1519c1d469eeaa7fcef1e546ea
Thomas Metschke <thomas.metschke@gmail.com>
2011-10-19 12:47:17 +0200

    make this shit work - really

--- scalarium_ganglia/recipes/configure-gweb2.rb
+++ scalarium_ganglia/recipes/configure-gweb2.rb
@@ -1,13 +1,9 @@
 instances = []
 
-ruby_block 'Create host view json config files for Ganglia' do
-  block do
-    node[:scalarium][:roles].each do |role_name, role_config|
-      role_config[:instances].each do |instance_name, instance_config|
-        unless instances.include?(instance_name)
-          instances << instance_name
-        end
-      end
+node[:scalarium][:roles].each do |role_name, role_config|
+  role_config[:instances].each do |instance_name, instance_config|
+    unless instances.include?(instance_name)
+      instances << instance_name
     end
   end
 end
