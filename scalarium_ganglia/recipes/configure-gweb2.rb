c9208276f7bd8047f11d6a02746166349dcfb723
Thomas Metschke <thomas.metschke@gmail.com>
2011-10-19 11:31:06 +0200

    move to ruby block

--- scalarium_ganglia/recipes/configure-gweb2.rb
+++ scalarium_ganglia/recipes/configure-gweb2.rb
@@ -1,13 +1,16 @@
-<% seen = [] -%>
-
-<% node[:scalarium][:roles].each do |role_name, role_config| -%>
-  <% role_config[:instances].each do |instance_name, instance_config| -%>
-    <% unless seen.include?(instance_name) -%>
-      template "#{node[:ganglia][:datadir]}/conf/host_#{instance_name}.json" do
-        source 'host_view_json.erb'
-        mode '0644'
+ruby_block 'Create host view json config files for Ganglia' do
+  block do
+    seen = []
+    node[:scalarium][:roles].each do |role_name, role_config|
+      role_config[:instances].each do |instance_name, instance_config|
+        unless seen.include?(instance_name)
+          template "#{node[:ganglia][:datadir]}/conf/host_#{instance_name}.json" do
+            source 'host_view_json.erb'
+            mode '0644'
+          end
+          seen << instance_name
+        end
       end
-      <% seen << instance_name -%>
-    <% end -%>
-  <% end -%>
-<% end -%>
+    end
+  end
+end
