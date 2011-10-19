6f52710964c36cf666ea91780dd78cbe440a78d3
Thomas Metschke <thomas.metschke@gmail.com>
2011-10-19 11:23:18 +0200

    syntax

--- scalarium_ganglia/recipes/configure-gweb2.rb
+++ scalarium_ganglia/recipes/configure-gweb2.rb
@@ -1,13 +1,13 @@
-seen = []
+<% seen = [] -%>
 
-node[:scalarium][:roles].each do |role_name, role_config|
-  role_config[:instances].each do |instance_name, instance_config|
-    unless seen.include?(instance_name)
+<% node[:scalarium][:roles].each do |role_name, role_config| -%>
+  <% role_config[:instances].each do |instance_name, instance_config| -%>
+    <% unless seen.include?(instance_name) -%>
       template "#{node[:ganglia][:datadir]}/conf/host_#{instance_name}.json" do
         source 'host_view_json.erb'
         mode '0644'
       end
-      seen << instance_name
-    end
-  end
-end
+      <% seen << instance_name -%>
+    <% end -%>
+  <% end -%>
+<% end -%>
