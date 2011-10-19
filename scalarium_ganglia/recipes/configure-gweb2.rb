d1ee0f0284c0dc5bc4f1462e28865065f052e854
Thomas Metschke <thomas.metschke@gmail.com>
2011-10-19 11:36:57 +0200

    move template out

--- scalarium_ganglia/recipes/configure-gweb2.rb
+++ scalarium_ganglia/recipes/configure-gweb2.rb
@@ -1,16 +1,19 @@
 ruby_block 'Create host view json config files for Ganglia' do
   block do
-    seen = []
+    instances = []
     node[:scalarium][:roles].each do |role_name, role_config|
       role_config[:instances].each do |instance_name, instance_config|
-        unless seen.include?(instance_name)
-          template "#{node[:ganglia][:datadir]}/conf/host_#{instance_name}.json" do
-            source 'host_view_json.erb'
-            mode '0644'
-          end
-          seen << instance_name
+        unless instances.include?(instance_name)
+          instances << instance_name
         end
       end
     end
+  end
+end
+
+instances.each do |instance_name|
+  template "#{node[:ganglia][:datadir]}/conf/host_#{instance_name}.json" do
+    source 'host_view_json.erb'
+    mode '0644'
   end
 end
