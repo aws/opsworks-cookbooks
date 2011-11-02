6ae32e1b141e3a5a762e1111b6ac46cd36625cbf
Thomas Metschke <thomas.metschke@gmail.com>
2011-11-02 18:26:10 +0100

    add switch between apache and nginx on more places

--- scalarium_ganglia/recipes/configure-client.rb
+++ scalarium_ganglia/recipes/configure-client.rb
@@ -28,7 +28,7 @@ if monitoring_master.nil?
   end
 end
 
-if node[:scalarium][:instance][:roles].any?{|role| ['rails-app', 'php-app', 'monitoring-master'].include?(role) }
+if node[:scalarium][:instance][:roles].any?{|role| ['php-app', 'monitoring-master'].include?(role) } || (@roles.include?('rails-app') && node[:scalarium][:rails_stack][:name] == 'apache_passenger')
   Dir.glob("#{node[:apache][:log_dir]}/*-ganglia.log").each do |ganglia_log|
     cron "Ganglia Apache Monitoring #{ganglia_log}" do
       minute "*/2"
