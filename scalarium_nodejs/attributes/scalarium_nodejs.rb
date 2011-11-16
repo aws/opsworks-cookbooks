5e96640be158beab02b4fb0e0f8667cb40574b02
Daniel Hüsch <daniel.huesch@gmail.com>
2011-11-16 09:24:01 +0100

    Bump default versions for node.js and npm.

--- scalarium_nodejs/attributes/scalarium_nodejs.rb
+++ scalarium_nodejs/attributes/scalarium_nodejs.rb
@@ -1,5 +1,5 @@
-default[:scalarium_nodejs][:version] = '0.4.7'
-default[:scalarium_nodejs][:npm_version] = '1.0.13'
+default[:scalarium_nodejs][:version] = '0.6.1'
+default[:scalarium_nodejs][:npm_version] = '1.0.105'
 default[:scalarium_nodejs][:pkgrelease] = '1'
 arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
 default[:scalarium_nodejs][:deb] = "nodejs_#{node[:scalarium_nodejs][:version]}-#{node[:scalarium_nodejs][:pkgrelease]}_#{arch}.deb"
