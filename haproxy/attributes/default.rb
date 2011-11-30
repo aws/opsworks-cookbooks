2f40c959ee8ce68a4ecea4708bf3a36a70e73d8c
Jonathan Weiss <jw@innerewut.de>
2011-11-30 15:16:01 +0100

    extract HAProxy load factors and increase them a bit

--- haproxy/attributes/default.rb
+++ haproxy/attributes/default.rb
@@ -12,6 +12,16 @@ default[:haproxy][:global_max_connections] = '80000'
 default[:haproxy][:default_max_connections] = '80000'
 default[:haproxy][:retries] = '3'
 
+# load factors for maxcon
+default[:haproxy][:maxcon_factor_rails_app] = '7'
+default[:haproxy][:maxcon_factor_rails_app_ssl] = '7'
+default[:haproxy][:maxcon_factor_php_app] = '10'
+default[:haproxy][:maxcon_factor_php_app_ssl] = '10'
+default[:haproxy][:maxcon_factor_nodejs_app] = '10'
+default[:haproxy][:maxcon_factor_nodejs_app_ssl] = '10'
+default[:haproxy][:maxcon_factor_static] = '15'
+default[:haproxy][:maxcon_factor_static_ssl] = '15'
+
 def random_haproxy_pw
   rand_array = []
   "a".upto("z"){|e| rand_array << e}
