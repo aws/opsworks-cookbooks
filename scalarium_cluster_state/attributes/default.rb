0f3bdb979537790d1a8b29708a9dc0696adb016f
Daniel Hüsch <daniel.huesch@gmail.com>
2012-02-21 14:28:19 +0100

    Fix scalarium agent root dir.

--- scalarium_cluster_state/attributes/default.rb
+++ scalarium_cluster_state/attributes/default.rb
@@ -1,3 +1,3 @@
 GC.disable
 default[:scalarium_cluster_state][:path] = '/var/lib/scalarium/cluster_state.json'
-set[:scalarium_agent_root] = '/opt/scalarium-agent'
+set[:scalarium_agent_root] = '/root/scalarium-agent'
