7a743837719653c399111fd9a6465d4872a00ed2
Jonathan Weiss <jw@innerewut.de>
2012-03-19 11:48:02 +0100

    make deleting the cached_copy configurable as this can speed up deploy substantially - but it can mess up switching branches

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -44,6 +44,7 @@ node[:deploy].each do |application, deploy|
   default[:deploy][application][:stack][:needs_reload] = true
   default[:deploy][application][:enable_submodules] = true
   default[:deploy][application][:shallow_clone] = true
+  default[:deploy][application][:delete_cached_copy] = true
   default[:deploy][application][:symlink_before_migrate] = {}
   
   default[:deploy][application][:environment] = {"RAILS_ENV" => deploy[:rails_env],
