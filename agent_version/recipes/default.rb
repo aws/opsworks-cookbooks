20558d42db34be091e76c4ae82b2cc13d7edc617
Daniel Hüsch <daniel.huesch@gmail.com>
2012-04-16 06:13:41 +0200

    Fix agent version file ownership.

--- agent_version/recipes/default.rb
+++ agent_version/recipes/default.rb
@@ -2,8 +2,8 @@ template "#{node[:scalarium_agent_root]}/TARGET_VERSION" do
   cookbook 'agent_version'
   source 'TARGET_VERSION.erb'
   backup false
-  owner 'root'
-  group 'root'
+  owner 'scalarium-agent'
+  group 'scalarium-agent'
   mode 0600
   variables :version => node.scalarium.agent_version
 end
