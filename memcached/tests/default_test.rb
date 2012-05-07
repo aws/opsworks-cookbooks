8be2d366723e64e7ccf5fe602d6c8f11a72a2d7f
Christian Requena <alberto@peritor.com>
2012-05-07 16:30:57 +0200

    test for the memcached cookbook.  Test server/service and some files

--- memcached/tests/default_test.rb
+++ memcached/tests/default_test.rb
@@ -1,73 +1,56 @@
 # test the memcached installation/service
+require 'memcached'
 
-class DhTest < MiniTest::Chef::TestCase
-    attr_accessor: cache
+class MemcacheTest < MiniTest::Chef::TestCase
+ 
+  def test_exist_service_configuration_file
+    assert File.read('/etc/default/memcached')
+  end
 
-    def test_exist_configuration_file
-          assert File.read('/etc/memcached.conf')
-    end
+  def test_exist_monit_configuration_file
+    assert File.read('/etc/monit/conf.d/memcached.monitrc')
+  end
 
-    def test_exist_service_configuration_file
-          assert File.read('/etc/default/memcached')
-    end
+  def test_service_started
+    assert service("memcached").must_be_running
+  end
 
-    def test_exist_monit_configuration_file
-          assert File.read('/etc/monit/conf.d/memcached.monitrc')
-    end
+  def test_tcp_access_to_server
+   assert TCPSocket.new('127.0.0.1', '<%=port%>') rescue puts "Memcached is not accepting connections on 127.0.0.1:<%=port%>"
+   assert TCPSocket.new('localhost', '<%=port%>') rescue puts "Memcached is not accepting connections on localhost:<%=port%>"
+   assert TCPSocket.new("#{node['instance']['private_ip']}", "<%=port%>") rescue puts "Memcached is not accepting connections on #{node['instance']['private_ip']}:<%=port%>"
+  end
 
-    def test_live_server?
-      assert TCPSocket.new('localhost', "<%=port%>") rescue puts "Memcached is not accepting connections on localhost:<%=port%>"
-    end
+  def test_access_service  
+    %w{127.0.0.1  localhost  #{node['instance']['private_ip']}}.each do |host|
+      m = Memcached.new(["#{host}:<%=port%>")]
+      #test different types of data
+      key1 = 'foo'
+      key2 = 'bar'*50
+      key3 = '£∞'*50
+      value1 = 'abc'
+      value2 = 'xyz'*1000
+      value3 = '∞§¶•ª'*1000
 
-    def test_service_running
-      require 'memcached'
-      @cache = Memcached.new("localhost:<%=port%>") 
-      value = 'hello'
-      @cache.set 'test', value
-      assert @cache.get 'test' #=> "hello"  
-    end
-
-    def test_live_server  
-      return puts("Skipping EventMachine test, no live server") if !test_live_server?
-
-      require 'eventmachine'
-      require 'memcache/event_machine'
-      ex = nil
-      within_em do
-        begin
-          m = MemCache.new(['127.0.0.1:<%=port%>', 'localhost:<%=port%>'])
-          key1 = 'foo'
-          key2 = 'bar'*50
-          key3 = '£∞'*50
-          value1 = 'abc'
-          value2 = 'xyz'*1000
-          value3 = '∞§¶•ª'*1000
-      
-          1000.times do
-            assert_equal "STORED\r\n", m.set(key1, value1)
-            assert_equal "STORED\r\n", m.set(key2, value2)
-            assert_equal "STORED\r\n", m.set(key3, value3)
-            assert_equal value1, m.get(key1)
-            assert_equal value2, m.get(key2)
-            assert_equal value3, m.get(key3)
-            assert_equal "DELETED\r\n", m.delete(key1)
-            assert_equal "STORED\r\n", m.set(key1, value2)
-            assert_equal value2, m.get(key1)
-            assert_equal "STORED\r\n", m.set(key2, value3)
-            assert_equal value3, m.get(key2)
-            assert_equal "STORED\r\n", m.set(key3, value1)
-            assert_equal value1, m.get(key3)
-            assert_equal({ key1 => value2, key2 => value3, key3 => value1 }, 
-                         m.get_multi(key1, key2, key3))
-          end
-        rescue Exception => exp
-          puts exp.message
-          ex = exp
-        ensure
-          EM.stop
-        end
+      10.times do
+        assert_equal "STORED\r\n", m.set(key1, value1)
+        assert_equal "STORED\r\n", m.set(key2, value2)
+        assert_equal "STORED\r\n", m.set(key3, value3)
+        assert_equal value1, m.get(key1)
+        assert_equal value2, m.get(key2)
+        assert_equal value3, m.get(key3)
+        assert_equal "DELETED\r\n", m.delete(key1)
+        assert_equal "STORED\r\n", m.set(key1, value2)
+        assert_equal value2, m.get(key1)
+        assert_equal "STORED\r\n", m.set(key2, value3)
+        assert_equal value3, m.get(key2)
+        assert_equal "STORED\r\n", m.set(key3, value1)
+        assert_equal value1, m.get(key3)
       end
-      raise ex if ex
+      rescue Exception => exp
+        puts exp.message
+        ex = exp
+      end
   end
 
 end
