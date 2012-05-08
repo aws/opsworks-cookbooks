a0e7d44e3af629c7159d3e179bbb99cbe7642ce0
Christian Requena <alberto@peritor.com>
2012-05-09 00:33:45 +0200

    cleaned up, fixed private ip issue and started using more build in assertions

--- memcached/tests/default_test.rb
+++ memcached/tests/default_test.rb
@@ -1,56 +1,81 @@
 # test the memcached installation/service
 require 'memcached'
+include MiniTest::Chef::Assertions
 
 class MemcacheTest < MiniTest::Chef::TestCase
  
   def test_exist_service_configuration_file
-    assert File.read('/etc/default/memcached')
+    file('/etc/default/memcached').must_exist
   end
 
   def test_exist_monit_configuration_file
-    assert File.read('/etc/monit/conf.d/memcached.monitrc')
+    file('/etc/monit/conf.d/memcached.monitrc').must_exist
   end
 
   def test_service_started
-    assert service("memcached").must_be_running
+    service("memcached").must_be_running
   end
 
-  def test_tcp_access_to_server
-   assert TCPSocket.new('127.0.0.1', '<%=port%>') rescue puts "Memcached is not accepting connections on 127.0.0.1:<%=port%>"
-   assert TCPSocket.new('localhost', '<%=port%>') rescue puts "Memcached is not accepting connections on localhost:<%=port%>"
-   assert TCPSocket.new("#{node['instance']['private_ip']}", "<%=port%>") rescue puts "Memcached is not accepting connections on #{node['instance']['private_ip']}:<%=port%>"
+  def access_memcache_over_tcp(host)
+	port = "#{node[:memcached][:port]}"
+	refute_equal nil,	
+		     TCPSocket.new("#{host}", "#{port}"), 
+		     "Memcached is not accepting connections on #{host}:#{port}"
   end
 
-  def test_access_service  
-    %w{127.0.0.1  localhost  #{node['instance']['private_ip']}}.each do |host|
-      m = Memcached.new(["#{host}:<%=port%>"])
-      #test different types of data
-      key1 = 'foo'
-      key2 = 'bar'*50
-      key3 = '£∞'*50
-      value1 = 'abc'
-      value2 = 'xyz'*1000
-      value3 = '∞§¶•ª'*1000
-
-      10.times do
-        assert_equal "STORED\r\n", m.set(key1, value1)
-        assert_equal "STORED\r\n", m.set(key2, value2)
-        assert_equal "STORED\r\n", m.set(key3, value3)
-        assert_equal value1, m.get(key1)
-        assert_equal value2, m.get(key2)
-        assert_equal value3, m.get(key3)
-        assert_equal "DELETED\r\n", m.delete(key1)
-        assert_equal "STORED\r\n", m.set(key1, value2)
-        assert_equal value2, m.get(key1)
-        assert_equal "STORED\r\n", m.set(key2, value3)
-        assert_equal value3, m.get(key2)
-        assert_equal "STORED\r\n", m.set(key3, value1)
-        assert_equal value1, m.get(key3)
-      end
-      rescue Exception => exp
-        puts exp.message
-        ex = exp
-      end
+  def test_memcache_access_via_tcp_on_127_0_0_1
+  	access_memcache_over_tcp('127.0.0.1')
   end
 
+  def test_memcache_access_via_tcp_on_localhost
+  	access_memcache_over_tcp('localhost')
+  end
+
+  def test_memcache_access_via_tcp_on_private_ip
+  	access_memcache_over_tcp("#{node[:scalarium]['instance']['private_ip']}")
+  end
+
+  def access_memcached_service(host) 
+    port = "#{node[:memcached][:port]}"
+
+    m = Memcached.new(["#{host}:#{port}"],{:binary_protocol=>false})
+
+    #test different types of data. Memcached has issues with utf8 characters.
+    key1 = 'foo'
+    key2 = 'bar'*2
+    #key3 = '£∞'
+    key3 = "223FF"
+    value1 = 'abc'
+    value2 = 'xyz'*3
+    #value3 = '∞§¶•ª'*2
+    value3 = 'ERR..'
+
+    100.times do
+      assert_equal nil , m.set(key1, value1), "STORED\r\n"
+      assert_equal nil , m.set(key2, value2), "STORED\r\n"
+      assert_equal nil , m.set(key3, value3), "STORED\r\n"
+      assert_equal value1, m.get(key1)
+      assert_equal value2, m.get(key2)
+      assert_equal value3, m.get(key3)
+      assert_equal nil , m.delete(key1), "DELETED\r\n"
+      assert_equal nil , m.set(key1, value2), "STORED\r\n"
+      assert_equal value2, m.get(key1)
+      assert_equal nil, m.set(key2, value3), "STORED\r\n"
+      assert_equal value3, m.get(key2)
+      assert_equal nil, m.set(key3, value1), "STORED\r\n"
+      assert_equal value1, m.get(key3)
+    end
+  end
+ 
+  def test_access_service_on_127_0_0_1
+    access_memcached_service('127.0.0.1')
+  end
+  
+  def test_access_service_on_localhost
+    access_memcached_service('localhost')
+  end
+  
+  def test_access_service_on_private_ip
+    access_memcached_service("#{node[:scalarium]['instance']['private_ip']}")
+  end
 end
