c6afd2c516b32b18a4723b3a07442b6904919bd6
Alberto <alberto@peritor.com>
2012-05-22 16:06:54 +0200

    fixed some metadata values to coupe with chef specs

--- dependencies/metadata.rb
+++ dependencies/metadata.rb
@@ -1,36 +1,36 @@
 maintainer "Peritor GmbH"
 maintainer_email "scalarium@peritor.com"
 description "Install .gem and .deb depencencies"
-version "0.1"
+version "0.2"
 recipe "dependencies::update", "Update all packages and gems"
 
 attribute "dependencies/gems",
   :display_name => "Gems to install",
   :description => "A list of Rubygems to install",
-  :required => false,
+  :required => "optional",
   :type => 'hash'
 
 attribute "dependencies/debs",
   :display_name => "Debian packages to install",
   :description => "A list of Debian packages (.deb) to install",
-  :required => false,
+  :required => 'optional',
   :type => 'hash'
   
 attribute "dependencies/update_debs",
   :display_name => "Update sources",
   :description => "Update sources using apt-get update",
-  :required => false,
-  :type => 'boolean'
+  :required => 'optional',
+  :type => 'string'
 
 attribute "dependencies/upgrade_debs",
   :display_name => "Update packages",
   :description => "Update packages using apt-get upgrade",
-  :required => false,
-  :type => 'boolean'
+  :required => 'optional',
+  :type => 'string'
 
 attribute "dependencies/upgrade_gems",
   :display_name => "Update gems",
   :description => "Update gems using gem update",
-  :required => false,
-  :type => 'boolean'
+  :required => 'optional',
+  :type => 'string'
 
