b6703d3f0b6a11b17a5cc6c50ad7d5a820554442
Alberto <alberto@peritor.com>
2012-06-26 15:49:04 +0200

    extended wilcard range to include different libmysqlclient1x

--- mysql/recipes/percona_client.rb
+++ mysql/recipes/percona_client.rb
@@ -7,5 +7,5 @@ package "libnet-daemon-perl"
 
 execute "Install Percona XtraDB client libraries" do
   cwd "#{node[:percona][:tmp_dir]}/#{node[:lsb][:release]}_#{node[:scalarium][:instance][:architecture]}"
-  command "dpkg -i libmysqlclient16* libmysqlclient-dev* percona-server-client* percona-server-common*"
+  command "dpkg -i libmysqlclient* percona-server-client* percona-server-common*"
 end
