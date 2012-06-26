dd5c31ccc759636063a78c9ac9245bb717617e3a
Alberto <alberto@peritor.com>
2012-06-26 16:04:43 +0200

    fixed extended wilcard range to include different libmysqlclient1x - delete old libmysqlclient15 stuff

--- mysql/recipes/percona_client.rb
+++ mysql/recipes/percona_client.rb
@@ -7,5 +7,6 @@ package "libnet-daemon-perl"
 
 execute "Install Percona XtraDB client libraries" do
   cwd "#{node[:percona][:tmp_dir]}/#{node[:lsb][:release]}_#{node[:scalarium][:instance][:architecture]}"
-  command "dpkg -i libmysqlclient* percona-server-client* percona-server-common*"
+  # rm libmysqlclient15 stuff. This brakes dependencies and to fix all packages is a PITA
+  command "rm -f libmysqlclient15*; dpkg -i libmysqlclient* percona-server-client* percona-server-common*"
 end
