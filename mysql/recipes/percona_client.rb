40acb9dacc875c207bc7157aeb390e92d9b59301
Alberto <alberto@peritor.com>
2012-07-02 09:35:33 +0200

    a less intrusive change on the installation of the percona pakages, just extend the used schema for the new packages

--- mysql/recipes/percona_client.rb
+++ mysql/recipes/percona_client.rb
@@ -7,6 +7,5 @@ package "libnet-daemon-perl"
 
 execute "Install Percona XtraDB client libraries" do
   cwd "#{node[:percona][:tmp_dir]}/#{node[:lsb][:release]}_#{node[:scalarium][:instance][:architecture]}"
-  # rm libmysqlclient15 stuff. This brakes dependencies and to fix all packages is a PITA
-  command "rm -f libmysqlclient15*; dpkg -i libmysqlclient* percona-server-client* percona-server-common*"
+  command "dpkg -i libmysqlclient1[68]* libmysqlclient-dev*  percona-server-client* percona-server-common*"
 end
