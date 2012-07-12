a373453983e30d6a50b8b2e296d74f432f7b5074
Jonathan Weiss <jw@innerewut.de>
2012-07-12 20:07:07 +0200

    Remove the Ubuntu landscape packages
    Landscape is not used, it makes login slower and sometimes even timing
    it out, e.g. http://askubuntu.com/questions/86057/ssh-hangs-about-a-minute-after-login/162373#162373

--- scalarium_initial_setup/recipes/default.rb
+++ scalarium_initial_setup/recipes/default.rb
@@ -1,3 +1,4 @@
 include_recipe "scalarium_initial_setup::sysctl"
 include_recipe 'scalarium_initial_setup::limits'
 include_recipe "scalarium_initial_setup::bind_mounts"
+include_recipe "scalarium_initial_setup::remove_landscape"
