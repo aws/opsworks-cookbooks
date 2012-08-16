4a6e1c330200394909918e4f83c00c9ed4305bc5
Alberto <alberto@peritor.com>
2012-08-16 12:17:57 +0200

    include recipe when testing is expected

--- scalarium_initial_setup/recipes/default.rb
+++ scalarium_initial_setup/recipes/default.rb
@@ -2,3 +2,5 @@ include_recipe "scalarium_initial_setup::sysctl"
 include_recipe 'scalarium_initial_setup::limits'
 include_recipe "scalarium_initial_setup::bind_mounts"
 include_recipe "scalarium_initial_setup::remove_landscape"
+
+include_recipe "setup_cookbook_tests_env" if node[:scalarium][:run_cookbook_tests] == 'true'
