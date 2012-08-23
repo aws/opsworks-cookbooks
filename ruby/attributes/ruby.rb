1b0524d3e69ba63b9f4300b24908da4b8ae4b46f
Alberto <alberto@peritor.com>
2012-08-23 14:49:38 +0200

    building the ruby package name for the different ruby stacks now happens in the cookbook

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -1,7 +1,17 @@
-default[:ruby][:major_version] = '1.9'
-default[:ruby][:full_version] = '1.9.2'
-default[:ruby][:patch] = 'p180'
-default[:ruby][:pkgrelease] = '3'
+# ruby 1.8.7 is ruby-enterprise, os we don't handle it here
+case node['scalarium']['ruby_version']
+when '1.9.2'
+  default[:ruby][:major_version] = '1.9'
+  default[:ruby][:full_version] = '1.9.2'
+  default[:ruby][:patch] = 'p180'
+  default[:ruby][:pkgrelease] = '3'
+when '1.9.3'
+  default[:ruby][:major_version] = '1.9'
+  default[:ruby][:full_version] = '1.9.3'
+  default[:ruby][:patch] = 'p194'
+  default[:ruby][:pkgrelease] = '1'
+end
+
 default[:ruby][:version] = "#{node[:ruby][:full_version]}#{node[:ruby][:patch]}"
 arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
 default[:ruby][:deb] = "ruby#{node[:ruby][:major_version]}_#{node[:ruby][:full_version]}-#{node[:ruby][:patch]}.#{node[:ruby][:pkgrelease]}_#{arch}.deb"
