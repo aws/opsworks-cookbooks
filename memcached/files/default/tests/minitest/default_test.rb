0ba7c51f9fa5c684259e963ba383bc60d9c2229d
Alberto <alberto@peritor.com>
2012-08-24 14:12:08 +0200

    deleted unnecesary changes

--- memcached/files/default/tests/minitest/default_test.rb
+++
@@ -1,6 +0,0 @@
-class DhTest < MiniTest::Chef::TestCase
-    def test_exist_configuration_file
-          assert File.read('/etc/memcached.conf')
-          #.include?('scalarium-agent ALL=NOPASSWD:/opt/scalarium-agent/bin/chef-solo'), 'allow scalarium-agent user to run chef-solo with sudo'
-    end
-end
