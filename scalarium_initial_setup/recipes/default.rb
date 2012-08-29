18292baf3a405cc94707dbf0c097e06a1bdc2a5a
Alberto <alberto@peritor.com>
2012-08-29 17:07:53 +0200

    give me a comment when runnig tests

--- scalarium_initial_setup/recipes/default.rb
+++ scalarium_initial_setup/recipes/default.rb
@@ -3,4 +3,7 @@ include_recipe 'scalarium_initial_setup::limits'
 include_recipe "scalarium_initial_setup::bind_mounts"
 include_recipe "scalarium_initial_setup::remove_landscape"
 
-include_recipe "scalarium_initial_setup::setup_cookbook_tests_env" if node[:scalarium][:run_cookbook_tests]
+if node[:scalarium][:run_cookbook_tests]
+  Chef::Log.info("Initializing Cookbook Test Environment.")
+  include_recipe "scalarium_initial_setup::setup_cookbook_tests_env"
+end
