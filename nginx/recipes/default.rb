420df03902fad83b49da708678020623e291eb07
itsthatoneguy <itsthatoneguy@someserver>
2012-09-17 12:47:42 -0700

    Add in directories that may not exist on RHEL based systems. This should not affect debian/ubuntu machines.

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -20,10 +20,24 @@
 
 package "nginx"
 
+directory node[:nginx][:dir] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
 directory node[:nginx][:log_dir] do
   mode 0755
   owner node[:nginx][:user]
   action :create
+end
+
+%w{sites-available sites-enabled conf.d}.each do |dir|
+  directory File.join(node[:nginx][:dir], dir) do
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
 end
 
 %w{nxensite nxdissite}.each do |nxscript|
