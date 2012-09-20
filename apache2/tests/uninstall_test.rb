17e4f18ab0011c8fb47d8e9754c263adc9298e92
itsthatoneguy <itsthatoneguy@someserver>
2012-09-20 16:36:25 -0700

    Fix up test for RHEL systems

--- apache2/tests/uninstall_test.rb
+++ apache2/tests/uninstall_test.rb
@@ -3,10 +3,26 @@ class Apache2UninstallTest < MiniTest::Chef::TestCase
   include MiniTest::Chef::Assertions
 
   def test_apache2_service_stopped
-    service("apache2").wont_be_running
+    case node[:platform]
+    when "debian","ubuntu"
+      service("apache2").wont_be_running
+    when "centos","redhat","amazon","fedora","scientific","oracle"
+      service("httpd").wont_be_running
+    else
+      # Fail test if we don't have a supported OS.
+      assert_equal(3, nil)
+    end
   end
 
   def test_apache2_package_removed
-    package("apache2").wont_be_installed
+    case node[:platform]
+    when "debian","ubuntu"
+      package("apache2").wont_be_installed
+    when "centos","redhat","amazon","fedora","scientific","oracle"
+      package("httpd").wont_be_installed
+    else
+      # Fail test if we don't have a supported OS.
+      assert_equal(3, nil)
+    end
   end
 end
