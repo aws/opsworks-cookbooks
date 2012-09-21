1a09b4ab35d15b3ad0b5e84b218a888bd6725178
itsthatoneguy <itsthatoneguy@someserver>
2012-09-21 14:29:57 -0700

    Initial whack at haproxy specs

--- haproxy/specs/stop_spec.rb
+++ haproxy/specs/stop_spec.rb
@@ -4,4 +4,7 @@ describe_recipe 'haproxy::stop' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'stops haproxy' do
+    service('haproxy').wont_be_running
+  end
 end
