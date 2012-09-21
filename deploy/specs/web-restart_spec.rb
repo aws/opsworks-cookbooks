00e8b723f6863089d03e2a3a731217d8b1bccc90
itsthatoneguy <itsthatoneguy@someserver>
2012-09-21 13:18:08 -0700

    Initial whack at web specs

--- deploy/specs/web-restart_spec.rb
+++ deploy/specs/web-restart_spec.rb
@@ -4,4 +4,7 @@ describe_recipe 'deploy::web-restart' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'starts nginx' do
+    service("nginx").must_be_running
+  end
 end
