189d265fc9fdbec0e3b8c3160ca4f63a7dade9b7
itsthatoneguy <itsthatoneguy@someserver>
2012-09-25 15:58:51 -0700

    Add spec files for passenger_apache2

--- passenger_apache2/specs/default_spec.rb
+++ passenger_apache2/specs/default_spec.rb
@@ -4,4 +4,39 @@ describe_recipe 'passenger_apache2::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  context 'distribution packages only', :if => dist_only? do
+    it 'installs rubygem-passenger package if RHEL based' do
+      package('rubygem-passenger').must_be_installed
+    end
+  end
+
+  context 'compilation is allowed', :unless => dist_only? do
+    it 'installs devel packages for building rubygem-passenger' do
+      case node[:platform]
+      when 'centos','redhat','amazon'
+        package('httpd-devel').must_be_installed
+        if node['platform_version'].to_f < 6.0
+          package('curl-devel').must_be_installed
+        else
+          package('libcurl-devel').must_be_installed
+          package('openssl-devel').must_be_installed
+          package('zlib-devel').must_be_installed
+        end
+      else
+        package('apache2-prefork-dev').must_be_installed
+        package('libapr1-dev').must_be_installed
+        if node[:passenger][:version] >= '3.0.0'
+          package('libcurl4-openssl-dev').must_be_installed
+        end
+      end
+    end
+
+    it 'ensures only defined passenger version is installed' do
+      assert system("test `#{node[:dependencies][:gem_binary]} list passenger | grep passenger | wc -l` -eq 1 && #{node[:dependencies][:gem_binary]} list passenger | grep '#{node[:passenger][:version]}'")
+    end
+
+    it 'installs apache2 passenger module' do
+      file(node[:passenger][:module_path]).must_exist
+    end
+  end
 end
