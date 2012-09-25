d74ac7eea4cdf2820c59fdb45da2dcdf3d81e27f
itsthatoneguy <itsthatoneguy@someserver>
2012-09-25 15:26:02 -0700

    Add spec files for nginx

--- nginx/specs/stop_spec.rb
+++ nginx/specs/stop_spec.rb
@@ -4,4 +4,7 @@ describe_recipe 'nginx::stop' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'stops nginx' do
+    service('nginx').wont_be_running
+  end
 end
--- nginx/specs/uninstall_spec.rb
+++ nginx/specs/uninstall_spec.rb
@@ -4,4 +4,11 @@ describe_recipe 'nginx::uninstall' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'stops nginx' do
+    service('nginx').wont_be_running
+  end
+
+  it 'uninstalls nginx' do
+    package('nginx').wont_be_installed
+  end
 end
