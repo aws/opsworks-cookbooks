47792cb67df43a5d2c4aa2032397eb323b19cedd
itsthatoneguy <itsthatoneguy@someserver>
2012-09-27 12:20:09 -0700

    Write up spec file

--- scalarium_bundler/specs/default_spec.rb
+++ scalarium_bundler/specs/default_spec.rb
@@ -4,4 +4,8 @@ describe_recipe 'scalarium_bundler::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'installs bundler gem' do
+    skip unless node[:scalarium_bundler][:manage_package]
+    assert system("#{node[:dependencies][:gem_binary]} list | grep bundler | grep #{node[:scalarium_bundler][:version]}")
+  end
 end
