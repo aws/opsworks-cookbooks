3b2b51a8e5c286bb781257d56a9d5b43337ae333
itsthatoneguy <itsthatoneguy@someserver>
2012-09-27 17:46:48 -0700

    Add specs

--- scalarium_rubygems/specs/default_spec.rb
+++ scalarium_rubygems/specs/default_spec.rb
@@ -4,4 +4,11 @@ describe_recipe 'scalarium_rubygems::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'installs rubygems' do
+    file('/usr/local/bin/gem').must_exist
+  end
+
+  it 'ensures rubygems is the right version' do
+    assert system("/usr/local/bin/gem -v | grep '#{node[:scalarium_rubygems][:version]}'")
+  end
 end
--- ssh_users/specs/default_spec.rb
+++ ssh_users/specs/default_spec.rb
@@ -4,4 +4,17 @@ describe_recipe 'ssh_users::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'creates scalarium group' do
+    group('scalarium').must_exist
+  end
+
+  it 'sets up sudoers file' do
+    file('/etc/sudoers').must_exist.with(:owner, 'root').and(:group, 'root').and(:mode, '440')
+  end
+
+  it 'includes the sudoers' do
+    node[:sudoers].each do |sudoer|
+      file('/etc/sudoers').must_include sudoer
+    end
+  end
 end
