77d41bed2c8f8d0edc63255d1118dd8ac952d695
itsthatoneguy <itsthatoneguy@someserver>
2012-09-28 13:29:05 -0700

    Use regex instead

--- scalarium_initial_setup/specs/limits_spec.rb
+++ scalarium_initial_setup/specs/limits_spec.rb
@@ -11,8 +11,10 @@ describe_recipe 'scalarium_initial_setup::limits' do
   it 'ensures limits.conf has right settings' do
     node[:scalarium_initial_setup][:limits].each do |key, value|
       unless value.nil?
-        file('/etc/security/limits.conf').must_include key
-        file('/etc/security/limits.conf').must_include value
+        file('/etc/security/limits.conf').must_match /^root            hard    #{key}      #{value}$/
+        file('/etc/security/limits.conf').must_match /^root            soft    #{key}      #{value}$/
+        file('/etc/security/limits.conf').must_match /^\*               hard    #{key}      #{value}$/
+        file('/etc/security/limits.conf').must_match /^\*               soft    #{key}      #{value}$/
       end
     end
   end
