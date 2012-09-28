c519b604c258550d197436fe7b6a7d9f00199ffa
itsthatoneguy <itsthatoneguy@someserver>
2012-09-27 17:10:55 -0700

    Add spec for scalarium_cluster_state

--- scalarium_cluster_state/specs/default_spec.rb
+++ scalarium_cluster_state/specs/default_spec.rb
@@ -4,4 +4,15 @@ describe_recipe 'scalarium_cluster_state::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'creates cluster state directory' do
+    directory(File.dirname(node[:scalarium_cluster_state][:path])).must_exist.with(:mode, '755')
+  end
+
+  it 'creates cluster_state.json' do
+    file(node[:scalarium_cluster_state][:path]).must_exist.with(:mode, '644')
+  end
+
+  it 'creates /etc/hosts' do
+    file('/etc/hosts').must_exist.with(:mode, '644')
+  end
 end
