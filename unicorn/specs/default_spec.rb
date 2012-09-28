c27d1bac757966061cb8cb44f28ccd991542590f
itsthatoneguy <itsthatoneguy@someserver>
2012-09-27 17:50:06 -0700

    Add specs

--- unicorn/specs/default_spec.rb
+++ unicorn/specs/default_spec.rb
@@ -4,4 +4,7 @@ describe_recipe 'unicorn::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'installs unicorn' do
+    assert system("#{node[:dependencies][:gem_binary]} list | grep unicorn | grep '#{node[:unicorn][:version]}'")
+  end
 end
