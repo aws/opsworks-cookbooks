9d55c0b1b4d0f2c879ee8c3c4c6c3c39b04ed30a
itsthatoneguy <itsthatoneguy@someserver>
2012-09-28 11:52:40 -0700

    Remove 'platform?'

--- scalarium_ganglia/specs/client_spec.rb
+++ scalarium_ganglia/specs/client_spec.rb
@@ -6,13 +6,13 @@ describe_recipe 'scalarium_ganglia::client' do
 
   describe 'ubuntu versions released in 2011' do
     it 'grabs custom deb files' do
-      skip unless platform?('ubuntu') && node[:platform_version].to_i == 11
+      skip unless node[:platform] == 'ubuntu' && node[:platform_version].to_i == 11
       file('/tmp/ganglia-monitor.deb').must_exist
       file('/tmp/libganglia1.deb').must_exist
     end
 
     it 'installs libapr1 and libconfuse0' do
-      skip unless platform?('ubuntu') && node[:platform_version].to_i == 11
+      skip unless node[:platform] == 'ubuntu' && node[:platform_version].to_i == 11
       package('libapr1').must_be_installed
       package('libconfuse0').must_be_installed
       if node[:platform_version].to_f == 11.04
@@ -21,7 +21,7 @@ describe_recipe 'scalarium_ganglia::client' do
     end
 
     it 'installs custom deb files' do
-      skip unless platform?('ubuntu') && node[:platform_version].to_i == 11
+      skip unless node[:platform] == 'ubuntu' && node[:platform_version].to_i == 11
       package('libganglia1').must_be_installed
       package('ganglia-monitor').must_be_installed
     end
@@ -29,14 +29,14 @@ describe_recipe 'scalarium_ganglia::client' do
 
   describe 'all other debian based systems' do
     it 'installs ganglia-monitor' do
-      skip if platform?('ubuntu') && node[:platform_version].to_i == 11
+      skip if node[:platform] == 'ubuntu' && node[:platform_version].to_i == 11
       package('ganglia-monitor').must_be_installed
     end
   end
 
   describe 'rhel based systems' do
     it 'installs ganglia-gmond' do
-      skip unless platform?('centos','redhat','fedora','amazon')
+      skip unless ['centos','redhat','fedora','amazon'].include?(node[:platform])
       package('ganglia-gmond').must_be_installed
     end
   end
