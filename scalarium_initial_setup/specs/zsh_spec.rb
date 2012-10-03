bbdd62d7c615d7b0042182eac94f460017cf764c
itsthatoneguy <itsthatoneguy@someserver>
2012-10-03 15:13:10 -0700

    Add zsh spec

---
+++ scalarium_initial_setup/specs/zsh_spec.rb
@@ -0,0 +1,14 @@
+require 'minitest/spec'
+
+describe_recipe 'scalarium_initial_setup::limits' do
+  include MiniTest::Chef::Resources
+  include MiniTest::Chef::Assertions
+
+  it 'installs zsh' do
+    package('zsh').must_be_installed
+  end
+
+  it 'gives us /bin/zsh' do
+    file('/bin/zsh').must_exist
+  end
+end
