15860b1ed0c234c0888965c5076f0b06f301927a
itsthatoneguy <itsthatoneguy@someserver>
2012-10-04 16:14:06 -0700

    Fix up mod_php5, so we load the right module.

--- apache2/recipes/mod_php5.rb
+++ apache2/recipes/mod_php5.rb
@@ -26,7 +26,24 @@ when "centos", "redhat", "fedora", "amazon"
   package "php" do
     action :install
     notifies :run, resources(:execute => "generate-module-list"), :immediately
+    not_if "which php"
+  end
+
+  # remove stock config
+  file File.join(node[:apache][:dir], 'conf.d', 'php.conf') do
+    action :delete
+  end
+
+  # replace with debian config
+  template File.join(node[:apache][:dir], 'mods-available', 'php5.conf') do
+    source 'mods/php5.conf.erb'
+    notifies :restart, 'service[apache2]'
   end
 end
 
-apache_module "php5"
+apache_module "php5" do
+  case node['platform']
+  when 'redhat','centos','scientific','amazon','fedora','freebsd'
+    filename 'libphp5.so'
+  end
+end
