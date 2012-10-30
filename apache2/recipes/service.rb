b4ea700bcca89dd5dafbdf2d0ee12e41e55ef2b7
Alberto <alberto@peritor.com>
2012-10-30 05:22:39 +0100

    code style and compact

--- apache2/recipes/service.rb
+++ apache2/recipes/service.rb
@@ -17,27 +17,24 @@
 # limitations under the License.
 #
 
-service "apache2" do
+service 'apache2' do
   case node[:platform]
-  when "centos","redhat","fedora","suse","amazon"
-    service_name "httpd"
+  when 'centos','redhat','fedora','amazon'
+    service_name 'httpd'
     # If restarted/reloaded too quickly httpd has a habit of failing.
     # This may happen with multiple recipes notifying apache to restart - like
     # during the initial bootstrap.
-    restart_command "/sbin/service httpd restart && sleep 1"
-    reload_command "/sbin/service httpd reload && sleep 1"
-  when "debian","ubuntu"
-    restart_command "/etc/init.d/apache2 restart && sleep 1"
-    reload_command "/etc/init.d/apache2 reload && sleep 1"
+    restart_command '/sbin/service httpd restart && sleep 1'
+    reload_command '/sbin/service httpd reload && sleep 1'
+  when 'debian','ubuntu'
+    restart_command '/etc/init.d/apache2 restart && sleep 1'
+    reload_command '/etc/init.d/apache2 reload && sleep 1'
   end
   supports value_for_platform(
-    "debian" => { "4.0" => [ :restart, :reload ], "default" => [ :restart, :reload, :status ] },
-    "ubuntu" => { "default" => [ :restart, :reload, :status ] },
-    "centos" => { "default" => [ :restart, :reload, :status ] },
-    "redhat" => { "default" => [ :restart, :reload, :status ] },
-    "fedora" => { "default" => [ :restart, :reload, :status ] },
-    "amazon" => { "default" => [ :restart, :reload, :status ] },
-    "default" => { "default" => [:restart, :reload ] }
+    'debian' => { '4.0' => [ :restart, :reload ], 'default' => [ :restart, :reload, :status ] },
+    'ubuntu' => { 'default' => [ :restart, :reload, :status ] },
+    ['centos','redhat','fedora','amazon'] => { 'default' => [ :restart, :reload, :status ] },
+    'default' => { 'default' => [:restart, :reload ] }
   )
   action :nothing
 end
