73fa19e378460b65392235483c278b5fddf33d06
Alberto <alberto@peritor.com>
2012-10-30 10:29:05 +0100

    deprecation comments

--- deploy/specs/source_spec.rb
+++ deploy/specs/source_spec.rb
@@ -4,4 +4,5 @@ describe_recipe 'deploy::source' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  # This recipe is deprecated!
 end
--- deploy/specs/user_spec.rb
+++ deploy/specs/user_spec.rb
@@ -4,4 +4,5 @@ describe_recipe 'deploy::user' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  # This recipe is deprecated
 end
