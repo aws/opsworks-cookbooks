323878b6fe327b95d5cdb33a2559f9bc50ac8103
Alberto <alberto@peritor.com>
2012-10-30 09:22:46 +0100

    code style, compact when possible, reduce platform list and quick fix rpm stuff, because is not implemented on the application jet

--- dependencies/recipes/update.rb
+++ dependencies/recipes/update.rb
@@ -1,38 +1,38 @@
 #
 # Cookbook Name:: dependencies
 # Recipe:: update
-#
 
-include_recipe "packages"
-include_recipe "gem_support"
+include_recipe 'packages'
+include_recipe 'gem_support'
 
 case node[:platform]
-when "centos", "redhat", "fedora", "suse", "amazon"
-  if node[:dependencies][:upgrade_rpms]
-    execute "yum -y update" do
-      command "yum -y update"
+when 'centos','redhat','fedora','amazon'
+  #if node[:dependencies][:upgrade_rpms] - not implemented in the application jet
+  if node[:dependencies][:upgrade_debs]
+    execute 'yum -y update' do
+      command 'yum -y update'
       action :run
     end
   end
-when "debian", "ubuntu"
+when 'debian','ubuntu'
   if node[:dependencies][:update_debs]
-    execute "apt-get update" do
-      command "apt-get update"
+    execute 'apt-get update' do
+      command 'apt-get update'
       action :run
     end
   end
 
   if node[:dependencies][:upgrade_debs]
-    execute "apt-get upgrade" do
-      command "apt-get upgrade -y"
+    execute 'apt-get upgrade' do
+      command 'apt-get upgrade -y'
       action :run
     end
   end
 end
 
 if node[:dependencies][:upgrade_gems]
-  execute "gem update" do
-    command "gem update"
+  execute 'gem update' do
+    command 'gem update'
     action :run
   end
 end
