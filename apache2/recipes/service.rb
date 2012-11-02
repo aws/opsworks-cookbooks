5131769eb4b1db7c7d938fd824e690ca817b4278
Alberto <alberto@peritor.com>
2012-11-02 22:22:25 +0100

    make apache2 manageable on amazon linux and some refactoring

--- apache2/recipes/service.rb
+++ apache2/recipes/service.rb
@@ -18,26 +18,35 @@
 #
 
 service 'apache2' do
-  case platform?
-  when 'centos','redhat','fedora','amazon'
-    service_name 'httpd'
-    # If restarted/reloaded too quickly httpd has a habit of failing.
-    # This may happen with multiple recipes notifying apache to restart - like
-    # during the initial bootstrap.
-    restart_command '/sbin/service httpd restart && sleep 1'
-    reload_command '/sbin/service httpd reload && sleep 1'
-  when 'debian','ubuntu'
-    restart_command '/etc/init.d/apache2 restart && sleep 1'
-    reload_command '/etc/init.d/apache2 reload && sleep 1'
+  service_name value_for_platform(
+    ['centos','redhat','fedora','amazon'] => {'default' => 'httpd'},
+    ['debian','ubuntu'] => {'default' => 'apache2'}
+  )
+
+  # If restarted/reloaded too quickly httpd has a habit of failing.
+  # This may happen with multiple recipes notifying apache to restart - like
+  # during the initial bootstrap.
+  ['restart','reload'].each do |srv_cmd|
+    send("#{srv_cmd}_command", value_for_platform(
+        ['centos','redhat','fedora','amazon'] => {
+          'default' => "/sbin/service httpd #{srv_cmd} && sleep 1"
+        },
+        ['debian','ubuntu'] => {
+          'default' => "/etc/init.d/apache2 #{srv_cmd} && sleep 1"
+        }
+      )
+    )
   end
 
   supports value_for_platform(
-    'debian' => {'4.0' => [:restart, :reload],
-                 'default' => [:restart, :reload, :status]
+    'debian' => {
+      '4.0' => [:restart, :reload],
+      'default' => [:restart, :reload, :status]
     },
-    'ubuntu' => {'default' => [:restart, :reload, :status]},
-    ['centos','redhat','fedora','amazon'] => {'default' => [:restart, :reload, :status]},
-    'default' => {'default' => [:restart, :reload]}
+    'ubuntu' => { 'default' => [:restart, :reload, :status] },
+    ['centos','redhat','fedora','amazon'] => { 'default' => [:restart, :reload, :status] },
+    'default' => { 'default' => [:restart, :reload] }
   )
+
   action :nothing
 end
