78da5c2ffc269868e6908ff3c1c525e1badc0826
Alberto <alberto@peritor.com>
2012-11-05 14:41:56 +0100

    style and use value_for_platform instad of "case"

--- scalarium_ganglia/recipes/monitor-mysql.rb
+++ scalarium_ganglia/recipes/monitor-mysql.rb
@@ -1,23 +1,23 @@
-case node[:platform]
-when "centos","redhat","fedora","scientific","amazon","oracle"
-  package "MySQL-python"
-when "debian","ubuntu"
-  package "python-mysqldb"
+package 'python mysql support' do
+  package_name value_for_platform(
+    ['centos','redhat','fedora','amazon'] => {'default' => 'MySQL-python'},
+    ['debian','ubuntu'] => {'default' => 'python-mysqldb'}
+  )
 end
 
-template "/etc/ganglia/conf.d/mysql.pyconf" do
-  source "mysql.pyconf.erb"
-  owner "root"
-  group "root"
-  mode '0644'
+template '/etc/ganglia/conf.d/mysql.pyconf' do
+  source 'mysql.pyconf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
 end
 
-cookbook_file "/etc/ganglia/python_modules/mysql.py" do
-  source "mysql.py"
-  mode "0755"
+cookbook_file '/etc/ganglia/python_modules/mysql.py' do
+  source 'mysql.py'
+  mode 0755
 end
 
-cookbook_file "/etc/ganglia/python_modules/DBUtil.py" do
-  source "DBUtil.py"
-  mode "0755"
+cookbook_file '/etc/ganglia/python_modules/DBUtil.py' do
+  source 'DBUtil.py'
+  mode 0755
 end
