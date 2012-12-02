f4d0ff50bb082bead7bd410977421df5c6126b79
Requena <>
2012-12-02 19:32:20 +0100

    install package screen here and not in userdata.sh

--- scalarium_initial_setup/recipes/default.rb
+++ scalarium_initial_setup/recipes/default.rb
@@ -3,7 +3,11 @@ include_recipe 'scalarium_initial_setup::limits'
 include_recipe "scalarium_initial_setup::bind_mounts"
 include_recipe "scalarium_initial_setup::remove_landscape"
 
-if node[:scalarium][:run_cookbook_tests]
-  Chef::Log.info("Initializing Cookbook Test Environment.")
-  include_recipe "scalarium_initial_setup::setup_cookbook_tests_env"
-end
+include_recipe 'scalarium_initial_setup::setup_rhel_repos'
+
+include_recipe 'scalarium_initial_setup::package_procps'
+include_recipe 'scalarium_initial_setup::package_ntpd'
+include_recipe 'scalarium_initial_setup::package_vim'
+include_recipe 'scalarium_initial_setup::package_zsh'
+include_recipe 'scalarium_initial_setup::package_sqlite'
+include_recipe 'scalarium_initial_setup::package_screen'
