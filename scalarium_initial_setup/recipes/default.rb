5b0d43e9c36bc8f983c47957b792ce11bbc08101
Requena <alberto@peritor.com>
2012-12-24 11:49:40 +0100

    create /vol during agent initialization time - removed from userdata.sh

--- scalarium_initial_setup/recipes/default.rb
+++ scalarium_initial_setup/recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe 'scalarium_initial_setup::sysctl'
 include_recipe 'scalarium_initial_setup::limits'
 include_recipe 'scalarium_initial_setup::bind_mounts'
+include_recipe 'scalarium_initial_setup::vol_mount_point'
 include_recipe 'scalarium_initial_setup::remove_landscape'
 
 include_recipe 'scalarium_initial_setup::setup_rhel_repos'
