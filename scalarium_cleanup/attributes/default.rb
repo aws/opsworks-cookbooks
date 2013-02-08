a9c9a3e7c64735d955be159573e1e373eee09f65
Requena <requena@amazon.com>
2013-02-08 15:52:12 +0100

    update path to chef's data directory

--- scalarium_cleanup/attributes/default.rb
+++ scalarium_cleanup/attributes/default.rb
@@ -1,4 +1,4 @@
 include_attribute 'scalarium_initial_setup::default'
 
 default[:scalarium_cleanup][:keep_logs] = 5
-default[:scalarium_cleanup][:log_dir] = "#{node[:scalarium][:agent][:base_dir]}/log/chef"
+default[:scalarium_cleanup][:log_dir] = "#{node[:scalarium][:agent][:shared_dir]}/log/chef"
