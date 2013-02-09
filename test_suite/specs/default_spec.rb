b4f73c9b84324926aa4a541bbfe80e90933b68eb
Requena <requena@amazon.com>
2013-02-09 16:12:03 +0100

    test_suite is an extra cookbook

--- test_suite/specs/default_spec.rb
+++ test_suite/specs/default_spec.rb
@@ -1,6 +1,6 @@
 require 'minitest/spec'
 
-describe_recipe 'scalarium_initial_setup::setup_cookbook_tests_env' do
+describe_recipe 'test_suite::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
