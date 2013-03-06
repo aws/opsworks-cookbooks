bca4c5c28e58d508486923b9b8ac2bc8c19b75a9
Christian Requena <requena@amazon.com>
2013-03-06 16:32:58 +0100

    fix deployment directorie's group to allow access to nginx and apache webservers on amazon linux

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -7,7 +7,7 @@ case node[:platform]
 when 'debian','ubuntu'
   default[:opsworks][:deploy_user][:group] = 'www-data'
 when 'centos','redhat','fedora','amazon'
-  default[:opsworks][:deploy_user][:group] = 'apache'
+  default[:opsworks][:deploy_user][:group] = node['opsworks']['rails_stack']['name'] == 'nginx_unicorn' ? 'nginx' : 'apache'
 end
 
 default[:opsworks][:rails][:ignore_bundler_groups] = ['test', 'development']
