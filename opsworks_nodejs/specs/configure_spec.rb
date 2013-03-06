415cdc30c763f6243dedfaf492d2ca7395af9bff
dznz <daniel.zollinger@changery.com>
2013-03-07 10:40:32 +1300

    Fix minor syntax errors in cookbook specs
    `rake validate_syntax` fails due to a number of small ruby syntax errors
    in the cookbooks:
    
    - `opsworks_nodejs` needed a space or method brackets around a couple of
      regex arguments.
    - `opsworks_initial_setup/specs/hosts_spec.rb` contained a rogue closing
      tag and an extra `end` statement.
    - Specs for php deploy were a bit bung. Haven't run them so don't know
      if they pass, but at least they are valid ruby now.

--- opsworks_nodejs/specs/configure_spec.rb
+++ opsworks_nodejs/specs/configure_spec.rb
@@ -8,7 +8,7 @@ describe_recipe 'opsworks_nodejs::configure' do
     config = File.join("#{deploy[:deploy_to]}",'shared/config/opsworks.js')
     file( config ).must_exist.with(
          :mode, '0660').and(:owner, "#{deploy[:user]}").and(:group, "#{deploy[:group]}")
-    file( config ).must_match/^exports.db\ =\ #{deploy[:database].to_json}/
-    file( config ).must_match/^exports.memcached\ =\ #{deploy[:memcached].to_json}/
+    file( config ).must_match(/^exports.db\ =\ #{deploy[:database].to_json}/)
+    file( config ).must_match(/^exports.memcached\ =\ #{deploy[:memcached].to_json}/)
   end
 end
