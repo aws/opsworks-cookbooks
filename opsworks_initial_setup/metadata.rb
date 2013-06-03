4464844b7c7396b8b4ee4602fa601198a40b7eb6
Christian Requena <requena@amazon.com>
2013-06-03 15:28:58 +0200

    opsworks_initial_setup depends on opsworks_commons

--- opsworks_initial_setup/metadata.rb
+++ opsworks_initial_setup/metadata.rb
@@ -2,5 +2,7 @@ maintainer "Amazon Web Services"
 description "Initial Setup for EC2 instances"
 version "0.1"
 
+depends "opsworks_commons"
+
 recipe "opsworks_initial_setup::sysctl", "Sets some sysctls to improve network performance"
 recipe "opsworks_initial_setup::bind_mounts", "Set up some bind mounts for apps, logs & co"
