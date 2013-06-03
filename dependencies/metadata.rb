98b087d5e44965babf56d5f155fcd6e34acd0d17
Christian Requena <requena@amazon.com>
2013-06-03 15:20:05 +0200

    add missing metadata files and fix metadata files for dependencies and ruby_enterprise cookbooks

--- dependencies/metadata.rb
+++ dependencies/metadata.rb
@@ -3,26 +3,32 @@ description "Install .gem and .deb depencencies"
 version "0.1"
 recipe "dependencies::update", "Update all packages and gems"
 
+depends "ruby_enterprise"
+depends "ruby"
+depends "gem_support"
+
 attribute "dependencies/gems",
   :display_name => "Gems to install",
   :description => "A list of Rubygems to install",
-  :required => false,
-  :type => 'hash'
+  :required => false
 
 attribute "dependencies/debs",
   :display_name => "Debian packages to install",
   :description => "A list of Debian packages (.deb) to install",
-  :required => false,
-  :type => 'hash'
-  
+  :required => false
+
 attribute "dependencies/update_debs",
   :display_name => "Update sources",
   :description => "Update sources using apt-get update",
-  :required => false,
-  :type => 'boolean'
+  :required => false
 
 attribute "dependencies/upgrade_debs",
   :display_name => "Update packages",
   :description => "Update packages using apt-get upgrade",
-  :required => false,
-  :type => 'boolean'
+  :required => false
+
+attribute "dependencies/upgrade_gems",
+  :display_name => "Update gems",
+  :description => "Update gems using gem update",
+  :required => false
+
--- ruby_enterprise/metadata.rb
+++ ruby_enterprise/metadata.rb
@@ -3,5 +3,4 @@ license          "Apache 2.0"
 description      "Installs/Configures ruby-enterprise"
 version          "0.1"
 
-depends "build-essential"
 supports "ubuntu"
