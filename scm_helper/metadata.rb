3389472510835cca0a229ab86cd54791fd0b4ea7
Christian Requena <requena@amazon.com>
2013-06-12 17:10:00 +0200

    update metadata for scm_helper cookbook. It depends on opsworks_commons

--- scm_helper/metadata.rb
+++ scm_helper/metadata.rb
@@ -1,3 +1,5 @@
 maintainer        "Amazon Web Services"
 description       "Offers a few library functions to interact with source control systems"
 version           "0.1"
+
+depends "opsworks_commons"
