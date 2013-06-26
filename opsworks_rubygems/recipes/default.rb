a39f73eb7dae74feca8fe9449243c69752915450
Michael Gruenewald <gruenem@amazon.de>
2013-06-26 13:39:43 +0200

    run rubygems' setup.rb with --disable-gems in 1.9+

--- opsworks_rubygems/recipes/default.rb
+++ opsworks_rubygems/recipes/default.rb
@@ -14,7 +14,7 @@ execute "tar xvfz rubygems-#{node[:opsworks_rubygems][:version]}.tgz" do
 end
 
 execute "Updating Rubygems to #{node[:opsworks_rubygems][:version]}" do
-  command "/usr/bin/env LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 /usr/local/bin/ruby setup.rb --no-rdoc --no-ri" # workaround for US-ASCII errors with rubygems 2.0.3 on opsworks
+  command node[:opsworks_rubygems][:setup_command]
   cwd "/tmp/rubygems-#{node[:opsworks_rubygems][:version]}"
   umask 022
   not_if do
