6b4007844ca7d476bb6f2f5a7c8295724c416e06
Michael Gruenewald <gruenem@amazon.de>
2013-06-28 17:38:40 +0200

    use correct syntax for default attributes

--- nginx/attributes/nginx.rb
+++ nginx/attributes/nginx.rb
@@ -1,16 +1,14 @@
-nginx Mash.new unless attribute?('nginx')
-
 case node[:platform]
 when 'debian','ubuntu'
-  nginx[:dir]     = '/etc/nginx'
-  nginx[:log_dir] = '/var/log/nginx'
-  nginx[:user]    = 'www-data'
-  nginx[:binary]  = '/usr/sbin/nginx'
+  default[:nginx][:dir]     = '/etc/nginx'
+  default[:nginx][:log_dir] = '/var/log/nginx'
+  default[:nginx][:user]    = 'www-data'
+  default[:nginx][:binary]  = '/usr/sbin/nginx'
 when 'centos','redhat','fedora','amazon'
-  nginx[:dir]     = '/etc/nginx'
-  nginx[:log_dir] = '/var/log/nginx'
-  nginx[:user]    = 'nginx'
-  nginx[:binary]  = '/usr/sbin/nginx'
+  default[:nginx][:dir]     = '/etc/nginx'
+  default[:nginx][:log_dir] = '/var/log/nginx'
+  default[:nginx][:user]    = 'nginx'
+  default[:nginx][:binary]  = '/usr/sbin/nginx'
 else
   Chef::Log.error "Cannot configure nginx, platform unknown"
 end
