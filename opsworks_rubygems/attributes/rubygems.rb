73213eda9becbd4abf38cf4a5c5e9fff5317e511
Christian Requena <requena@amazon.com>
2013-06-28 12:24:37 +0200

    Load defaults attributes on rubygems attributes file.

--- opsworks_rubygems/attributes/rubygems.rb
+++ opsworks_rubygems/attributes/rubygems.rb
@@ -1,3 +1,5 @@
+include_attribute 'opsworks_initial_setup::default'
+
 default['opsworks_rubygems']['version'] = '2.0.3'
 
 # set LC_ALL and LANG to workaround US-ASCII errors with rubygems 2.0.3 on opsworks
