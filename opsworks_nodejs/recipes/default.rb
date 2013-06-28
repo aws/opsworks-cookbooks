3096805b212c947843c7b34eaad34be45ae41936
Christian Requena <requena@amazon.com>
2013-06-28 14:12:39 +0200

    Refactor nodejs default recipe to support upgrades and downgrades.

--- opsworks_nodejs/recipes/default.rb
+++ opsworks_nodejs/recipes/default.rb
@@ -1,16 +1,25 @@
+local_nodejs_up_to_date = ::File.exists?("/usr/local/bin/node") && system("/usr/local/bin/node -v | grep -q '#{node[:opsworks_nodejs][:version]}'")
+
 case node[:platform]
 when 'debian', 'ubuntu'
   remote_file "/tmp/#{node[:opsworks_nodejs][:deb]}" do
     source node[:opsworks_nodejs][:deb_url]
     action :create_if_missing
     not_if do
-      ::File.exists?("/usr/local/bin/node") &&
-      system("/usr/local/bin/node -v | grep -q '#{node[:opsworks_nodejs][:version]}'")
+      local_nodejs_up_to_date
     end
   end
 
-  apt_package "Install node.js #{node[:opsworks_nodejs][:version]}" do
-    source "/tmp/#{node[:opsworks_nodejs][:deb]}"
+  execute "Remove old node.js versions due to update" do
+    command "dpkg --purge nodejs"
+    only_if do
+      ::File.exists?("/tmp/#{node[:opsworks_nodejs][:deb]}")
+    end
+  end
+
+  execute "Install node.js #{node[:opsworks_nodejs][:version]}" do
+    cwd "/tmp"
+    command "dpkg -i /tmp/#{node[:opsworks_nodejs][:deb]}"
     only_if do
       ::File.exists?("/tmp/#{node[:opsworks_nodejs][:deb]}")
     end
@@ -21,13 +30,14 @@ when 'centos','redhat','fedora','amazon'
     source node[:opsworks_nodejs][:rpm_url]
     action :create_if_missing
     not_if do
-      ::File.exists?("/usr/local/bin/node") &&
-      system("/usr/local/bin/node -v | grep -q '#{node[:opsworks_nodejs][:version]}'")
+      local_nodejs_up_to_date
     end
   end
 
   rpm_package "Install node.js #{node[:opsworks_nodejs][:version]}" do
     source "/tmp/#{node[:opsworks_nodejs][:rpm]}"
+    action :upgrade
+    options "--oldpackage"
     only_if do
      ::File.exists?("/tmp/#{node[:opsworks_nodejs][:rpm]}")
     end
