e790c2fe1bd28c39fa10f194b6129a63d6e85bab
Michael Gruenewald <gruenem@amazon.de>
2013-06-28 18:47:45 +0200

    remove hardcoded Chef version

--- opsworks_initial_setup/recipes/tweak_chef_yum_dump.rb
+++ opsworks_initial_setup/recipes/tweak_chef_yum_dump.rb
@@ -3,7 +3,7 @@ if node[:platform] == 'amazon'
   bash 'set lock timeout in Chef\'s yum-dump.py on micro instances' do
     user 'root'
     code <<-EOC
-      sed -i 's/lock_timeout\s=\s.*$/lock_timeout = #{node[:opsworks_initial_setup][:micro][:yum_dump_lock_timeout]}/' #{node[:opsworks_agent][:current_dir]}/vendor/bundle/ruby/1.8/gems/chef-0.9.15.5/lib/chef/provider/package/yum-dump.py
+      sed -i '/^lock_timeout/I s/=\s.*$/= #{node[:opsworks_initial_setup][:micro][:yum_dump_lock_timeout]}/' #{node[:opsworks_agent][:current_dir]}/vendor/bundle/ruby/1.8/gems/chef-#{Chef::VERSION}/lib/chef/provider/package/yum-dump.py
     EOC
     only_if { node[:opsworks][:instance][:instance_type] == 't1.micro' }
   end
