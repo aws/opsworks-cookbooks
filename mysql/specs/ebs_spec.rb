9362eac3b0ec9ae90d239c17338728bab97d1616
Udo Juettner <udoj@amazon.com>
2013-07-03 00:30:29 +0200

    Add tests for logdir.

--- mysql/specs/ebs_spec.rb
+++ mysql/specs/ebs_spec.rb
@@ -12,4 +12,13 @@ describe_recipe 'mysql::ebs' do
   it 'should cover the data directory by an autofs map' do
     assert system("automount -m | grep '#{node[:mysql][:datadir]} | -fstype=none,bind,rw :#{node[:mysql][:ec2_path]}'")
   end
+
+  it 'should have a directory for logs' do
+    directory(node[:mysql][:logdir]).must_exist.with(:owner, node[:mysql][:user]).and(
+      :group, node[:mysql][:group])
+  end
+
+  it 'should cover the log directory by an autofs map' do
+    assert system("automount -m | grep '#{node[:mysql][:logdir]} | -fstype=none,bind,rw'")
+  end
 end
