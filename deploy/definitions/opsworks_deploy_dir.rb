eae4cbde0a5944bc9006271604cf3d3cfd84e49e
Christian Requena <requena@amazon.com>
2013-07-10 12:10:43 +0200

    create shared dir on deployed apps directory with the right permissions and ownership

--- deploy/definitions/opsworks_deploy_dir.rb
+++ deploy/definitions/opsworks_deploy_dir.rb
@@ -1,5 +1,13 @@
 define :opsworks_deploy_dir do
 
+  directory "#{params[:path]}/shared" do
+    group params[:group]
+    owner params[:user]
+    mode 0770
+    action :create
+    recursive true
+  end
+
   # create shared/ directory structure
   ['log','config','system','pids','scripts','sockets'].each do |dir_name|
     directory "#{params[:path]}/shared/#{dir_name}" do
