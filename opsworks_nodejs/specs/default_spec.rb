0e36ef70bc39e01dcad6a4d3d0949cd0fbd65f72
Christian Requena <requena@amazon.com>
2013-07-14 21:55:53 +0200

    refactor and clean up nodejs tests

--- opsworks_nodejs/specs/default_spec.rb
+++ opsworks_nodejs/specs/default_spec.rb
@@ -4,21 +4,32 @@ describe_recipe 'opsworks_nodejs::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
-  describe 'debian based systems' do
-    it 'grabs deb file' do
+  describe 'delete downloaded packages' do
+    it 'deletes it them on deb-based systems' do
       skip unless ['debian','ubuntu'].include?(node[:platform])
-      file(File.join('/tmp', node[:opsworks_nodejs][:deb])).must_exist
+      file(File.join('/tmp', node[:opsworks_nodejs][:deb])).wont_exist
     end
-  end
 
-  describe 'rhel based systems' do
-    it 'grabs rpm file' do
+    it 'deletes them on rpm-based systems' do
       skip unless ['centos','redhat','fedora','amazon'].include?(node[:platform])
-      file(File.join('/tmp', node[:opsworks_nodejs][:rpm])).must_exist
+      file(File.join('/tmp', node[:opsworks_nodejs][:rpm])).wont_exist
     end
   end
 
-  it 'installs nodejs pkg' do
-     (`node --version`).chomp.must_equal("v#{node[:opsworks_nodejs][:version]}")
+  it 'installs nodejs on user space' do
+    file("/usr/local/bin/node").must_exist
   end
+
+  it 'access the right node executable from the default path' do
+     (`which node`).chomp.must_equal("/usr/local/bin/node")
+  end
+
+  it 'installs the expected version of nodejs' do
+     (`/usr/local/bin/node --version`).chomp.must_equal("v#{node[:opsworks_nodejs][:version]}")
+  end
+
+  it 'installs npm' do
+    file("/usr/local/bin/npm").must_exist
+  end
+
 end
