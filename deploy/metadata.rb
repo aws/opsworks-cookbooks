07cbf232a9b5d0878be830125a440602087a633b
Christian Requena <requena@amazon.com>
2013-07-15 03:33:35 +0200

    add more depencies to deploy metadata file.

--- deploy/metadata.rb
+++ deploy/metadata.rb
@@ -9,8 +9,9 @@ recipe "deploy::mysql", "Create the MySQL database for an app"
 recipe "deploy::mysql-configure", "Reconfigure the database"
 recipe "deploy::logrotate", "Logrotate configuration for log files in shared/log/"
 
-depends 'dependencies'
-depends 'apache2'
-depends 'mod_php5_apache2'
-depends 'nginx'
-depends 'ssh_users'
+depends "dependencies"
+depends "apache2"
+depends "mod_php5_apache2"
+depends "nginx"
+depends "ssh_users"
+depends "opsworks_agent_monit"
