f9153bca3a7ed6ff7a53a286fb04a968c515b781
Jens Braeuer <braeuer@amazon.com>
2013-07-19 19:56:21 +0200

    Run custom cookbooks after checkout.
    Make Chef find the custom cookbooks, that were checked out in this
    run. Code by joweiss.

--- opsworks_custom_cookbooks/recipes/execute.rb
+++ opsworks_custom_cookbooks/recipes/execute.rb
@@ -7,7 +7,14 @@ ruby_block("Compile Custom OpsWorks Run List") do
       opsworks_run_list = Chef::RunList.new(*node[:opsworks_custom_cookbooks][:recipes])
       Chef::Log.info "New Run List expands to #{opsworks_run_list.run_list_items.map(&:name).inspect}"
 
-      self.run_context.load(opsworks_run_list)
+      # Reload and rerun after custom cookbooks have been added to the filesystem
+      cl = Chef::CookbookLoader.new(Chef::Config[:cookbook_path])
+      cl.load_cookbooks
+      cookbook_collection = Chef::CookbookCollection.new(cl)
+      custom_cookbook_run_context = Chef::RunContext.new(self.run_context.node, cookbook_collection, self.run_context.events)
+      custom_cookbook_run_context.load(opsworks_run_list)
+      custom_cookbook_runner = Chef::Runner.new(custom_cookbook_run_context)
+      custom_cookbook_runner.converge
 
     rescue Exception => e
       Chef::Log.error "Caught exception while compiling opsworks custom run list: #{e.class} - #{e.message} - #{e.backtrace.join("\n")}"
