f4235247bbd86e53fe125dbe45b1b8774d05e691
Michael Gruenewald <gruenem@amazon.de>
2013-07-23 17:37:27 +0200

    take the recipe execution out of the exception handler

--- opsworks_custom_cookbooks/recipes/execute.rb
+++ opsworks_custom_cookbooks/recipes/execute.rb
@@ -13,12 +13,13 @@ ruby_block("Compile Custom OpsWorks Run List") do
       cookbook_collection = Chef::CookbookCollection.new(cl)
       custom_cookbook_run_context = Chef::RunContext.new(self.run_context.node, cookbook_collection, self.run_context.events)
       custom_cookbook_run_context.load(opsworks_run_list)
-      custom_cookbook_runner = Chef::Runner.new(custom_cookbook_run_context)
-      custom_cookbook_runner.converge
 
     rescue Exception => e
       Chef::Log.error "Caught exception while compiling opsworks custom run list: #{e.class} - #{e.message} - #{e.backtrace.join("\n")}"
       raise e
     end
+
+    custom_cookbook_runner = Chef::Runner.new(custom_cookbook_run_context)
+    custom_cookbook_runner.converge
   end
 end
