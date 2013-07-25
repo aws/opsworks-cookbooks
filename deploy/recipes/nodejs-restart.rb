ca2583e13d60aab5a6012620f3e6b0daac934364
Jens Braeuer <braeuer@amazon.com>
2013-07-25 15:43:46 +0200

    Drop trailing whitespace.

--- deploy/recipes/nodejs-restart.rb
+++ deploy/recipes/nodejs-restart.rb
@@ -5,7 +5,7 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping deploy::nodejs-restart for application #{application} as it is not a node.js app")
     next
   end
-  
+
   ruby_block "restart node.js application #{application}" do
     block do
       Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
