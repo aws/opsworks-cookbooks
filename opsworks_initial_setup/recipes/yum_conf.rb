cfab8ec770dafa757a44250f757f7b9542afb203
Michael Gruenewald <gruenem@amazon.de>
2013-08-01 19:38:51 +0200

    disable yum update-motd plugin

--- opsworks_initial_setup/recipes/yum_conf.rb
+++ opsworks_initial_setup/recipes/yum_conf.rb
@@ -4,3 +4,14 @@ template '/etc/yum.conf' do
   owner 'root'
   group 'root'
 end
+
+ruby_block "disable yum update-motd plugin" do
+  block do
+    rc = Chef::Util::FileEdit.new("/etc/yum/pluginconf.d/update-motd.conf")
+    rc.search_file_replace_line(/^\s*enabled\s*=\s*1\s*$/, "enabled=0")
+    rc.write_file
+  end
+  only_if do
+    ::File.exists?("/etc/yum/pluginconf.d/update-motd.conf")
+  end
+end
