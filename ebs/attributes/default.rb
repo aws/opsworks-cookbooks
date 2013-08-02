01dba0ed56ef5a6f7688cc15a14ccf93888bd717
Udo Juettner <udoj@amazon.com>
2013-08-02 17:26:57 +0200

    Fix getting devices for RAIDs with Chef 11.

--- ebs/attributes/default.rb
+++ ebs/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:ebs][:devices] = {}
-default[:ebs][:raids] = {}
+default[:ebs][:devices] ||= {}
+default[:ebs][:raids] ||= {}
 default[:ebs][:mdadm_chunk_size] = '256'
 default[:ebs][:md_read_ahead] = '65536' # 64k
 
--- ebs/recipes/raids.rb
+++ ebs/recipes/raids.rb
@@ -6,7 +6,7 @@ execute 'Load device mapper kernel module' do
   ignore_failure true
 end
 
-node.default[:ebs][:raids].each do |raid_device, options|
+node.set[:ebs][:raids].each do |raid_device, options|
   Chef::Log.info "Processing RAID #{raid_device} with options #{options} "
   lvm_device = BlockDevice.lvm_device(raid_device)
 
