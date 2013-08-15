6932b6c8358b77197222216d61e837df0af58bc2
SpeedyWinds <cesar.torres@cojourners.com>
2013-08-14 22:04:10 -0400

    Adding ulimit'

---
+++ ulimit/attributes/default.rb
@@ -0,0 +1,1 @@
+default['ulimit']['pam_su_template_cookbook'] = nil
---
+++ ulimit/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Brightcove, Inc"
+maintainer_email "masterchefs@brightcove.com"
+license          "Apache 2.0"
+description      "Installs/Configures ulimit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "ulimit"
+version          "0.1.4"
