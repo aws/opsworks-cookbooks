e8d91b92c2231962c9bb2effe437612b651b2806
Kyle House <kyle.house@sailpoint.com>
2013-09-03 14:23:53 -0500

    pointing callback to shared dir, using chef template. Also creating deploy dir

--- deploy/definitions/opsworks_deploy_dir.rb
+++ deploy/definitions/opsworks_deploy_dir.rb
@@ -9,7 +9,7 @@ define :opsworks_deploy_dir do
   end
 
   # create shared/ directory structure
-  ['log','config','system','pids','scripts','sockets'].each do |dir_name|
+  ['deploy','log','config','system','pids','scripts','sockets'].each do |dir_name|
     directory "#{params[:path]}/shared/#{dir_name}" do
       group params[:group]
       owner params[:user]
