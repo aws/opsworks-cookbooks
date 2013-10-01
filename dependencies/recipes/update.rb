92b09c5b3edb403a3d72557318a0084c9f50cb00
Michael Gruenewald <gruenem@amazon.de>
2013-10-01 11:58:41 +0200

    Handle version lock changes for Amazon Linux

--- dependencies/recipes/update.rb
+++ dependencies/recipes/update.rb
@@ -7,6 +7,18 @@ include_recipe 'gem_support'
 
 case node[:platform]
 when 'centos','redhat','fedora','amazon'
+  ruby_block "upgrade the release version lock" do
+    block do
+      rc = Chef::Util::FileEdit.new("/etc/yum.conf")
+      release_line = "releasever=#{node[:dependencies][:os_release_version]}"
+      rc.search_file_replace_line /^\s*releasever\s*=/, release_line
+      rc.write_file
+    end
+    only_if do
+      node[:dependencies][:os_release_version] && ::File.exists?("/etc/yum.conf")
+    end
+  end
+
   #if node[:dependencies][:upgrade_rpms] - not implemented in the application jet
   if node[:dependencies][:upgrade_debs]
     execute 'yum -y update' do
