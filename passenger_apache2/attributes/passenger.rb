f4b95bf85d55bd6f59bffdf7c6600a9d679bc248
Josh Starcher <josh.starcher@gmail.com>
2013-10-14 14:31:45 -0400

    Fix passenger buildout path

--- passenger_apache2/attributes/passenger.rb
+++ passenger_apache2/attributes/passenger.rb
@@ -1,15 +1,13 @@
-require 'rubygems/version'
-
 include_attribute 'opsworks_initial_setup::default'
 include_attribute 'rails::rails'
 include_attribute 'packages::packages'
 
 case node[:opsworks][:ruby_version]
-when /^1\.8/
+when /1.8/
   default[:passenger][:gems_path] = '/usr/local/lib/ruby/gems/1.8/gems'
-when /^1\.9/
+when /1.9/
   default[:passenger][:gems_path] = '/usr/local/lib/ruby/gems/1.9.1/gems'
-when /^2\.0/
+when /2.0/
   default[:passenger][:gems_path] = '/usr/local/lib/ruby/gems/2.0.0/gems'
 end
 
@@ -20,10 +18,8 @@ if platform?('centos','redhat','fedora','amazon') and node[:packages][:dist_only
   default[:passenger][:module_path] = "#{node['apache']['libexecdir']}/mod_passenger.so"
 else
 
- if ::Gem::Version.new(node[:passenger][:version]) >= ::Gem::Version.new("4.0.7")
+ if node[:passenger][:version] =~ /^4/
    default[:passenger][:module_path] = "#{passenger[:root_path]}/buildout/apache2/mod_passenger.so"
- elsif ::Gem::Version.new(node[:passenger][:version]) >= ::Gem::Version.new("3.9")
-   default[:passenger][:module_path] = "#{passenger[:root_path]}/libout/apache2/mod_passenger.so"
  else
    default[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
  end
