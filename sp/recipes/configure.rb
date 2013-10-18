1b8a76a834cac38673e97e0855633f0fdcae5abf
Josh Starcher <josh.starcher@gmail.com>
2013-10-18 11:55:35 -0400

    Don't restart after custom config stuff

--- sp/recipes/configure.rb
+++ sp/recipes/configure.rb
@@ -3,12 +3,6 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  execute "restart Rails app #{application}" do
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
-  end
-
   directory "#{deploy[:deploy_to]}" do
     action :create
     recursive true
