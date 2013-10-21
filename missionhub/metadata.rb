01d1c22ddfc02209b357e28f87eac39dbcb6bf09
Josh Starcher <josh.starcher@gmail.com>
2013-10-21 08:54:16 -0400

    Remove dependency on hostsfile

--- missionhub/metadata.rb
+++ missionhub/metadata.rb
@@ -2,7 +2,6 @@ maintainer "Josh Starcher"
 description "Configure Missionhub"
 version "0.1"
 supports "ubuntu", ">= 8.10"
-depends 'hostsfile'
 
 recipe "missionhub::configure", "Re-configure missionhub"
 
