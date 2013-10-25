67561478480a8c306fed3714b79d909d446a0a70
Christian Requena <requena@amazon.com>
2013-10-25 16:19:10 +0200

    updates of ruby are also triggered by missing packages, not only binaries.
    Conflicts:
    	ruby/recipes/default.rb

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -2,7 +2,17 @@
 # enables updating stack from ruby 1.9 to ruby 2.0
 # currently we only support one user sapce ruby installation
 
-local_ruby_up_to_date = ::File.exists?("/usr/local/bin/ruby") && system("/usr/local/bin/ruby -v | grep -q '#{node['ruby']['version']}'")
+local_ruby_up_to_date = ::File.exists?(node[:ruby][:executable]) &&
+                        system("#{node[:ruby][:executable]} -v | grep '#{node['ruby']['version']}' > /dev/null 2>&1") &&
+                        system("rpm -qa | grep 'opsworks-ruby' > /dev/null 2>&1")
+
+if local_ruby_up_to_date
+  Chef::Log.info("Userspace Ruby version is #{node['ruby']['version']} - up-to-date")
+elsif !::File.exists?(node[:ruby][:executable])
+  Chef::Log.info("Userspace Ruby version is not #{node['ruby']['version']} - #{node[:ruby][:executable]} does not exist")
+else
+  Chef::Log.info("Userspace Ruby version is not #{node['ruby']['version']} - found #{`#{node[:ruby][:executable]} -v`}")
+end
 
 case node['platform']
 when 'debian','ubuntu'
