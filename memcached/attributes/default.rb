7c11bde995366a34a9d8249f91decf9f435533fa
Daniel Hüsch <huesch@amazon.com>
2013-10-25 13:35:26 +0200

    Fix memcached pid file path on Amazon Linux.

--- memcached/attributes/default.rb
+++ memcached/attributes/default.rb
@@ -2,7 +2,16 @@ default[:memcached][:memory] = 512
 default[:memcached][:port] = 11211
 default[:memcached][:user] = "nobody"
 default[:memcached][:max_connections] = "4096"
-default[:memcached][:pid_file] = "/var/run/memcached.pid"
+
+case node[:platform]
+when 'redhat', 'centos', 'fedora', 'amazon'
+  default[:memcached][:pid_file] = '/var/run/memcached/memcached.pid'
+when 'debian', 'ubuntu'
+  default[:memcached][:pid_file] = '/var/run/memcached.pid'
+else
+  raise 'Bailing out, unknown platform.'
+end
+
 default[:memcached][:start_command] = "/etc/init.d/memcached start"
 default[:memcached][:stop_command] = "/etc/init.d/memcached stop"
 default[:memcached][:testing][:gem_version] = '1.6.1'
