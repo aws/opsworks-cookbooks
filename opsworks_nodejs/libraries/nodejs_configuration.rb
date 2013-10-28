711536d5ca9fca5a3947731995abb1e7637d912f
Daniel Hüsch <huesch@amazon.com>
2013-10-28 22:37:48 +0100

    Capture STDERR when running npm install.

--- opsworks_nodejs/libraries/nodejs_configuration.rb
+++ opsworks_nodejs/libraries/nodejs_configuration.rb
@@ -3,7 +3,7 @@ module OpsWorks
     def self.npm_install(app_name, app_config, app_root_path)
       if File.exists?("#{app_root_path}/package.json")
         Chef::Log.info("package.json detected. Running npm install.")
-        Chef::Log.info(`sudo su deploy -c 'cd #{app_root_path} && npm install'`)
+        Chef::Log.info(`sudo su deploy -c 'cd #{app_root_path} && npm install 2>&1'`)
       end
     end
   end
