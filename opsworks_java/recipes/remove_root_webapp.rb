0695168ca22a3e0136b187bb914c5ac9b15e2a53
Udo Juettner <udoj@amazon.com>
2013-11-06 21:00:42 +0100

    Remove all license text fragments.

--- opsworks_java/recipes/remove_root_webapp.rb
+++ opsworks_java/recipes/remove_root_webapp.rb
@@ -1,16 +1,3 @@
-# Copyright 2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not
-# use this file except in compliance with the License. A copy of the License is
-# located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-# or implied. See the License for the specific language governing permissions
-# and limitations under the License.
-
 ruby_block 'remove the ROOT webapp' do
   block do
     ::FileUtils.rm_rf(::File.join(node['opsworks_java'][node['opsworks_java']['java_app_server']]['webapps_base_dir'], 'ROOT'), :secure => true)
