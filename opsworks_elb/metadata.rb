71fd4bce5999ae5d4c5a3f070c7b9994ad2ff9be
Josh Starcher <josh.starcher@gmail.com>
2014-01-11 12:30:17 -0500

    Fix elb cookbook metadata

--- opsworks_elb/metadata.rb
+++ opsworks_elb/metadata.rb
@@ -5,6 +5,6 @@ license           "Apache 2.0"
 description       "Provides recipes to register and deregister from elb for an opsworks instance"
 version           "0.0.2"
 
-recipe            "opsworks-elb::register"
-recipe            "opsworks-elb::deregister"
+recipe            "opsworks-elb::register", "Registers an instance with the ELB"
+recipe            "opsworks-elb::deregister", "De-registers an instance from the ELB"
 depends           "java"
