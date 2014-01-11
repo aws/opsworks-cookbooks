a4eceade5aed8c0595a5fd38de63c688d8d8a1f4
Josh Starcher <josh.starcher@gmail.com>
2014-01-11 11:56:36 -0500

    All elb recipes

---
+++ opsworks_elb/metadata.rb
@@ -0,0 +1,10 @@
+name              "opsworks-elb"
+maintainer        "CrowdMob Inc."
+maintainer_email  "developers@crowdmob.com"
+license           "Apache 2.0"
+description       "Provides recipes to register and deregister from elb for an opsworks instance"
+version           "0.0.2"
+
+recipe            "opsworks-elb::register"
+recipe            "opsworks-elb::deregister"
+depends           "java"
---
+++ opsworks_elb/recipes/default.rb
@@ -0,0 +1,34 @@
+include_recipe "java"
+
+java_home = node['aws']['elb']['java_home']
+
+package "unzip" do
+  action :install
+end
+
+directory "#{node['aws']['elb']['cli_install_path']}/elb" do
+  group      "root"
+  owner      "root"
+  mode       0770
+  action     :create
+  recursive  true
+end
+
+bash "install-aws-elb-cli" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    rm -rf ElasticLoadBalancing-*
+    rm -rf #{node['aws']['elb']['cli_install_path']}/elb
+    unzip #{node['aws']['elb']['cli_download_filename']}
+    rsync -a --no-o --no-g ElasticLoadBalancing-*/ #{node['aws']['elb']['cli_install_path']}/elb
+  EOH
+  action :nothing
+end
+
+remote_file File.join(Chef::Config[:file_cache_path], node['aws']['elb']['cli_download_filename']) do
+  source node['aws']['elb']['cli_download_url']
+  owner "root"
+  mode 0644
+  notifies :run, resources(:bash => "install-aws-elb-cli"), :immediately
+  not_if "/usr/bin/env JAVA_HOME=#{java_home} AWS_ELB_HOME=#{node['aws']['elb']['cli_install_path']}/elb #{node['aws']['elb']['cli_install_path']}/elb/bin/elb-cmd version | grep #{node['aws']['elb']['cli_version']}"
+end
