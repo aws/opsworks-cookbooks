867b077640ca2c0e73e3c28e68b38498aff2b815
Josh Starcher <josh.starcher@gmail.com>
2014-01-22 12:15:51 -0500

    Make sure the deploy['domains'] key exists before accessing it

--- passenger_apache2/definitions/passenger_web_app.rb
+++ passenger_apache2/definitions/passenger_web_app.rb
@@ -8,56 +8,58 @@ define :passenger_web_app do
   Chef::Log.debug(params)
   Chef::Log.debug(node)
 
-  template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.crt" do
-    cookbook 'passenger_apache2'
-    mode '0600'
-    source "ssl.key.erb"
-    variables :key => deploy['ssl_certificate']
-    notifies :restart, "service[apache2]"
-    only_if do
-      deploy['ssl_support']
+  if deploy['domains']
+    template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.crt" do
+      cookbook 'passenger_apache2'
+      mode '0600'
+      source "ssl.key.erb"
+      variables :key => deploy['ssl_certificate']
+      notifies :restart, "service[apache2]"
+      only_if do
+        deploy['ssl_support']
+      end
     end
-  end
 
-  template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.key" do
-    cookbook 'passenger_apache2'
-    mode '0600'
-    source "ssl.key.erb"
-    variables :key => deploy['ssl_certificate_key']
-    notifies :restart, "service[apache2]"
-    only_if do
-      deploy['ssl_support']
+    template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.key" do
+      cookbook 'passenger_apache2'
+      mode '0600'
+      source "ssl.key.erb"
+      variables :key => deploy['ssl_certificate_key']
+      notifies :restart, "service[apache2]"
+      only_if do
+        deploy['ssl_support']
+      end
     end
-  end
 
-  template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.ca" do
-    cookbook 'passenger_apache2'
-    mode '0600'
-    source "ssl.key.erb"
-    variables :key => deploy['ssl_certificate_ca']
-    notifies :restart, "service[apache2]"
-    only_if do
-      deploy['ssl_support'] && deploy['ssl_certificate_ca']
+    template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.ca" do
+      cookbook 'passenger_apache2'
+      mode '0600'
+      source "ssl.key.erb"
+      variables :key => deploy['ssl_certificate_ca']
+      notifies :restart, "service[apache2]"
+      only_if do
+        deploy['ssl_support'] && deploy['ssl_certificate_ca']
+      end
     end
-  end
 
-  # move away default virtual host so that the Rails app becomes the default virtual host
-  execute "mv away default virtual host" do
-    action :run
-    command "mv #{node['apache']['dir']}/sites-enabled/000-default #{node['apache']['dir']}/sites-enabled/zzz-default"
-    only_if do
-      File.exists?("#{node['apache']['dir']}/sites-enabled/000-default")
+    # move away default virtual host so that the Rails app becomes the default virtual host
+    execute "mv away default virtual host" do
+      action :run
+      command "mv #{node['apache']['dir']}/sites-enabled/000-default #{node['apache']['dir']}/sites-enabled/zzz-default"
+      only_if do
+        File.exists?("#{node['apache']['dir']}/sites-enabled/000-default")
+      end
     end
-  end
 
-  web_app deploy['application'] do
-    docroot deploy['absolute_document_root']
-    server_name deploy['domains'].first
-    server_aliases deploy['domains'][1, deploy['domains'].size] unless deploy['domains'][1, deploy['domains'].size].empty?
-    rails_env deploy['rails_env']
-    mounted_at deploy['mounted_at']
-    ssl_certificate_ca deploy['ssl_certificate_ca']
-    cookbook "passenger_apache2"
-    deploy deploy
+    web_app deploy['application'] do
+      docroot deploy['absolute_document_root']
+      server_name deploy['domains'].first
+      server_aliases deploy['domains'][1, deploy['domains'].size] unless deploy['domains'][1, deploy['domains'].size].empty?
+      rails_env deploy['rails_env']
+      mounted_at deploy['mounted_at']
+      ssl_certificate_ca deploy['ssl_certificate_ca']
+      cookbook "passenger_apache2"
+      deploy deploy
+    end
   end
 end
