784afa5b83554bd90b914989d128bdff0b378ba9
Josh Starcher <josh.starcher@gmail.com>
2014-01-22 11:26:21 -0500

    Try using string based keys

--- passenger_apache2/definitions/passenger_web_app.rb
+++ passenger_apache2/definitions/passenger_web_app.rb
@@ -1,59 +1,57 @@
 define :passenger_web_app do
   include_recipe "apache2::service"
-  deploy = params[:deploy]
-  application = params[:application]
-  
-  raise deploy.inspect
+  deploy = params['deploy']
+  application = params['application']
 
-  template "#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.crt" do
+  template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.crt" do
     cookbook 'passenger_apache2'
     mode '0600'
     source "ssl.key.erb"
-    variables :key => deploy[:ssl_certificate]
+    variables :key => deploy['ssl_certificate']
     notifies :restart, "service[apache2]"
     only_if do
-      deploy[:ssl_support]
+      deploy['ssl_support']
     end
   end
 
-  template "#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.key" do
+  template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.key" do
     cookbook 'passenger_apache2'
     mode '0600'
     source "ssl.key.erb"
-    variables :key => deploy[:ssl_certificate_key]
+    variables :key => deploy['ssl_certificate_key']
     notifies :restart, "service[apache2]"
     only_if do
-      deploy[:ssl_support]
+      deploy['ssl_support']
     end
   end
 
-  template "#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.ca" do
+  template "#{node['apache']['dir']}/ssl/#{deploy['domains'].first}.ca" do
     cookbook 'passenger_apache2'
     mode '0600'
     source "ssl.key.erb"
-    variables :key => deploy[:ssl_certificate_ca]
+    variables :key => deploy['ssl_certificate_ca']
     notifies :restart, "service[apache2]"
     only_if do
-      deploy[:ssl_support] && deploy[:ssl_certificate_ca]
+      deploy['ssl_support'] && deploy['ssl_certificate_ca']
     end
   end
 
   # move away default virtual host so that the Rails app becomes the default virtual host
   execute "mv away default virtual host" do
     action :run
-    command "mv #{node[:apache][:dir]}/sites-enabled/000-default #{node[:apache][:dir]}/sites-enabled/zzz-default"
+    command "mv #{node['apache']['dir']}/sites-enabled/000-default #{node['apache']['dir']}/sites-enabled/zzz-default"
     only_if do
-      File.exists?("#{node[:apache][:dir]}/sites-enabled/000-default")
+      File.exists?("#{node['apache']['dir']}/sites-enabled/000-default")
     end
   end
 
-  web_app deploy[:application] do
-    docroot deploy[:absolute_document_root]
-    server_name deploy[:domains].first
-    server_aliases deploy[:domains][1, deploy[:domains].size] unless deploy[:domains][1, deploy[:domains].size].empty?
-    rails_env deploy[:rails_env]
-    mounted_at deploy[:mounted_at]
-    ssl_certificate_ca deploy[:ssl_certificate_ca]
+  web_app deploy['application'] do
+    docroot deploy['absolute_document_root']
+    server_name deploy['domains'].first
+    server_aliases deploy['domains'][1, deploy['domains'].size] unless deploy['domains'][1, deploy['domains'].size].empty?
+    rails_env deploy['rails_env']
+    mounted_at deploy['mounted_at']
+    ssl_certificate_ca deploy['ssl_certificate_ca']
     cookbook "passenger_apache2"
     deploy deploy
   end
