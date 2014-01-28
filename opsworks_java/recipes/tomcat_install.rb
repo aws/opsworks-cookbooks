fb33deb89b3b1e465b92299df7f83cfc7419b358
athleticoffice <dexter@athleticoffice.com>
2014-01-28 16:22:15 -0600

    SF cookbook_files

--- opsworks_java/recipes/tomcat_install.rb
+++ opsworks_java/recipes/tomcat_install.rb
@@ -23,87 +23,72 @@ directory node['opsworks_java']['tomcat']['java_shared_lib_dir_ext'] do
   action :create
 end
 
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/activation.jar" do
+  cookbook_file "/usr/share/java/extactivation.jar" do
     source "activation.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  
-  link "{['opsworks_java']['tomcat']['lib_dir_ext']}/activation.jar" do
-    to "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/activation.jar"
-    not_if { !::File.exists?("{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/activation.jar") }
-  end
-
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/ccpp.jar" do
+  cookbook_file "/usr/share/java/ext/ccpp.jar" do
     source "ccpp.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/hsql.jar" do
+  cookbook_file "/usr/share/java/ext/hsql.jar" do
     source "hsql.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/jms.jar" do
+  cookbook_file "/usr/share/java/ext/jms.jar" do
     source "jms.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/jta.jar" do
+  cookbook_file "/usr/share/java/ext/jta.jar" do
     source "jta.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/jtds.jar" do
+  cookbook_file "/usr/share/java/ext/jtds.jar" do
     source "jtds.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/junit.jar" do
+  cookbook_file "/usr/share/java/ext/junit.jar" do
     source "junit.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/jtuf7.jar" do
+  cookbook_file "/usr/share/java/ext/jtuf7.jar" do
     source "jutf7.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/mail.jar" do
+  cookbook_file "/usr/share/java/ext/mail.jar" do
     source "mail.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/mysql.jar" do
+  cookbook_file "/usr/share/java/ext/mysql.jar" do
     source "mysql.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/persistance.jar" do
+  cookbook_file "/usr/share/java/ext/persistance.jar" do
     source "persistance.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/portal-service.jar" do
+  cookbook_file "/usr/share/java/ext/portal-service.jar" do
     source "portal-service.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/portlet.jar" do
+  cookbook_file "/usr/share/java/ext/portlet.jar" do
     source "portlet.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/postgresql.jar" do
+  cookbook_file "/usr/share/java/ext/postgresql.jar" do
     source "postgresql.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
-  cookbook_file "{['opsworks_java']['tomcat']['java_shared_lib_dir_ext']}/support-tomcat.jar" do
+  cookbook_file "/usr/share/java/ext/support-tomcat.jar" do
     source "support-tomcat.jar"
     mode "0644"
-    cookbook 'opsworks_java'
   end
+  
+  link "/usr/share/tomcat7/lib/ext" do
+    to "/usr/share/java/ext/support-tomcat.jar"
+    not_if { !::File.exists?("/usr/share/java/ext/support-tomcat.jar") }
+  end
+
 
 link ::File.join(node['opsworks_java']['tomcat']['lib_dir'], node['opsworks_java']['tomcat']['mysql_connector_jar']) do
   to ::File.join(node['opsworks_java']['tomcat']['java_shared_lib_dir'], node['opsworks_java']['tomcat']['mysql_connector_jar'])
