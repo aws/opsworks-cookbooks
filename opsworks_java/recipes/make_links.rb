9bb5ae6adab1631fe2eaa1ebc7ff720450b71d48
athleticoffice <dexter@athleticoffice.com>
2014-01-29 11:37:28 -0600

    Update make_links.rb

--- opsworks_java/recipes/make_links.rb
+++ opsworks_java/recipes/make_links.rb
@@ -1,4 +1,14 @@
-link ::File.join("/usr/share/java/ext", "ccpp.jar") do
-  to ::File.join("/usr/share/tomcat7/lib/ext", "ccpp.jar")
+link ::File.join("//usr/share/tomcat7/lib/ext", "activation.jar") do
+  to ::File.join("/usr/share/java/ext", "ccpp.jar")
+  action :create
+end
+
+link ::File.join("//usr/share/tomcat7/lib/ext", "ccpp.jar") do
+  to ::File.join("/usr/share/java/ext", "ccpp.jar")
+  action :create
+end
+
+link ::File.join("//usr/share/tomcat7/lib/ext", "hsql.jar") do
+  to ::File.join("/usr/share/java/ext", "hsql.jar")
   action :create
 end
