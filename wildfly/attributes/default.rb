44a168cc63ae3be1039448bf0118bf7ef3fbe9a9
ryancarlson <ryan.t.carlson@cru.org>
2014-01-31 15:55:16 -0500

    Moving wildfly recipes to wildfly cookbook  - Tomcat setup is no longer part of the opsworks_java cookbook by default.  its recipe must be enabled  - Setting up tomcat as part of including java was dumb IMO and shouldn't have been done that way in the first place

---
+++ wildfly/attributes/default.rb
@@ -0,0 +1,9 @@
+default['opsworks_java']['wildfly']['version'] = '8.0.0.CR1'
+default['opsworks_java']['wildfly']['user'] = 'wildfly'
+default['opsworks_java']['wildfly']['group'] = 'wildfly'
+default['opsworks_java']['wildfly']['service_name'] = 'wildfly'
+default['opsworks_java']['wildfly']['port'] = 8080
+default['opsworks_java']['wildfly']['secure_port'] = 8443
+default['opsworks_java']['wildfly']['ajp_port'] = 8009
+default['opsworks_java']['wildfly']['shutdown_port'] = 8005
+default['opsworks_java']['wildfly']['uri_encoding'] = 'UTF-8'
---
+++ wildfly/recipes/wildfly_service.rb
@@ -0,0 +1,12 @@
+service 'wildfly' do
+  service_name node['opsworks_java']['wildfly']['service_name']
+
+  case node[:platform_family]
+    when 'debian'
+      supports :restart => true, :reload => false, :status => true
+    when 'rhel'
+      supports :restart => true, :reload => true, :status => true
+  end
+
+  action :nothing
+end
