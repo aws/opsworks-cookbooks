20b9efce9d96b2f27c251accccac337ae4d7594a
ryancarlson <ryan.t.carlson@cru.org>
2014-02-04 14:19:12 -0500

    Adding and removing a couple parameters for crs_registration_api

--- conf_registration_api/attributes/default.rb
+++ conf_registration_api/attributes/default.rb
@@ -1,6 +1,8 @@
-default['crs-api']['client-url'] = 'http://cruglobal.github.io/conf-registration-web/#/'
+default['crs-api']['client-url'] = 'http://cruglobal.github.io/conf-registration-web/#'
 default['crs-api']['permission-activation-url-path'] = '/activatePermission'
-default['crs-api']['register-url-path'] = '/register'
+default['crs-api']['auth-url-path'] = '/auth'
+
+default['crs-api']['max-session-length'] = 4
 
 default['crs-api']['facebook-app-id'] = '217890171695297'
 
@@ -12,6 +14,9 @@ default['crs-api']['mail-server-username'] = 'AKIAJEGKFOK7SWWYQRQA'
 
 default['crs-api']['authnet-test-id'] = '9H59j8uV7'
 default['crs-api']['authnet-url'] ='https://test.authorize.net/gateway/transact.dll'
+default['crs-api']['authnet-test-mode'] = false
+
+default['crs-api']['currency-code'] = 'USD'
 
 default['crs-api']['errbit-endpoint'] = 'https://errors.uscm.org/notifier_api/v2/notices'
 default['crs-api']['errbit-environment'] = 'production'
