5349a63b9636a5a148ad95540cd0200a8bfdfc92
ryancarlson <ryan.t.carlson@cru.org>
2014-02-04 10:04:01 -0500

    Beginnings of a cookbook for deploying CRS specific things to AWS  - default properties  - crs settings file template

---
+++ conf-registration-api/attributes/default.rb
@@ -0,0 +1,18 @@
+default['crs-api']['client-url'] = 'http://cruglobal.github.io/conf-registration-web/#/'
+default['crs-api']['permission-activation-url-path'] = '/activatePermission'
+default['crs-api']['register-url-path'] = '/register'
+
+default['crs-api']['facebook-app-id'] = '217890171695297'
+
+default['crs-api']['database-url'] = 'jdbc:postgresql://10.91.175.60/crs_dev'
+default['crs-api']['database-username'] = 'crsuser'
+
+default['crs-api']['mail-server'] = 'email-smtp.us-east-1.amazonaws.com'
+default['crs-api']['mail-server-username'] = 'AKIAJEGKFOK7SWWYQRQA'
+
+default['crs-api']['authnet-test-id'] = '9H59j8uV7'
+
+default['crs-api']['errbit-endpoint'] = 'https://errors.uscm.org/notifier_api/v2/notices'
+default['crs-api']['errbit-environment'] = 'production'
+
+default['crs-api']['crs-email'] = 'cru-dev@cru.org'
