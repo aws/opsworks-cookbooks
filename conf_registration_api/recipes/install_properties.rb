7e4b08af8122a8f01ac568096b489cd8564316ba
ryancarlson <ryan.t.carlson@cru.org>
2014-02-04 13:29:51 -0500

    Moving propeties file to follow chef convention (templates/default)  - Renaming files ..settings.xml -> ..properties.xml

--- conf_registration_api/recipes/install_properties.rb
+++ conf_registration_api/recipes/install_properties.rb
@@ -1,22 +1,22 @@
 directory '/apps' do
   owner 'wildfly'
   group 'wildfly'
-  mode 0744
+  mode 0755
   action :create
 end
 
 directory '/apps/apps-config' do
   owner 'wildfly'
   group 'wildfly'
-  mode 0744
+  mode 0755
   action :create
 end
 
-template '/apps/apps-config/conf_registration_api-settings.xml' do
+template '/apps/apps-config/conf-registration-api-properties.xml' do
   owner 'wildfly'
   group 'wildfly'
   mode 0744
   backup false
-  source 'conf_registration_api_settings.xml.erb'
+  source 'conf_registration_api_properties.xml.erb'
   action :create
 end
