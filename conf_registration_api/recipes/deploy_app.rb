4aad944924449c17cc2b776b2ce835bc549820f3
Ryan Carlson <ryan.t.carlson@cru.org>
2014-02-05 16:52:22 -0500

    Fixing bug that parent directory did not first exist  -changing owner/group to parameterized values  -formatting

--- conf_registration_api/recipes/deploy_app.rb
+++ conf_registration_api/recipes/deploy_app.rb
@@ -1,21 +1,28 @@
+directory 'tmp/artifacts' do
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
+  mode 0744
+  action :create
+end
+
 directory '/tmp/artifacts/crs-api' do
-	
-	
-	action :create
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
+  mode 0744
+  action :create
 end
 
 git '/tmp/artifacts/crs-api' do
-	depth 5
-	repository 'git@github.com:CruGlobal/conf-registration-api.git'
-	revision 'mvn-repo'
-	user 'wildfly'
-	group 'wildfly'
+  depth 5
+  repository 'git@github.com:CruGlobal/conf-registration-api.git'
+  revision 'mvn-repo'
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
 end
 
 execute 'move file to server' do
-	command 'mv *.war /opt/wildfly-' + node['wildfly']['version'] + '/standalone/deployments/crs-http-json-api.war'
-	cwd '/tmp/artifacts/crs-api/conf-registration-api/org/cru/crs-http-json-api/' + node['crs-api']['version']
-	group 'wildfly'
-	user 'wildfly'
-	
+  command 'mv *.war /opt/wildfly-' + node['wildfly']['version'] + '/standalone/deployments/crs-http-json-api.war'
+  cwd '/tmp/artifacts/crs-api/conf-registration-api/org/cru/crs-http-json-api/' + node['crs-api']['version']
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
 end
