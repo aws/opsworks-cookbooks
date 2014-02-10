5290eb61354c126dcefd6d3b5957eee8debf8617
athleticoffice <dexter@athleticoffice.com>
2014-02-10 16:08:11 -0600

    Update make_servers.rb

--- opsworks_java/recipes/make_servers.rb
+++ opsworks_java/recipes/make_servers.rb
@@ -7,3 +7,13 @@
     action :run
     command "tomcat7-instance-create -p 8082 -c 8007 /opt/athleticoffice/servers/omega"
   end
+
+
+  bin_dir = ::File.join('/usr/share/tomcat7', 'bin')
+  server_bin_dir = ::File.join('/opt/athleticoffice/servers/alpha', 'bin')
+
+
+  link server_bin_dir do
+    to bin_dir
+    action :create
+  end
