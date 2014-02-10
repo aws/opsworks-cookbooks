9f2189603e6eba47563fb599e593f4a13316a81a
athleticoffice <dexter@athleticoffice.com>
2014-02-10 15:32:44 -0600

    Update make_servers.rb

--- opsworks_java/recipes/make_servers.rb
+++ opsworks_java/recipes/make_servers.rb
@@ -2,3 +2,8 @@
     action :run
     command "tomcat7-instance-create -p 8081 -c 8006 /opt/athleticoffice/servers/alpha"
   end
+  
+    execute 'create omega tomcat server' do
+    action :run
+    command "tomcat7-instance-create -p 8082 -c 8007 /opt/athleticoffice/servers/omega"
+  end
