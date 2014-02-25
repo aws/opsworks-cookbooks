c801811ab6256cc6225791652a9226d22f70f8c4
athleticoffice <dexter@athleticoffice.com>
2014-02-25 11:28:30 -0600

    Create custom.rb

---
+++ deploy/recipes/custom.rb
@@ -0,0 +1,16 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    app application
+    deploy_data deploy
+  end
+
+end
