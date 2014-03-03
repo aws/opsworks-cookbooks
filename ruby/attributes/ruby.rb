117167854d58c307b5b46acc3dd1d08ac899d610
Christian Requena <requena@amazon.de>
2014-03-03 15:41:20 +0100

    Update patch level for ruby packages

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -34,7 +34,7 @@ case node["opsworks"]["ruby_version"]
 when "2.1"
   default[:ruby][:major_version] = '2'
   default[:ruby][:minor_version] = '1'
-  default[:ruby][:patch_version] = '0'
+  default[:ruby][:patch_version] = '1'
   default[:ruby][:pkgrelease]    = '1'
 
   i = node[:ruby]
@@ -44,12 +44,12 @@ when "2.1"
 when "2.0.0"
   default[:ruby][:major_version] = '2.0'
   default[:ruby][:full_version] = '2.0.0'
-  default[:ruby][:patch] = 'p353'
+  default[:ruby][:patch] = 'p451'
   default[:ruby][:pkgrelease] = '1'
 when "1.9.3"
   default[:ruby][:major_version] = '1.9'
   default[:ruby][:full_version] = '1.9.3'
-  default[:ruby][:patch] = 'p484'
+  default[:ruby][:patch] = 'p545'
   default[:ruby][:pkgrelease] = '1'
 else
   default[:ruby][:major_version] = ''
