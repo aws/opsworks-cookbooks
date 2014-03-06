f2968417e2865806e7716c5241e5d3cb299d001b
Daniel Hüsch <huesch@amazon.com>
2014-03-06 13:58:32 +0100

    Use berkshelf packages with unified URLs and a berkshelf bin wrapper.

--- opsworks_custom_cookbooks/attributes/default.rb
+++ opsworks_custom_cookbooks/attributes/default.rb
@@ -20,15 +20,16 @@ default[:opsworks_custom_cookbooks][:enable_submodules] = true
 
 default[:opsworks_custom_cookbooks][:berkshelf_cookbook_path] = ChefClientConfigSimpleParser.get_attribute(Chef::Config[:config_file], 'cookbook_path').detect{|dir| dir =~ /berkshelf-cookbooks/}
 default[:opsworks_custom_cookbooks][:berkshelf_version] = '2.0.14'
-default[:opsworks_custom_cookbooks][:berkshelf_binary] = '/opt/aws/opsworks/local/lib/ruby/gems/2.0.0/bin/berks'
+default[:opsworks_custom_cookbooks][:berkshelf_pkg_release] = '1'
+default[:opsworks_custom_cookbooks][:berkshelf_binary] = '/opt/aws/opsworks/local/bin/berks'
 
 case node[:platform]
 when 'redhat', 'centos', 'fedora', 'amazon'
   arch = RUBY_PLATFORM.match(/64/) ? 'x86_64' : 'i686'
   default[:opsworks_custom_cookbooks][:berkshelf_package_file] = "berkshelf_#{node[:opsworks_custom_cookbooks][:berkshelf_version]}.rpm"
-  default[:opsworks_custom_cookbooks][:berkshelf_package_url] = "https://s3.amazonaws.com/huesch-dummy/packages/amazon/2012.09/opsworks-berkshelf-#{node[:opsworks_custom_cookbooks][:berkshelf_version]}-1.#{arch}.rpm"
+  default[:opsworks_custom_cookbooks][:berkshelf_package_url] = "https://s3.amazonaws.com/huesch-dummy/packages/amazon/2013.09/opsworks-berkshelf-#{node[:opsworks_custom_cookbooks][:berkshelf_version]}-#{node[:opsworks_custom_cookbooks][:berkshelf_pkg_release]}.#{arch}.rpm"
 when 'ubuntu', 'debian'
   arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
   default[:opsworks_custom_cookbooks][:berkshelf_package_file] = "berkshelf_#{node[:opsworks_custom_cookbooks][:berkshelf_version]}.deb"
-  default[:opsworks_custom_cookbooks][:berkshelf_package_url] = "https://s3.amazonaws.com/huesch-dummy/packages/ubuntu/12.04/opsworks-berkshelf_#{node[:opsworks_custom_cookbooks][:berkshelf_version]}_#{arch}.deb"
+  default[:opsworks_custom_cookbooks][:berkshelf_package_url] = "https://s3.amazonaws.com/huesch-dummy/packages/ubuntu/12.04/opsworks-berkshelf_#{node[:opsworks_custom_cookbooks][:berkshelf_version]}-#{node[:opsworks_custom_cookbooks][:berkshelf_pkg_release]}_#{arch}.deb"
 end
