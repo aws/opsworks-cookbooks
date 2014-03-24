649b4e932fd32b58c0d602e0556ec303f36a2b72
Christian Requena <requena@amazon.de>
2014-03-24 09:46:02 +0100

    mysql installation conditional uses has_key? and update mysql client test
    cr https://cr.amazon.com/r/2250396/

--- mysql/recipes/client.rb
+++ mysql/recipes/client.rb
@@ -1,5 +1,5 @@
-if node[:opsworks][:layers]['db-master']
-  include_recipe 'mysql::client_install'
+if node[:opsworks][:layers].has_key?('db-master')
+  include_recipes 'mysql::client_install'
 else
   Chef::Log.info 'No db-master node found. Skipping MySQL client package installation.'
 end
