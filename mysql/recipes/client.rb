c6dd2d8791d176eb88014e857a7571876cc09946
Christian Requena <requena@amazon.de>
2014-03-25 18:22:36 +0100

    Fix include_recipes typo to include_recipe

--- mysql/recipes/client.rb
+++ mysql/recipes/client.rb
@@ -1,5 +1,5 @@
 if node[:opsworks][:layers].has_key?('db-master')
-  include_recipes 'mysql::client_install'
+  include_recipe 'mysql::client_install'
 else
   Chef::Log.info 'No db-master node found. Skipping MySQL client package installation.'
 end
