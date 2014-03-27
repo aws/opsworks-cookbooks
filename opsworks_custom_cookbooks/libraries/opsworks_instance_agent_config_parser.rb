e2be7e83cc7ed94085b2eb05f4e5d4481fb77960
Daniel Hüsch <huesch@amazon.com>
2014-03-27 12:16:20 +0100

    Merge all cookbooks sources and use a single cookbook_path in Chef runs.
    cr https://cr.amazon.com/r/2264452/

--- opsworks_custom_cookbooks/libraries/opsworks_instance_agent_config_parser.rb
+++ opsworks_custom_cookbooks/libraries/opsworks_instance_agent_config_parser.rb
@@ -3,8 +3,16 @@ module OpsworksInstanceAgentConfig
 
   CHEF_CONFIG_YAML='/var/lib/aws/opsworks/client.yml'
 
+  def self.default_cookbooks_path
+    YAML.load_file(CHEF_CONFIG_YAML)[:default_cookbooks_path]
+  end
+
   def self.site_cookbooks_path
     YAML.load_file(CHEF_CONFIG_YAML)[:site_cookbooks_path]
+  end
+
+  def self.merged_cookbooks_path
+    YAML.load_file(CHEF_CONFIG_YAML)[:merged_cookbooks_path]
   end
 
   def self.berkshelf_cookbooks_path
--- opsworks_custom_cookbooks/recipes/load.rb
+++ opsworks_custom_cookbooks/recipes/load.rb
@@ -9,3 +9,11 @@ else
   end
 end
 
+ruby_block 'merge all cookbooks sources' do
+  block do
+     FileUtils.rm_rf OpsworksInstanceAgentConfig.merged_cookbooks_path
+     FileUtils.cp_r OpsworksInstanceAgentConfig.default_cookbooks_path, OpsworksInstanceAgentConfig.merged_cookbooks_path
+     FileUtils.cp_r "#{OpsworksInstanceAgentConfig.berkshelf_cookbooks_path}/.", OpsworksInstanceAgentConfig.merged_cookbooks_path if ::File.directory?(OpsworksInstanceAgentConfig.berkshelf_cookbooks_path)
+     FileUtils.cp_r "#{OpsworksInstanceAgentConfig.site_cookbooks_path}/.", OpsworksInstanceAgentConfig.merged_cookbooks_path if ::File.directory?(OpsworksInstanceAgentConfig.site_cookbooks_path)
+  end
+end
