29059f6714accd60cdc329767b05a8002c9b2af4
Christian Requena <requena@amazon.de>
2014-03-27 14:08:18 +0100

    Generate /etc/hosts using search
    cr https://cr.amazon.com/r/2264769/

--- opsworks_stack_state_sync/recipes/hosts.rb
+++ opsworks_stack_state_sync/recipes/hosts.rb
@@ -3,5 +3,8 @@ require 'resolv'
 template '/etc/hosts' do
   source "hosts.erb"
   mode "0644"
-  variables(:opsworks => node[:opsworks])
+  variables(
+    :localhost_name => node[:opsworks][:instance][:hostname],
+    :nodes => search(:node, "name:*")
+  )
 end
