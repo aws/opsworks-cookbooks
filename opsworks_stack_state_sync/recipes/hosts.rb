27ea25c00849fd787c15999930d1542926167eb7
Daniel Hüsch <huesch@amazon.com>
2014-03-27 15:56:25 +0100

    Revert "Generate /etc/hosts using search"
    This reverts commit d438e557986e64fe1a0b994b2674d61c77b3b6ed.

--- opsworks_stack_state_sync/recipes/hosts.rb
+++ opsworks_stack_state_sync/recipes/hosts.rb
@@ -3,7 +3,5 @@ require 'resolv'
 template '/etc/hosts' do
   source "hosts.erb"
   mode "0644"
-  variables(:localhost_name => node[:opsworks][:instance][:hostname],
-            :nodes => nodes
-           )
+  variables(:opsworks => node[:opsworks])
 end
