368d1b5e8009318f928fe64e5afdfed25485e522
Udo Juettner <udoj@amazon.com>
2014-04-07 17:27:48 +0200

    Remove logcheck/logtail, as it's actually not used - and logcheck installation fails on Amazon Linux 2014.03.
    Conflicts:
    	opsworks_ganglia/recipes/monitor-apache.rb

--- opsworks_ganglia/specs/monitor-apache_spec.rb
+++ opsworks_ganglia/specs/monitor-apache_spec.rb
@@ -4,15 +4,6 @@ describe_recipe 'opsworks_ganglia::monitor-apache' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
-  it 'installs logtail dep' do
-    case node[:platform]
-    when 'centos','redhat','fedora','amazon'
-      package('logcheck').must_be_installed
-    when 'debian','ubuntu'
-      package('logtail').must_be_installed
-    end
-  end
-
   it 'creates /etc/ganglia/conf.d/apache.pyconf' do
     file('/etc/ganglia/conf.d/apache.pyconf').must_exist.with(:mode, '644')
   end
