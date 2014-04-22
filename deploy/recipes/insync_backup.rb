80b7f9b3e75aefe03f3968b1bc5fb74cce23ed9b
kachan777 <katsumata@enrise-corp.co.jp>
2014-04-22 19:52:46 +0900

    modified insync_backup.rb(chmod 700)

--- deploy/recipes/insync_backup.rb
+++ deploy/recipes/insync_backup.rb
@@ -7,6 +7,7 @@ bash "insync backup" do
 user 'root'
 group 'root'
 code <<-EOC
+chmod 700 /opt/aws/opsworks/current/site-cookbooks/backup_script/backup.sh
 /opt/aws/opsworks/current/site-cookbooks/backup_script/backup.sh
 EOC
 end
