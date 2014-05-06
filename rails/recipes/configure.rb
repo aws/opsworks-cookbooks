d2c78361372ee3c2f0b6f0fce2961c766cd819ab
Trevor Lischka <tlischka@amazon.de>
2014-05-06 15:02:01 +0200

    changed rails cookbook to map adapter=mysql to adapter=mysql2 if using RDS

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -9,16 +9,17 @@ node[:deploy].each do |application, deploy|
     action :nothing
   end
 
-  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
-  deploy = node[:deploy][application]
-
   template "#{deploy[:deploy_to]}/shared/config/database.yml" do
     source "database.yml.erb"
     cookbook 'rails'
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
-    variables(:database => deploy[:database], :environment => deploy[:rails_env])
+    variables(
+      :database => database_info,
+      :environment => deploy[:rails_env],
+      :database_adapter => OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
+    )
 
     notifies :run, "execute[restart Rails app #{application}]"
 
