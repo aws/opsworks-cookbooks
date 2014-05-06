81dfc793869c75508198ae895c47f5984955272b
Trevor Lischka <tlischka@amazon.de>
2014-05-06 15:26:32 +0200

    added attribute to deploy cookbook to set a default for :data_source_provider in each application's database section

--- rails/libraries/rails_configuration.rb
+++ rails/libraries/rails_configuration.rb
@@ -1,6 +1,5 @@
+module OpsWorks
   module RailsConfiguration
-    
-
     def self.determine_database_adapter(app_name, app_config, app_root_path, options = {})
       options = {
         :consult_gemfile => true,
