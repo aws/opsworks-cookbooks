621755b82e770b0132e5967b840ef9a3c6c9dbdc
Daniel Hüsch <huesch@amazon.com>
2014-05-14 16:34:36 +0200

    Revert "added attribute to deploy cookbook to set a default for"
    This reverts commit 804d24d19d2650745f0ea156fc844fba870974c9.

--- rails/libraries/rails_configuration.rb
+++ rails/libraries/rails_configuration.rb
@@ -1,5 +1,6 @@
-module OpsWorks
   module RailsConfiguration
+    
+
     def self.determine_database_adapter(app_name, app_config, app_root_path, options = {})
       options = {
         :consult_gemfile => true,
@@ -25,20 +26,20 @@ module OpsWorks
       if options[:consult_gemfile] and File.exists?("#{app_root_path}/Gemfile")
         bundle_list = `cd #{app_root_path}; /usr/local/bin/bundle list`
         adapter = if bundle_list.include?('mysql2')
-                    Chef::Log.info("Looks like #{app_name} uses mysql2 in its Gemfile")
-                    'mysql2'
-                  else
-                    Chef::Log.info("Gem mysql2 not found in the Gemfile of #{app_name}, defaulting to mysql")
-                    'mysql'
-                  end
+          Chef::Log.info("Looks like #{app_name} uses mysql2 in its Gemfile")
+          'mysql2'
+        else
+          Chef::Log.info("Gem mysql2 not found in the Gemfile of #{app_name}, defaulting to mysql")
+          'mysql'
+        end
       else # no Gemfile - guess adapter by Rails version
         adapter = if File.exists?("#{app_root_path}/config/application.rb")
-                    Chef::Log.info("Looks like #{app_name} is a Rails 3 application, defaulting to mysql2")
-                    'mysql2'
-                  else
-                    Chef::Log.info("No config/application.rb found, assuming #{app_name} is a Rails 2 application, defaulting to mysql")
-                    'mysql'
-                  end
+          Chef::Log.info("Looks like #{app_name} is a Rails 3 application, defaulting to mysql2")
+          'mysql2'
+        else
+          Chef::Log.info("No config/application.rb found, assuming #{app_name} is a Rails 2 application, defaulting to mysql")
+          'mysql'
+        end
       end
 
       adapter
