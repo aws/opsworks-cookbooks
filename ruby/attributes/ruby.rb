939c431a55857d127f4bc7cbfee189ddbd2cd2a2
Udo Juettner <udoj@amazon.com>
2014-05-22 10:28:11 +0200

    Default to Ruby 2.1.2 as packages are now available.

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -34,7 +34,7 @@ case node["opsworks"]["ruby_version"]
 when "2.1"
   default[:ruby][:major_version] = '2'
   default[:ruby][:minor_version] = '1'
-  default[:ruby][:patch_version] = '1'
+  default[:ruby][:patch_version] = '2'
   default[:ruby][:pkgrelease]    = '1'
 
   i = node[:ruby]
