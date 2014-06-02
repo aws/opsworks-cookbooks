fa142006a59006fc367418a1b235e6c1aaf45c9d
ryancarlson <ryan.t.carlson@cru.org>
2014-06-02 14:47:04 -0400

    add --silent tag to get around console warning

--- wildfly/recipes/add_deploy_user.rb
+++ wildfly/recipes/add_deploy_user.rb
@@ -1,4 +1,4 @@
 execute "add user" do
   user "root"
-  command 'sudo /opt/wildfly-' + node['wildfly']['version'] + '/bin/add-user.sh ' + node['wildfly']['deploy_username'] + ' ' + node['wildfly']['deploy_password']
+  command '/opt/wildfly-' + node['wildfly']['version'] + '/bin/add-user.sh --silent ' + node['wildfly']['deploy_username'] + ' ' + node['wildfly']['deploy_password']
 end
