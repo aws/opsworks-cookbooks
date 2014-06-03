f8b61742ff9b09cc373df84c3205de87b0eb46a5
ryancarlson <ryan.t.carlson@cru.org>
2014-06-03 17:18:20 -0400

    Updates to godtools_api recipes  - removing deploy_app script  - removing user script  - changing all dashes to underscores  - updating install_properties recipe to:    - access the appropriate variables    - do things under the wildfly user, not godtools user

--- godtools_api/attributes/default.rb
+++ godtools_api/attributes/default.rb
@@ -1,14 +1,6 @@
-default['godtools']['user'] = 'godtools'
-default['godtools']['group'] = 'godtools'
-
 default['godtools']['version'] = '1.0.0-SNAPSHOT'
 
-default['godtools']['database-url'] = 'jdbc:postgresql://10.7.196.222/crs_dev'
-default['godtools']['database-username'] = 'crsuser'
+default['godtools']['database_url'] = 'jdbc:postgresql://10.7.196.222/godtools'
+default['godtools']['database_username'] = 'godtoolsuser'
 
-default['godtools']['mail-server'] = 'email-smtp.us-east-1.amazonaws.com'
-default['godtools']['mail-server-username'] = 'AKIAJEGKFOK7SWWYQRQA'
-
-default['godtools']['errbit-endpoint'] = 'https://errors.uscm.org/notifier_api/v2/notices'
-default['godtools']['errbit-environment'] = 'production'
-
+default['godtools']['onesky_api_key'] = 'JSlA3a3tTcixcpEegcympv3HQCJB7q1H'
--- godtools_api/recipes/install_properties.rb
+++ godtools_api/recipes/install_properties.rb
@@ -1,27 +1,22 @@
 directory '/apps' do
-  owner node['crs-api']['user']
-  group node['crs-api']['group']
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
   mode 0755
   action :create
 end
 
 directory '/apps/apps-config' do
-  owner node['crs-api']['user']
-  group node['crs-api']['group']
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
   mode 0755
   action :create
 end
 
-template '/apps/apps-config/conf-registration-api-properties.xml' do
-  owner node['crs-api']['user']
-  group node['crs-api']['group']
+template '/apps/apps-config/godtools-api-properties.xml' do
+  owner node['wildfly']['user']
+  group node['wildfly']['group']
   mode 0744
   backup false
-  source 'conf_registration_api_properties.xml.erb'
+  source 'godtools_api_properties.xml.erb'
   action :create
-  
-end
-
-execute 'restart service' do
-  command 'sudo service ' + node['crs-api']['service-name'] + ' restart'
 end
