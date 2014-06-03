d408de3ba7836751e7dd0885bc85bfba20b252ff
ryancarlson <ryan.t.carlson@cru.org>
2014-06-03 17:34:56 -0400

    remove user/group altogether to see how that works

--- godtools_api/recipes/install_properties.rb
+++ godtools_api/recipes/install_properties.rb
@@ -1,20 +1,14 @@
 directory '/apps' do
-  owner node['wildfly']['user']
-  group node['wildfly']['group']
   mode 0755
   action :create
 end
 
 directory '/apps/apps-config' do
-  owner node['wildfly']['user']
-  group node['wildfly']['group']
   mode 0755
   action :create
 end
 
 template '/apps/apps-config/godtools-api-properties.xml' do
-  owner node['wildfly']['user']
-  group node['wildfly']['group']
   mode 0744
   backup false
   source 'godtools_api_properties.xml.erb'
