4258107546da2b01aa2a883bdce22c74a5e45bee
Udo Juettner <udoj@amazon.com>
2014-06-12 19:57:25 +0200

    Use Ganglia OS packages on Ubuntu 14.04 and adapt daemon control.
    Use value_for_platform where possible and make check for Ubuntu version more explicit.
    
    Switch from initctl to service for daemon control.

--- opsworks_ganglia/recipes/service-gmetad.rb
+++ opsworks_ganglia/recipes/service-gmetad.rb
@@ -1,4 +1,9 @@
 service 'gmetad' do
+  provider value_for_platform(
+    'ubuntu' => {
+      '14.04' => Chef::Provider::Service::Upstart
+    }
+  )
   supports :status => false, :restart => true
   action :nothing
 end
