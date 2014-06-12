e8e2497719fa0bb75768cd7f566275807ed3c6bd
Udo Juettner <udoj@amazon.com>
2014-06-12 18:14:40 +0200

    Set default patchlevel for Ruby 1.9.3 to p547.

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -40,7 +40,7 @@ when "2.0.0"
 when "1.9.3"
   default[:ruby][:major_version] = '1'
   default[:ruby][:minor_version] = '9'
-  default[:ruby][:patch] = 'p545'  # this attribute will disapier in favor of the sematic versioning schema
+  default[:ruby][:patch] = 'p547'  # this attribute will disappear in favor of the sematic versioning schema
   default[:ruby][:patch_version] = node[:ruby][:patch]
   default[:ruby][:pkgrelease] = '1'
 
