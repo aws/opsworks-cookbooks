f8b17a3cca2cedac834a655dcd5da5bc766803e1
Udo Juettner <udoj@amazon.com>
2014-06-16 16:04:35 +0200

    Fix gmetad service test for non-default provider upstart.

--- opsworks_ganglia/specs/server_spec.rb
+++ opsworks_ganglia/specs/server_spec.rb
@@ -22,7 +22,15 @@ describe_recipe 'opsworks_ganglia::server' do
   end
 
   it 'starts and enables gmetad' do
-    service('gmetad').must_be_running
-    service('gmetad').must_be_enabled
+    if node[:platform] == 'ubuntu' && node[:platform_version] == '14.04'
+      service_resource = Chef::Resource::Service.new('gmetad', @run_context)
+      service_resource.provider Chef::Provider::Service::Upstart
+      service = service_resource.provider_for_action(:start).load_current_resource
+      service.running.must_equal true
+      service.enabled.must_equal true
+    else
+      service('gmetad').must_be_running
+      service('gmetad').must_be_enabled
+    end
   end
 end
