dd27a9953ef1191e84ccbb2f547f9fcc518c027a
Udo Juettner <udoj@amazon.com>
2014-06-17 16:20:57 +0200

    Fix test when no CA information is given.
    Move three SSL file-related tests into one.

--- mod_php5_apache2/specs/php_spec.rb
+++ mod_php5_apache2/specs/php_spec.rb
@@ -4,26 +4,12 @@ describe_recipe 'mod_php5_apache2::php' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
-  it "creates the application's certificate" do
+  it "creates the application's SSL files" do
     node[:deploy].each do |application, deploy|
       if deploy[:application_type] == 'php' && deploy[:ssl_support]
         file("#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.crt").must_exist.with(:mode, '0600')
-      end
-    end
-  end
-
-  it "creates the application's certificate key" do
-    node[:deploy].each do |application, deploy|
-      if deploy[:application_type] == 'php' && deploy[:ssl_support]
         file("#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.key").must_exist.with(:mode, '0600')
-      end
-    end
-  end
-
-  it "creates the application's certificate chain file" do
-    node[:deploy].each do |application, deploy|
-      if deploy[:application_type] == 'php' && deploy[:ssl_support]
-        file("#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.ca").must_exist.with(:mode, '0600')
+        file("#{node[:apache][:dir]}/ssl/#{deploy[:domains].first}.ca").must_exist.with(:mode, '0600') if deploy[:ssl_certificate_ca]
       end
     end
   end
