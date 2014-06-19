dc06ad797bad0e1f2faaa1229a56a5a3c0061525
Jonathan Weiss <joweiss@amazon.com>
2014-06-19 18:27:23 +0200

    Bundle aws-flow-1.0.10 until it is released

--- opsworks_aws_flow_ruby/recipes/setup.rb
+++ opsworks_aws_flow_ruby/recipes/setup.rb
@@ -1,4 +1,13 @@
-gem_package 'aws-flow' do
-  source node['opsworks_aws_flow_ruby']['gem_source'] if node['opsworks_aws_flow_ruby']['gem_source']
-  version node['opsworks_aws_flow_ruby']['version']
+#gem_package 'aws-flow' do
+#  source node['opsworks_aws_flow_ruby']['gem_source'] if node['opsworks_aws_flow_ruby']['gem_source']
+#  version node['opsworks_aws_flow_ruby']['version']
+#end
+
+cookbook_file "aws-flow-1.0.10.gem" do
+  path "/tmp/aws-flow-1.0.10.gem"
+end
+
+
+execute "Install unreleased aws-flow gem" do
+  command "gem install /tmp/aws-flow-1.0.10.gem"
 end
