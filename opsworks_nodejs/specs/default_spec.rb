9fbb3534c71e849820c122becfb0b661a37f9b6f
Christian Requena <requena@amazon.de>
2014-06-19 15:01:54 +0200

    Use opsworks_commons_assets_installer to manage installation of NodeJs packages.
    cr https://cr.amazon.com/r/2540017/

--- opsworks_nodejs/specs/default_spec.rb
+++ opsworks_nodejs/specs/default_spec.rb
@@ -4,15 +4,6 @@ describe_recipe 'opsworks_nodejs::default' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
-  it 'deletes downloaded packages' do
-    case node[:platform]
-    when'debian','ubuntu'
-      file(File.join('/tmp', node[:opsworks_nodejs][:deb])).wont_exist
-    when 'centos','redhat','fedora','amazon'
-      file(File.join('/tmp', node[:opsworks_nodejs][:rpm])).wont_exist
-    end
-  end
-
   it 'access the right node executable from the default path' do
      (`which node`).chomp.must_equal("/usr/local/bin/node")
   end
