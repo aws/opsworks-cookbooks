5ee3c352435318a14ebc53c8542008ed5097e86b
Christian Requena <requena@amazon.de>
2014-06-23 16:52:09 +0200

    Handle removal of berkshelf gems in one command and remove unnecessary code.

--- opsworks_custom_cookbooks/libraries/single_gem_version.rb
+++ opsworks_custom_cookbooks/libraries/single_gem_version.rb
@@ -1,8 +1,13 @@
+#
+# DEPRECATED Module
+# this code will be removed on the next agent version.
+
 module OpsWorks
   module GemSupport
     require 'rubygems/version'
 
     def uninstall_other_gem_versions(name, ensured_version)
+      Chef::Log.warn("Using deprecated method OpsWorks::GemSupport.uninstall_other_gem_versions. This method will be removed on the next agent version.")
       versions = `#{node['opsworks_custom_cookbooks']['gem_binary']} list #{name}`
       versions = versions.scan(/(\d[a-zA-Z0-9\.]*)/).flatten.compact
       for version in versions
