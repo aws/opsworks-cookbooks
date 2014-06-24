e30bc8ad61739b29449e07e0651cbf22060e808e
Christian Requena <requena@amazon.de>
2014-06-24 18:02:10 +0200

    Improve the design of cookbooks for deploying rails applications to fix RDS support
    cr https://cr.amazon.com/r/2567970/

--- deploy/definitions/opsworks_rails.rb
+++ deploy/definitions/opsworks_rails.rb
@@ -1,21 +1,15 @@
 define :opsworks_rails do
   deploy = params[:deploy_data]
-  application = params[:app]
 
   include_recipe node[:opsworks][:rails_stack][:recipe]
 
-  # write out memcached.yml
-  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-    cookbook "rails"
-    source "memcached.yml.erb"
-    mode "0660"
-    owner deploy[:user]
-    group deploy[:group]
-    variables(:memcached => (deploy[:memcached] || {}), :environment => deploy[:rails_env])
-
-    only_if do
-      deploy[:memcached][:host].present?
-    end
+  rails_install_dependencies "Install database dependencies for rails application #{params[:app].inspect}" do
+    database_adapter OpsWorks::RailsConfiguration.determine_database_adapter(
+      params[:app],
+      deploy,
+      "#{deploy[:deploy_to]}/current",
+      :force => node[:force_database_adapter_detection]
+    )
   end
 
   execute "symlinking subdir mount if necessary" do
@@ -25,5 +19,4 @@ define :opsworks_rails do
       deploy[:mounted_at] && File.exists?("/var/www")
     end
   end
-
-end 
+end
--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -1,54 +1,27 @@
 include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
 
-  execute "restart Rails app #{application}" do
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
-  end
+  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(
+    application,
+    deploy,
+    "#{deploy[:deploy_to]}/current",
+    :force => node[:force_database_adapter_detection],
+    :consult_gemfile => deploy[:auto_bundle_on_deploy]
+  )
 
-  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
-  deploy = node[:deploy][application]
+  deploy = node[:deploy][application] # update the value, as a key was just added.
 
-  case node[:deploy][application][:database][:adapter]
-  when /mysql/
-    #include_recipe "mysql::client"
-  when "postgresql"
-    include_recipe "opsworks_postgresql::client_install"
-  end
-
-  template "#{deploy[:deploy_to]}/shared/config/database.yml" do
-    source "database.yml.erb"
-    cookbook 'rails'
-    mode "0660"
+  rails_configuration "Update opsworks configration for app #{application.inspect} and restart rails application stack" do
+    application application
+    deploy_to deploy[:deploy_to]
+    rails_env deploy[:rails_env]
+    user deploy[:user]
     group deploy[:group]
-    owner deploy[:user]
-    variables(:database => deploy[:database], :environment => deploy[:rails_env])
+    database_data deploy[:database]
+    memcached_data deploy[:memcached] || {}
 
-    notifies :run, "execute[restart Rails app #{application}]"
-
-    only_if do
-      deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-    end
+    restart true
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-    source "memcached.yml.erb"
-    cookbook 'rails'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(
-      :memcached => deploy[:memcached] || {},
-      :environment => deploy[:rails_env]
-    )
-
-    notifies :run, "execute[restart Rails app #{application}]"
-
-    only_if do
-      deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-    end
-  end
 end
