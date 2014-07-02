59816681f4ad2176b82eac6f534c34079debab5a
Jens Braeuer <braeuer@amazon.com>
2014-07-02 13:05:12 +0200

    Delete trailing whitespace.

--- opsworks_custom_cookbooks/recipes/default.rb
+++ opsworks_custom_cookbooks/recipes/default.rb
@@ -6,10 +6,10 @@ ruby_block "load and execute the new cookbooks" do
   block do
     if ::File.directory?(node[:opsworks_custom_cookbooks][:destination])
       Chef::Log.info("Loading custom cookbooks from #{node[:opsworks_custom_cookbooks][:destination]}")
-      
+
       # add new Cookbooks and keep old
       Chef::Config.cookbook_path = [Chef::Config.cookbook_path, node[:opsworks_custom_cookbooks][:destination]].flatten
-      
+
       load_new_cookbooks
 
       node[:opsworks_custom_cookbooks][:recipes].each do |r|
@@ -23,7 +23,7 @@ ruby_block "load and execute the new cookbooks" do
       end
     end
   end
-  
+
   only_if do
     Chef::Log.info("Loading and executing custom cookbooks") if node[:opsworks_custom_cookbooks][:enabled]
     node[:opsworks_custom_cookbooks][:enabled]
