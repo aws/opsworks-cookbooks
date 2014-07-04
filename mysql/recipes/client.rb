0a2f1f4b57f66895bf2e944552ebfd95e0e6b8be
Eike Bernhardt <eike@unorganized.net>
2014-07-04 12:29:33 +0200

    Check if rds_instances is nil?
    If running this recipe locally without a defined database layer, node[:opsworks][:stack][:rds_instances] might be nil

--- mysql/recipes/client.rb
+++ mysql/recipes/client.rb
@@ -1,4 +1,4 @@
-if node[:opsworks][:layers].has_key?('db-master') || (node[:opsworks][:stack][:rds_instances].any?{|rds_instance| rds_instance[:engine] == 'mysql'})
+if node[:opsworks][:layers].has_key?('db-master') || (!node[:opsworks][:stack][:rds_instances].nil? && node[:opsworks][:stack][:rds_instances].any?{|rds_instance| rds_instance[:engine] == 'mysql'})
   Chef::Log.info 'Detected a db-master layer or at least one MySQL RDS DB instance - making sure the MySQL client is installed'
   include_recipe 'mysql::client_install'
 else
