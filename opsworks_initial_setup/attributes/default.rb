d858fce2849ff48169987adbead2532908d85570
Jens Braeuer <braeuer@amazon.com>
2014-07-10 14:24:24 +0200

    Enable swap on t1.micro and t2.micro.

--- opsworks_initial_setup/attributes/default.rb
+++ opsworks_initial_setup/attributes/default.rb
@@ -28,6 +28,7 @@ default[:opsworks][:ruby_stack] = 'ruby_enterprise'
 default[:opsworks][:ruby_version] = '1.9.3'
 default[:opsworks][:run_cookbook_tests] = false
 
+default['opsworks_initial_setup']['swapfile_instancetypes'] = [ 't1.micro', 't2.micro' ]
 default['opsworks_initial_setup']['swapfile_name'] = '/var/swapfile'
 default['opsworks_initial_setup']['swapfile_size_mb'] = 256
 
