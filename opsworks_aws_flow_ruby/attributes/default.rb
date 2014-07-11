1c3b7b1df7e5ce33fd4e0505d4322edb57357bc5
Jonathan Weiss <joweiss@amazon.com>
2014-07-11 19:25:11 +0200

    Also write user agent prefix

--- opsworks_aws_flow_ruby/attributes/default.rb
+++ opsworks_aws_flow_ruby/attributes/default.rb
@@ -2,3 +2,4 @@ default['opsworks_aws_flow_ruby'] = {}
 default['opsworks_aws_flow_ruby']['user'] = 'deploy'
 default['opsworks_aws_flow_ruby']['group'] = 'deploy'
 default['opsworks_aws_flow_ruby']['version'] = '1.3.0'
+default['opsworks_aws_flow_ruby']['user_agent_prefix'] = 'ruby-flow-opsworks'
