7aaaff5ad99bb1217b780633cc71fa533faa9a55
Jonathan Weiss <joweiss@amazon.com>
2014-07-17 10:57:45 +0200

    Remove debug and make AWS Flow Ruby configure work

--- deploy/recipes/aws-flow-ruby.rb
+++ deploy/recipes/aws-flow-ruby.rb
@@ -1,17 +1,12 @@
 include_recipe 'deploy'
 require 'json'
 
-Chef::Log.info("I am a message from the #{recipe_name} recipe in the #{cookbook_name} cookbook.")
-
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'aws-flow-ruby'
     Chef::Log.info("Skipping deploy::aws-flow-ruby application #{application} as it is not an AWS Flow Ruby app")
     next
   end
 
-  Chef::Log.info("Doing deploy::aws-flow-ruby application #{application}")
-
-  # create the initrc and monit files
   opsworks_aws_flow_ruby do
     deploy_data deploy
     app application
--- opsworks_aws_flow_ruby/recipes/configure.rb
+++ opsworks_aws_flow_ruby/recipes/configure.rb
@@ -1,1 +1,23 @@
-# Do nothing
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'aws-flow-ruby'
+    Chef::Log.info("Skipping deploy::aws-flow-ruby application #{application} as it is not an AWS Flow Ruby app")
+    next
+  end
+
+  file "#{deploy[:deploy_to]}/current/runner_config.json" do
+    user deploy[:user]
+    group deploy[:group]
+    content "#{JSON.pretty_generate((deploy[:aws_flow_ruby_settings] || {}).dup.update('user_agent_prefix' => node.default['opsworks_aws_flow_ruby']['user_agent_prefix'])})"
+    notifies :run, "ruby_block[restart AWS Flow Ruby application #{application}]"
+  end
+
+  ruby_block "restart AWS Flow Ruby application #{application}" do
+    action :nothing
+    block do
+      Chef::Log.info("restart AWS Flow Ruby via: #{node[:deploy][application][:aws_flow_ruby][:restart_command]}")
+      Chef::Log.info(`#{node[:deploy][application][:aws_flow_ruby][:restart_command]}`)
+      $? == 0
+    end
+  end
+
+end
