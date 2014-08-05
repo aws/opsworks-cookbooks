8adc6cc713885703eff68285ba2c22b1d1bd123f
Jonathan Weiss <joweiss@amazon.com>
2014-08-05 15:33:47 +0200

    Revert renaming of namespace

--- opsworks_aws_flow_ruby/attributes/default.rb
+++ opsworks_aws_flow_ruby/attributes/default.rb
@@ -1,4 +1,7 @@
-default['opsworks']['aws_flow_ruby']['user'] = 'deploy'
-default['opsworks']['aws_flow_ruby']['group'] = 'deploy'
-default['opsworks']['aws_flow_ruby']['version'] = '1.3.0'
-default['opsworks']['aws_flow_ruby']['user_agent_prefix'] = 'ruby-flow-opsworks'
+default['opsworks_aws_flow_ruby']['user'] = 'deploy'
+default['opsworks_aws_flow_ruby']['group'] = 'deploy'
+default['opsworks_aws_flow_ruby']['version'] = '1.3.0'
+default['opsworks_aws_flow_ruby']['user_agent_prefix'] = 'ruby-flow-opsworks'
+
+
+#"aws_flow_ruby_gem_version"=>"1.3.0"}
--- opsworks_aws_flow_ruby/recipes/setup.rb
+++ opsworks_aws_flow_ruby/recipes/setup.rb
@@ -1,3 +1,3 @@
 gem_package 'aws-flow' do
-  version node['opsworks']['aws_flow_ruby']['version']
+  version node['opsworks_aws_flow_ruby']['version']
 end
