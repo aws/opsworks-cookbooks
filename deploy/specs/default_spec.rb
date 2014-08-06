fd5e8039f598bc93adc20ec9adf43f60d32f5247
Florian Eitel <eitel@amazon.com>
2014-08-05 22:05:21 -0700

    fixed cookbook tests for using root as deploy user
    cr https://cr.amazon.com/r/2704262/

--- deploy/specs/default_spec.rb
+++ deploy/specs/default_spec.rb
@@ -7,9 +7,14 @@ describe_recipe 'deploy::default' do
   it 'creates users defined for each application with the right information' do
     node[:deploy].each do |application, deploy|
       user(deploy[:user]).must_exist
-      user(deploy[:user]).must_have(:home, deploy[:home])
+      if deploy[:user] == "root"
+        user(deploy[:user]).must_have(:home, "/root")
+        user(deploy[:user]).must_have(:comment, "root")
+      else
+        user(deploy[:user]).must_have(:home, deploy[:home])
+        user(deploy[:user]).must_have(:comment, "deploy user")
+      end
       user(deploy[:user]).must_have(:shell, deploy[:shell])
-      user(deploy[:user]).must_have(:comment, "deploy user")
 
       # NOTE: Why doesn't this work?
       # Because :gid compares against the numerical GID, not the
