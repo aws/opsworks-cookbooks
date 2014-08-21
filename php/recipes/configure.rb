78ebcb5d743624ef034793591455766328223e24
Christian Requena <requena@amazon.de>
2014-08-21 15:36:02 +0200

    Merge pull request #176 from pixelcog/master-chef-11.10-phpfix
    Fixes for PHP Cookbook

--- php/recipes/configure.rb
+++ php/recipes/configure.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'php'
-    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    Chef::Log.debug("Skipping php::configure application #{application} as it is not an PHP app")
     next
   end
 
