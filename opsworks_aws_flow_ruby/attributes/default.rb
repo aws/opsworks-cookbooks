cc213d0af20c8f201d22590da9bc274546aa2561
Thomas Metschke <metschke@amazon.com>
2014-08-27 16:32:06 +0200

    Remove unused attributes

--- opsworks_aws_flow_ruby/attributes/default.rb
+++ opsworks_aws_flow_ruby/attributes/default.rb
@@ -1,3 +1,1 @@
-default['opsworks_aws_flow_ruby']['user'] = 'deploy'
-default['opsworks_aws_flow_ruby']['group'] = 'deploy'
 default['opsworks_aws_flow_ruby']['user_agent_prefix'] = 'ruby-flow-opsworks'
