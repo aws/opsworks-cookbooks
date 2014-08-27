48540a6a039d488274708a312dcb72ab71c16fe9
Thomas Metschke <metschke@amazon.com>
2014-08-27 10:53:30 +0200

    remove gemversion and installation of aws flow ruby gem

--- opsworks_aws_flow_ruby/attributes/default.rb
+++ opsworks_aws_flow_ruby/attributes/default.rb
@@ -1,4 +1,3 @@
 default['opsworks_aws_flow_ruby']['user'] = 'deploy'
 default['opsworks_aws_flow_ruby']['group'] = 'deploy'
-default['opsworks_aws_flow_ruby']['version'] = node['opsworks']['aws_flow_ruby_gem_version'] || '1.3.0'
 default['opsworks_aws_flow_ruby']['user_agent_prefix'] = 'ruby-flow-opsworks'
