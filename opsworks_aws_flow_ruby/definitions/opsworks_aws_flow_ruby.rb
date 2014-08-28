734a238f0cf7c729dc958b0838ccb8fa65375ca5
Pieter de Bie <pdebie@amazon.com>
2014-08-28 15:34:04 +0200

    Flow: Move gem version check into ruby block

--- opsworks_aws_flow_ruby/definitions/opsworks_aws_flow_ruby.rb
+++ opsworks_aws_flow_ruby/definitions/opsworks_aws_flow_ruby.rb
@@ -19,24 +19,6 @@ define :opsworks_aws_flow_ruby do
     )
   end
 
-  # Check for Gemfile
-  unless File.exists?("#{deploy_dir}/Gemfile.lock")
-    raise "No Gemfile found. You need a Gemfile to use AWS Flow (Ruby)"
-  end
-
-  bundle_output = OpsWorks::ShellOut.shellout("/usr/local/bin/bundle list", :cwd => deploy_dir)
-  Chef::Log.info bundle_output
-
-  version_match = bundle_output.match(/ aws-flow \((.*)\)$/)
-  unless version_match
-    raise "Gem 'aws-flow' needs to be added to your Gemfile"
-  end
-
-  minimum_version = node['opsworks_aws_flow_ruby']['minimum_flow_gem_version']
-  if Gem::Version.new(version_match[1]) < Gem::Version.new(minimum_version)
-    raise "Gem 'aws-flow' needs to be version #{minimum_version} or higher."
-  end
-
   file "#{deploy_dir}/runner_config.json" do
     user deploy[:user]
     group deploy[:group]
@@ -57,6 +39,24 @@ define :opsworks_aws_flow_ruby do
   ruby_block "restart AWS Flow Ruby application #{application}" do
     action :nothing
     block do
+      # Check for Gemfile
+      unless File.exists?("#{deploy_dir}/Gemfile.lock")
+        raise "No Gemfile found. You need a Gemfile to use AWS Flow (Ruby)"
+      end
+
+      bundle_output = OpsWorks::ShellOut.shellout("/usr/local/bin/bundle list", :cwd => deploy_dir)
+      Chef::Log.info bundle_output
+
+      version_match = bundle_output.match(/ aws-flow \((.*)\)$/)
+      unless version_match
+        raise "Gem 'aws-flow' needs to be added to your Gemfile"
+      end
+
+      minimum_version = node['opsworks_aws_flow_ruby']['minimum_flow_gem_version']
+      if Gem::Version.new(version_match[1]) < Gem::Version.new(minimum_version)
+        raise "Gem 'aws-flow' needs to be version #{minimum_version} or higher."
+      end
+
       Chef::Log.info OpsWorks::ShellOut.shellout("#{deploy_dir}/runner.initrc restart")
     end
   end
