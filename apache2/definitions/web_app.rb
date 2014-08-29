57279441eb82dfdf8fb237fe9ffd6194386e471b
Florian Eitel <eitel@amazon.com>
2014-08-29 18:01:07 +0200

    Check if web_app has a application to deploy before access env vars.
    Ganglia master will fail on setup without the check

--- apache2/definitions/web_app.rb
+++ apache2/definitions/web_app.rb
@@ -39,10 +39,16 @@ define :web_app, :template => 'web_app.conf.erb' do
     if params[:cookbook]
       cookbook params[:cookbook]
     end
+
+    environment_variables = if node[:deploy][application_name].nil?
+                              {}
+                            else
+                              node[:deploy][application_name][:environment_variables]
+                            end
     variables(
       :application_name => application_name,
       :params => params,
-      :environment => OpsWorks::Escape.escape_double_quotes(node[:deploy][application_name][:environment_variables])
+      :environment => OpsWorks::Escape.escape_double_quotes(environment_variables)
     )
     if ::File.exists?("#{node[:apache][:dir]}/sites-enabled/#{application_name}.conf")
       notifies :reload, "service[apache2]", :delayed
