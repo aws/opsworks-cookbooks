68efbbf52f98311a5fcf0e9d9a3dca60d76fa378
Jonathan Weiss <joweiss@amazon.com>
2014-09-09 00:57:09 +0200

    Some fixes for PHP deployment and DB handling:
    * Skip non-php apps when installing the PHP db drivers
    * Fallback to MySQL driver
    * Handle apps with no database config

--- mod_php5_apache2/recipes/default.rb
+++ mod_php5_apache2/recipes/default.rb
@@ -8,11 +8,17 @@ node[:mod_php5_apache2][:packages].each do |pkg|
 end
 
 node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+  next if node[:deploy][application][:database].nil?
+
   case node[:deploy][application][:database][:type]
-  when "mysql"
-    include_recipe 'mod_php5_apache2::mysql_adapter'
   when "postgresql"
     include_recipe 'mod_php5_apache2::postgresql_adapter'
+  else # mysql or just backwards compatible
+    include_recipe 'mod_php5_apache2::mysql_adapter'
   end
 end
 
