ff867ff2d5fbf77b7a4a85827c92fc26d28dd107
Michael Gruenewald <gruenem@amazon.de>
2014-10-09 16:23:36 +0200

    Don't try to mount EBS volumes if they are not available.

--- opsworks_initial_setup/attributes/default.rb
+++ opsworks_initial_setup/attributes/default.rb
@@ -62,7 +62,7 @@ default[:opsworks_initial_setup][:limits][:rtprio] = nil
 default[:opsworks_initial_setup][:yum_dump_file] = File.join(Chef::CHEF_ROOT, "chef", "provider", "package", "yum-dump.py")
 default[:opsworks_initial_setup][:yum_dump_lock_timeout] = 120
 
-default[:opsworks_initial_setup][:autofs_map_file] = "/etc/auto.opsworks"
+default[:opsworks_initial_setup][:autofs_map_file] = "/etc/auto.opsworks" if infrastructure_class?('ec2')
 
 case node[:platform]
 when 'redhat','centos','fedora','amazon'
