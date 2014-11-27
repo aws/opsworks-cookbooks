7105334f0012e83a5b252705e0c657fef91beb2c
Oli Young <oliyoung@gmail.com>
2014-11-27 15:14:31 +1030

    Configuring opsworks-resque

--- opsworks-resque/attributes/configure.rb
+++ opsworks-resque/attributes/configure.rb
@@ -1,2 +1,2 @@
-default[:resque][:workers] = ['*']
-default[:resque][:path] = "/srv/www/mailee_staging/current"
+default[:resque][:workers] = { "*" => 1 }
+default[:resque][:path] = "/srv/www/app/current"
