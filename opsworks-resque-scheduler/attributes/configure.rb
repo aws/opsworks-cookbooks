e555e53ba9ede57d38642d191a954db5f7ce3a5a
Oli Young <oliyoung@gmail.com>
2014-11-27 14:23:14 +1030

    Configuring opsworks-resque-scheduler

--- opsworks-resque-scheduler/attributes/configure.rb
+++ opsworks-resque-scheduler/attributes/configure.rb
@@ -1,2 +1,2 @@
-default['resque-scheduler']['path'] = "/srv/www/mailee_staging/current"
-default['resque-scheduler']['rails_env'] = "development"
+default['resque-scheduler']['path'] = "/srv/www/app/current"
+default['resque-scheduler']['rails_env'] = "production"
