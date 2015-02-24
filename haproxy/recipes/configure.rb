a399ce2eda47eeff5fcb3aa176d0450b3dee5061
James M. Greene <james.m.greene@gmail.com>
2015-02-24 05:30:56 -0600

    DRY: Include the "haproxy::service" recipe
    Don't Repeat Yourself: Include the "haproxy::service" recipe here instead of defining it the exact same way in 2 places.

--- haproxy/recipes/configure.rb
+++ haproxy/recipes/configure.rb
@@ -1,7 +1,4 @@
-service "haproxy" do
-  supports :restart => true, :status => true, :reload => true
-  action :nothing # only define so that it can be restarted if the config changed
-end
+include_recipe "haproxy::service"
 
 template "/etc/haproxy/haproxy.cfg" do
   cookbook "haproxy"
