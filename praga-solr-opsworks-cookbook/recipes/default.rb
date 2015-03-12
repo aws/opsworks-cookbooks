8dc9a6f93169c3349a6264e36e3b9005823348ee
Rafael Manoel <rafael.manoel@gmail.com>
2015-03-12 15:35:17 -0300

    adiciona a instalacao do java

--- praga-solr-opsworks-cookbook/recipes/default.rb
+++ praga-solr-opsworks-cookbook/recipes/default.rb
@@ -1,3 +1,4 @@
+include_recipe 'opsworks_java::jvm_install'
 include_recipe 'praga-solr-opsworks-cookbook::solr'
 include_recipe 'praga-solr-opsworks-cookbook::config'
 include_recipe 'praga-solr-opsworks-cookbook::crond'
