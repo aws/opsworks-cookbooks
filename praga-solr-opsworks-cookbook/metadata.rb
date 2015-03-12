86b9f3814f11dfc167a09efa478c7c942b25a783
Rafael Manoel <rafael.manoel@gmail.com>
2015-03-12 15:39:44 -0300

    remove a dependencia

--- praga-solr-opsworks-cookbook/metadata.rb
+++ praga-solr-opsworks-cookbook/metadata.rb
@@ -6,4 +6,3 @@ description      'Installs/Configures praga-solr-opsworks-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'java'
