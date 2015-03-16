cf6d6e465d8831866511b7c0b4202f46cc9c72c6
Florian Eitel <eitel@amazon.com>
2015-03-16 18:26:13 +0100

    Fix cookbook tests for rds postgres + php.

--- mod_php5_apache2/specs/default_spec.rb
+++ mod_php5_apache2/specs/default_spec.rb
@@ -5,8 +5,8 @@ describe_recipe 'mod_php5_apache2::default' do
   include MiniTest::Chef::Assertions
 
   it 'installs a pile of php packages' do
-    case node[:platform]
-    when 'debian', 'ubuntu'
+    case node[:platform_family]
+    when "debian"
       packages = [
         'php5-xsl',
         'php5-curl',
@@ -16,7 +16,6 @@ describe_recipe 'mod_php5_apache2::default' do
         'php5-gd',
         'php5-cli',
         'php5-sasl',
-        'php5-mysql',
         'php5-mcrypt',
         'php5-memcache',
         'php-pear',
@@ -27,7 +26,16 @@ describe_recipe 'mod_php5_apache2::default' do
         'php-html-common'
       ]
 
-    when 'redhat', 'centos', 'fedora', 'amazon'
+      node[:deploy].each do |_, deploy|
+        next if deploy[:application_type] != "php" || deploy[:database][:type].nil?
+        if deploy[:database][:type] == "postgresql"
+          packages << "php5-pgsql"
+        else
+          packages << "php5-mysql"
+        end
+      end
+
+    when "rhel"
       packages = [
         'php-xml',
         'php-common',
@@ -36,15 +44,24 @@ describe_recipe 'mod_php5_apache2::default' do
         'php-gd',
         'php-cli',
         'php-pear-Auth-SASL',
-        'php-mysql',
         'php-mcrypt',
         'php-pecl-memcache',
         'php-pear',
         'php-pear-XML-Parser',
         'php-pear-Mail-Mime',
         'php-pear-DB',
-       'php-pear-HTML-Common'
+        'php-pear-HTML-Common'
       ]
+
+      node[:deploy].each do |_, deploy|
+        next if deploy[:application_type] != "php" || deploy[:database][:type].nil?
+        if deploy[:database][:type] == "postgresql"
+          packages << "php-pgsql"
+        else
+          packages << "php-mysql"
+        end
+      end
+
     end
 
     packages.each do |pkg|
