208c6407e8cbe731fa388fa852b7f8b63cb3e20e
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-03-19 13:50:19 -0300

    Separa cookbook de isntalacao e config do solr + fix na instalacao para rodar como deploy

--- praga-solr-opsworks-cookbook/recipes/crond.rb
+++
@@ -1,6 +0,0 @@
-cron "solr-index-update" do
-  hour "*"
-  minute "*/15"
-  weekday "*"
-  command "curl 'http://localhost/solr/praga/dataimport?clean=false&commit=true&command=delta-import&wt=json'"
-end
--- praga-solr-opsworks-cookbook/recipes/default.rb
+++
@@ -1,4 +0,0 @@
-include_recipe 'opsworks_java::jvm_install'
-include_recipe 'praga-solr-opsworks-cookbook::solr'
-include_recipe 'praga-solr-opsworks-cookbook::config'
-include_recipe 'praga-solr-opsworks-cookbook::crond'
