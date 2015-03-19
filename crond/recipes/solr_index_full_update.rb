3a72d25965c4637f8e7824e6f918796929a3a3b9
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-03-19 13:04:33 -0300

    fix na receita do cron

--- crond/recipes/solr_index_full_update.rb
+++ crond/recipes/solr_index_full_update.rb
@@ -1,6 +1,14 @@
-cron_d "solr-index-update" do
-  hour "*"
-  minute "*/15"
-  weekday "*"
-  command "export RAILS_ENV=staging&& cd /srv/www/praga/current && bundle exec rake cache:clean solr:update_index['full-import']"
+cron "solr-index-update" do
+  action node.tags.include?('solr-index-update') ? :create : :delete
+  minute '*/15'
+  hour '*'
+  weekday '*'
+  user "deploy"
+  mailto "asilva@estantevirtual.com.br"
+  home "/srv/www/praga/current"
+  command %w{
+    export RAILS_ENV=staging && 
+    cd /srv/www/praga/current && 
+    bundle exec rake cache:clean solr:update_index['full-import']
+  }.join(' ')
 end
