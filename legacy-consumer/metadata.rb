632db71a7b507915fad6ee028e2352ba3c4157ef
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-03-20 12:46:05 -0300

    Legacy Consumer cookbook

--- legacy-consumer/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'praga-solr-opsworks-cookbook'
-maintainer       'Estante Virtual'
-maintainer_email 'ti@estantevirtual.com.br'
-license          'All rights reserved'
-description      'Installs/Configures praga-solr-opsworks-cookbook'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
--- legacy-consumer/recipes/default.rb
+++
@@ -1,1 +0,0 @@
-include_recipe 'legacy-consumers::install'
