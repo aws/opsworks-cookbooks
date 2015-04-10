b47f79e9d44b2d714fdfe0a7f45d55e44a675486
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-04-10 16:13:59 -0300

    add receita para criar usuario catalogodigital no ftp

---
+++ proftpd/recipes/catalogoeditorial.rb
@@ -0,0 +1,8 @@
+user 'catalogoeditorial' do
+  supports :manage_home => true
+  comment 'Catalogo Editorial User'
+  gid 'users'
+  home '/home/catalogoeditorial'
+  shell '/bin/false'
+  password '$1$IbK0KxRL$pnXrASwzWwPRvyNdEpWmm1'
+end
