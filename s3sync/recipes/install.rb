149c4d0c9ea9522d2ca055c95b2bd9d3e9b3955c
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-04-13 14:46:08 -0300

    cria cookbook para sync do ftp para s3 [base de referencia]

---
+++ s3sync/recipes/install.rb
@@ -0,0 +1,18 @@
+node[:deploy].each do |app_name, deploy|
+
+  package 's3cmd' do
+    action :install
+  end
+
+  directory "/etc/s3sync/" do
+    owner 'root'
+    mode 0755
+  end 
+  
+  cookbook_file "s3cfg" do
+    path "/etc/s3sync/s3cfg"
+    owner 'roor'
+  end
+
+
+end
