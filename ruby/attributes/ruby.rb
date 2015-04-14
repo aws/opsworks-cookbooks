a8107542947883cc3cd19c379debb593e7aa673b
Florian Eitel <eitel@amazon.com>
2015-04-14 16:29:18 +0200

    Bump patch level of ruby versions to latest.
    2.2.2
    2.1.6
    2.0.0-p645

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -21,7 +21,7 @@ case node["opsworks"]["ruby_version"]
 when "2.2"
   default[:ruby][:major_version] = '2'
   default[:ruby][:minor_version] = '2'
-  default[:ruby][:patch_version] = '0'
+  default[:ruby][:patch_version] = '2'
   default[:ruby][:pkgrelease]    = '1'
 
   default[:ruby][:full_version] = [node[:ruby][:major_version], node[:ruby][:minor_version]].join(".")
@@ -30,7 +30,7 @@ when "2.2"
 when "2.1"
   default[:ruby][:major_version] = '2'
   default[:ruby][:minor_version] = '1'
-  default[:ruby][:patch_version] = '5'
+  default[:ruby][:patch_version] = '6'
   default[:ruby][:pkgrelease]    = '1'
 
   default[:ruby][:full_version] = [node[:ruby][:major_version], node[:ruby][:minor_version]].join(".")
@@ -39,7 +39,7 @@ when "2.1"
 when "2.0.0"
   default[:ruby][:major_version] = '2'
   default[:ruby][:minor_version] = '0'
-  default[:ruby][:patch] = 'p598' # this attribute will disappear in favor of the sematic versioning schema
+  default[:ruby][:patch] = 'p645' # this attribute will disappear in favor of the sematic versioning schema
   default[:ruby][:patch_version] = node[:ruby][:patch]
   default[:ruby][:pkgrelease] = '1'
 
