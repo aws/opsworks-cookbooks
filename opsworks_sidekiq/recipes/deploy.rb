a04c9d4c14db5ab51447a1cbb2297153187f2816
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-04-21 12:37:34 +1000

    update sidekiq recipe

--- opsworks_sidekiq/recipes/deploy.rb
+++ opsworks_sidekiq/recipes/deploy.rb
@@ -3,7 +3,7 @@ node[:deploy].each do |application, deploy|
     sidekiq_config = deploy['sidekiq']
     release_path = ::File.join(deploy[:deploy_to], 'current')
     start_command = sidekiq_config['start_command'] || "bundle exec sidekiq -e production -C config/sidekiq.yml -r ./config/boot.rb 2>&1 >> log/sidekiq.log"
-    env = deploy['environment_variables'] || {}
+    env = OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]) || {}
 
     template "setup sidekiq.conf" do
       path "/etc/init/sidekiq-#{application}.conf"
