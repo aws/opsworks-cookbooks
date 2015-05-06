053625817a94220b3ab6d15cd2e129a0ab6dec48
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-05-06 15:11:19 -0300

    core para teste de busca

--- praga-solr-config/recipes/search.rb
+++ praga-solr-config/recipes/search.rb
@@ -1,27 +1,45 @@
 node[:deploy].each do |app_name, deploy|
-  template "/opt/solr/server/solr/conf/#{node[:search]["staging"][:core_name]}/conf/data-config.xml" do
-    source "cores/data-config.xml"
+  
+  remote_directory "/opt/solr/server/solr/#{node[:search]["staging"][:core_name]}" do
+    files_mode '0640'
+    mode '0770'
+    owner 'deploy'
+    source "cores/#{node[:search]["staging"][:core_name]}"
+  end
+
+  template "/opt/solr/server/solr/#{node[:search]["staging"][:core_name]}/conf/data-config.xml" do
+    source "cores/data-config.xml.erb"
     owner deploy[:user]
     group 'www-data'
     mode 0440
-    variables({ :driver => default[:search]["staging"][:drive],
-                :url => default[:search]["staging"][:url],
-                :password => default[:search]["staging"][:password],
-                :user => default[:search]["staging"][:user],
-                :query => default[:search]["staging"][:query],
-                :deltaImportQuery => default[:search]["staging"][:deltaImportQuery],
-                :deltaQuery => default[:search]["staging"][:deltaQuery]})
+    variables({ :driver => node[:search]["staging"][:drive],
+                :url => node[:search]["staging"][:url],
+                :password => node[:search]["staging"][:password],
+                :user => node[:search]["staging"][:user],
+                :query => node[:search]["staging"][:query],
+                :deltaImportQuery => node[:search]["staging"][:deltaImportQuery],
+                :deltaQuery => node[:search]["staging"][:deltaQuery]})
   end
 
-  template "/opt/solr/server/solr/conf/#{node[:search]["staging"][:core_name]}/core.properties" do
-    source "cores/data-config.xml"
+  template "/opt/solr/server/solr/#{node[:search]["staging"][:core_name]}/conf/solrconfig.xml" do
+    source "cores/solrconfig.xml.erb"
     owner deploy[:user]
     group 'www-data'
     mode 0440
     variables({ :name => node[:search]["staging"][:core_name]})
   end
 
+  template "/opt/solr/server/solr/#{node[:search]["staging"][:core_name]}/core.properties" do
+    source "cores/core.properties.erb"
+    owner deploy[:user]
+    group 'www-data'
+    mode 0440
+    variables({ :name => node[:search]["staging"][:core_name]})
+  end
 
+  execute '/opt/solr/bin/solr restart' do
+    user "deploy"
+  end
 
 end
 
