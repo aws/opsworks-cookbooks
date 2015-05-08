688c5d2256f68ad68fab8efe78dd3e05a591e11f
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-05-08 13:09:05 -0300

    add query e index

--- praga-solr-config/attributes/search.rb
+++ praga-solr-config/attributes/search.rb
@@ -4,46 +4,7 @@ default[:search][:staging][:deltaQuery] = ""
 default[:search][:staging][:url] = "jdbc:mysql://dev.virtualshelf.net/estantevirtual"
 default[:search][:staging][:user] = "ev-search"
 default[:search][:staging][:query] = <<SQL
-SELECT 
-c.rec_id,
-c.user_id,
-c.autor,
-c.titulo,
-c.idioma,
-LTRIM_JUNK(lower(c.editora), 1,1,1,1) editora,
-c.descr,
-c.capa,
-c.tipo,
-c.estado estado_livro,
-c.capa,
-c.peso,
-c.peso_est,
-c.preco,
-DATEDIFF(NOW(), c.data_reg) reg_dias_atras,
-UNIX_TIMESTAMP(c.data_reg) data_reg,
-u.estado,
-LTRIM_JUNK(lower(u.cidade), 1, 1, 1) cidade,
-c.estante,
-c.ano,
-u.nomeabrev,
-u.username,
-u.ativo usuario_ativo,
-c.ativo catalogo_ativo,
-bc.idioma titulo_idioma,
-cdna.preco_externo preco_externo,
-cdna.capa_dna capa_dna,
-br.dna_nvends_ativos
-FROM
-catalogo c
-INNER JOIN usuarios u ON (u.user_id = c.user_id)
-LEFT JOIN catalogo_dna_nova2 cdna ON ( c.rec_id = cdna.rec_id )
-LEFT JOIN booktree_20150407.booktree_radicais br on (cdna.dna_crc = br.radical_crc )
-LEFT JOIN booktree_20150407.booktree_crawl bc on (br.radical_id = bc.radical_id )
-WHERE
-c.ativo IN (1,11)
-AND u.ativo IN (1,11)
-AND LTRIM_JUNK(c.autor) <> ''
-AND LTRIM_JUNK(c.titulo) <> ''
+SELECT  c.rec_id, c.user_id, c.autor, c.titulo, c.idioma, LTRIM_JUNK(lower(c.editora), 1,1,1,1) editora, c.descr, c.capa, c.tipo, c.estado estado_livro, c.capa, c.peso, c.peso_est, c.preco, DATEDIFF(NOW(), c.data_reg) reg_dias_atras, UNIX_TIMESTAMP(c.data_reg) data_reg, u.estado, LTRIM_JUNK(lower(u.cidade), 1, 1, 1) cidade, c.estante, c.ano, u.nomeabrev, u.username, u.ativo usuario_ativo, c.ativo catalogo_ativo, bc.idioma titulo_idioma, cdna.preco_externo preco_externo, cdna.capa_dna capa_dna, br.dna_nvends_ativos FROM catalogo c INNER JOIN usuarios u ON (u.user_id = c.user_id) LEFT JOIN catalogo_dna_nova2 cdna ON ( c.rec_id = cdna.rec_id ) LEFT JOIN booktree_20150407.booktree_radicais br on (cdna.dna_crc = br.radical_crc ) LEFT JOIN booktree_20150407.booktree_crawl bc on (br.radical_id = bc.radical_id ) WHERE c.ativo IN (1,11) AND u.ativo IN (1,11) AND LTRIM_JUNK(c.autor) <> '' AND LTRIM_JUNK(c.titulo) <> ''
 SQL
 default[:search][:staging][:password] = "123mudar"
 default[:search][:staging][:drive] = "com.mysql.jdbc.Driver"
