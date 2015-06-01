3763551b102da9107e2ddf30ecb974bcd56b5c37
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-06-01 17:14:03 -0300

    criando teplate solr.in.sh alterando o calor do limite de memoria utilizado pelo solr

--- praga-solr-config/recipes/config.rb
+++ praga-solr-config/recipes/config.rb
@@ -14,6 +14,18 @@ node[:deploy].each do |app_name, deploy|
     source "config"
   end
 
+  template "/opt/solr/bin/solr.in.sh" do
+    source "solr.in.sh.erb"
+    owner deploy[:user]
+    group 'www-data'
+    mode 0440
+    variables({
+      :SOLR_MAX_MEM => node[:config][env][:SOLR_MAX_MEM],
+      :SOLR_MIN_MEM => node[:config][env][:SOLR_MIN_MEM]
+    })
+
+  end
+
   execute "if [ ! -d '/mnt/var/solr' ]; then mkdir /mnt/var/solr; fi;"
 
   execute '/opt/solr/bin/solr restart' do
