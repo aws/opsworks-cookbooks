f0d64fedbbd74143efd13fae7d946a6cbb4b0ac0
John Jarvis <johnjarv@amazon.com>
2015-06-05 17:10:06 +0200

    Skip libmemcached-devel for cookbook tests
    This doesn't seem necessary in rhel 7 (or in amzn linux for that matter)
    to install the memcache gem

--- memcached/recipes/prepare_tests.rb
+++ memcached/recipes/prepare_tests.rb
@@ -1,10 +1,12 @@
-# dependencie for the memcached gem
-package 'libmemcached development libraries' do
-  package_name value_for_platform_family(
-    "rhel" => "libmemcached-devel",
-    "debian" => "libmemcached-dev"
-  )
-end.run_action(:install)
+# dependencie for the memcached gem, not needed (and not available) for rhel7
+unless platform?("redhat") && Chef::VersionConstraint.new("~> 7.0").include?(node["platform_version"])
+  package 'libmemcached development libraries' do
+    package_name value_for_platform_family(
+      "rhel" => "libmemcached-devel",
+      "debian" => "libmemcached-dev"
+    )
+  end.run_action(:install)
+end
 
 package 'libsasl2-dev' do
   package_name value_for_platform_family(
