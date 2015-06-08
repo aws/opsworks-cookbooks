783a1c4c7d83a925981bf388eca05a7af8c5416a
Rafael <rrs.demolay@gmail.com>
2015-06-08 17:23:22 -0300

    create nca recipe

---
+++ nca/recipes/database.rb
@@ -0,0 +1,40 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
+  deploy = node[:deploy][application]
+
+  db_vars = Hash.new
+
+  deploy[:environment_variables].each do |k, v|
+    if k.is_a? String
+      if k.match(/^db_/)
+        db_vars[k[3, k.length]] = v
+      end
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+    source "database.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:database => deploy[:database], :environment => deploy[:rails_env], :dbs => db_vars)
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+end
