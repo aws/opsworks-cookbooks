351577103fbc2847552e8f6ae5b3856e9b0ea40c
Florian Eitel <eitel@amazon.com>
2015-06-17 15:06:47 +0200

    Enable selinux httpd_var_run_t target for unicorn socket.

--- deploy/definitions/opsworks_deploy_dir.rb
+++ deploy/definitions/opsworks_deploy_dir.rb
@@ -19,4 +19,15 @@ define :opsworks_deploy_dir do
     end
   end
 
+  bash "Enable selinux httpd_var_run_t target for unicorn socket" do
+    dir_path_socket = "#{params[:path]}/shared/sockets"
+    context = "httpd_var_run_t"
+
+    user "root"
+    code <<-EOH
+    semanage fcontext --add --type #{context} "#{dir_path_socket}(/.*)?" && restorecon -rv "#{dir_path_socket}"
+    EOH
+    not_if { OpsWorks::ShellOut.shellout("/usr/sbin/semanage fcontext -l") =~ /^#{Regexp.escape("#{dir_path_socket}(/.*)?")}\s.*\ssystem_u:object_r:#{context}:s0/ }
+    only_if { platform_family?("rhel") && ::File.exist?("/usr/sbin/getenforce") && OpsWorks::ShellOut.shellout("/usr/sbin/getenforce").strip == "Enforcing" }
+  end
 end
