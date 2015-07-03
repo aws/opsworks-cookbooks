532a661f19ecf3118c464aac74783484e2b0e026
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-03 17:41:44 -0300

    fix

--- praga-solr-config/attributes/config.rb
+++ praga-solr-config/attributes/config.rb
@@ -8,3 +8,15 @@ default[:config]["development"][:SOLR_MIN_MEM] = "512"
 
 default[:config]["production"][:SOLR_MAX_MEM] = "20480"
 default[:config]["production"][:SOLR_MIN_MEM] = "512"
+
+
+default[:default]["staging"][:SOLR_MAX_MEM] = "20480"
+default[:default]["staging"][:SOLR_MIN_MEM] = "512"
+
+
+default[:default]["development"][:SOLR_MAX_MEM] = "20480"
+default[:default]["development"][:SOLR_MIN_MEM] = "512"
+
+
+default[:default]["production"][:SOLR_MAX_MEM] = "20480"
+default[:default]["production"][:SOLR_MIN_MEM] = "512"
