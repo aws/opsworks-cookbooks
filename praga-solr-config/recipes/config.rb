532cb8530367de8ba1231d9969776c240df08555
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-03 18:06:32 -0300

    fix

--- praga-solr-config/recipes/config.rb
+++ praga-solr-config/recipes/config.rb
@@ -19,19 +19,19 @@ node[:deploy].each do |app_name, deploy|
   end
   Chef::Log.info(node[:config])
   Chef::Log.info(env)
-  
+
   template "/opt/solr/bin/solr.in.sh" do
     source "solr.in.sh.erb"
     owner deploy[:user]
     group 'www-data'
     mode 0440
-    variables({
-      :SOLR_MAX_MEM => node[:config][env][:SOLR_MAX_MEM],
-      :SOLR_MIN_MEM => node[:config][env][:SOLR_MIN_MEM]
-    })
+    #variables({
+    #  :SOLR_MAX_MEM => node[:config][env][:SOLR_MAX_MEM],
+    #  :SOLR_MIN_MEM => node[:config][env][:SOLR_MIN_MEM]
+    #})
 
   end
-
+  execute 'chmod 755 /opt/solr/bin/sols'
   execute '/opt/solr/bin/solr restart' do
     user "deploy"
   end
