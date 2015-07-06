7d868127f034eb3bf03e7eb475039396ce3145d7
Florian Eitel <eitel@amazon.com>
2015-07-06 14:44:11 +0200

    Revert "Don't fail if epel is not available for Red Hat Enterprise Linux."
    This reverts commit 6315240bf05421d22ae0b17df4356e4432523a47.

--- opsworks_initial_setup/recipes/setup_rhel_repos.rb
+++ opsworks_initial_setup/recipes/setup_rhel_repos.rb
@@ -1,4 +1,4 @@
-if node[:platform_family] == "rhel"
+if node[:platform] == 'amazon'
   # We only have to run 'yum-config-manager --quiet --enable epel' in order
   # to get EPEL working. Handle EPEL as optional as it may have
   # availability problems and we do not want it to effect built-in cookbooks
