545342d6111c2d62e447da294d84fa7acc7823ef
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-07 11:20:51 -0300

    corrige inicializador

--- solr/attributes/default.rb
+++ solr/attributes/default.rb
@@ -8,11 +8,11 @@
 default['solr']['version']  = '5.2.1'
 default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
 default['solr']['data_dir'] = "/opt/solr-#{default['solr']['version']}/server"
-default['solr']['dir']      = "/opt/solr-#{default['solr']['version']}"
+default['solr']['dir']      = "/opt/solr"
 default['solr']['port']     = '8984'
 default['solr']['pid_file'] = '/var/run/solr.pid'
-default['solr']['log_file'] = '/var/log/solr.log'
-default['solr']['user']     = 'root'
-default['solr']['group']    = 'root'
+default['solr']['log_file'] = "/opt/solr-#{default['solr']['version']}/server/logs/solr.log"
+default['solr']['user']     = 'deploy'
+default['solr']['group']    = 'deploy'
 default['solr']['install_java'] = true
 default['solr']['java_options'] = '-Xms512M -Xmx20480M'
