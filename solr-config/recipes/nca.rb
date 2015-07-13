24aac9ddc2264cab7845bed25d88f4d5c18055da
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-13 14:31:58 -0300

    fix solr-conf

--- solr-config/recipes/nca.rb
+++ solr-config/recipes/nca.rb
@@ -1,46 +1,17 @@
 node[:deploy].each do |app_name, deploy|
   env = deploy[:rails_env]
-  
-  remote_directory "/opt/solr/server/solr/#{node[:nca][env][:core_name]}" do
+
+  remote_directory "#{node[:nca][env][:path]}/#{node[:nca][env][:core_name]}" do
     files_mode '0640'
+    files_owner 'deploy'
     mode '0770'
     owner 'deploy'
+    recursive true
     source "cores/#{node[:nca][env][:core_name]}"
   end
-  # todo os templates
-  #template "/opt/solr/server/solr/#{node[:search][env][:core_name]}/conf/data-config.xml" do
-  #  source "cores/data-config.xml.erb"
-  #  owner deploy[:user]
-  #  group 'www-data'
-  #  mode 0440
-  #  variables({ :driver => node[:search][env][:drive],
-  #              :url => node[:search][env][:url],
-  #              :password => node[:search][env][:password],
-  #              :user => node[:search][env][:user],
-  #              :query => node[:search][env][:query],
-  #              :deltaImportQuery => node[:search][env][:deltaImportQuery],
-  #              :deltaQuery => node[:search][env][:deltaQuery]})
-  #end
 
-  #template "/opt/solr/server/solr/#{node[:search][env][:core_name]}/conf/solrconfig.xml" do
-  #  source "cores/solrconfig.xml.erb"
-  #  owner deploy[:user]
-  #  group 'www-data'
-  #  mode 0440
-  #  variables({ :name => node[:search][env][:core_name]})
-  #end
-
- # template "/opt/solr/server/solr/#{node[:search][env][:core_name]}/core.properties" do
- #   source "cores/core.properties.erb"
- #   owner deploy[:user]
- #   group 'www-data'
- #   mode 0440
- #   variables({ :name => node[:search][env][:core_name]})
- # end
-
-  execute '/opt/solr/bin/solr restart' do
-    user "deploy"
+  service 'solr' do
+    supports :restart => true, :status => true
+    action [:enable, :start]
   end
-
 end
-
