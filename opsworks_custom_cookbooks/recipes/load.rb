dc2b5357bd6057cc121c819d14ed6e66c693026c
Chanda, Saida <saichan@amazon.com>
2015-07-20 12:35:26 -0700

    improving the logic for handling the customcookbooks

--- opsworks_custom_cookbooks/recipes/load.rb
+++ opsworks_custom_cookbooks/recipes/load.rb
@@ -1,5 +1,13 @@
 if node[:opsworks_custom_cookbooks][:enabled]
   include_recipe "opsworks_custom_cookbooks::checkout"
+  ruby_block 'merge all cookbooks sources' do
+    block do
+       FileUtils.rm_rf Opsworks::InstanceAgent::Environment.merged_cookbooks_path
+       FileUtils.cp_r Opsworks::InstanceAgent::Environment.default_cookbooks_path, Opsworks::InstanceAgent::Environment.merged_cookbooks_path
+       FileUtils.cp_r "#{Opsworks::InstanceAgent::Environment.berkshelf_cookbooks_path}/.", Opsworks::InstanceAgent::Environment.merged_cookbooks_path if ::File.directory?(Opsworks::InstanceAgent::Environment.  berkshelf_cookbooks_path)
+       FileUtils.cp_r "#{Opsworks::InstanceAgent::Environment.site_cookbooks_path}/.", Opsworks::InstanceAgent::Environment.merged_cookbooks_path if ::File.directory?(Opsworks::InstanceAgent::Environment.  site_cookbooks_path)
+    end
+  end
 else
   directory node[:opsworks_custom_cookbooks][:destination] do
     action :delete
@@ -7,13 +15,11 @@ else
 
     only_if {node[:opsworks_custom_cookbooks][:destination]}
   end
-end
-
-ruby_block 'merge all cookbooks sources' do
-  block do
-     FileUtils.rm_rf Opsworks::InstanceAgent::Environment.merged_cookbooks_path
-     FileUtils.cp_r Opsworks::InstanceAgent::Environment.default_cookbooks_path, Opsworks::InstanceAgent::Environment.merged_cookbooks_path
-     FileUtils.cp_r "#{Opsworks::InstanceAgent::Environment.berkshelf_cookbooks_path}/.", Opsworks::InstanceAgent::Environment.merged_cookbooks_path if ::File.directory?(Opsworks::InstanceAgent::Environment.berkshelf_cookbooks_path)
-     FileUtils.cp_r "#{Opsworks::InstanceAgent::Environment.site_cookbooks_path}/.", Opsworks::InstanceAgent::Environment.merged_cookbooks_path if ::File.directory?(Opsworks::InstanceAgent::Environment.site_cookbooks_path)
+  template '/var/lib/aws/opsworks/client.stage2.rb' do
+    source 'client.stage.erb'
+    owner 'aws'
+    group 'aws'
+    mode '0644'
   end
 end
+
