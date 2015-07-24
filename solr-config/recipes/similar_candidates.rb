ee944c5a843af385c96fc7979237425522faafd1
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-24 14:10:47 -0300

    Criando core de consulta para migrador

---
+++ solr-config/recipes/similar_candidates.rb
@@ -0,0 +1,17 @@
+node[:deploy].each do |app_name, deploy|
+  env = deploy[:rails_env]
+
+  remote_directory "#{node[:praga][env][:path]}/#{node[:praga][env][:core_name]}" do
+    files_mode '0640'
+    files_owner 'deploy'
+    mode '0770'
+    owner 'deploy'
+    recursive true
+    source "cores/#{node[:praga][env][:core_name]}"
+  end
+
+  service 'solr' do
+    supports :restart => true, :status => true
+    action [:enable, :start]
+  end
+end
