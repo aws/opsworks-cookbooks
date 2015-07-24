3d77d298e220778f2cdee5ebd2c58e7ee69b2a36
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-24 14:25:29 -0300

    new core attributes

--- solr-config/attributes/similar_candidates.rb
+++ solr-config/attributes/similar_candidates.rb
@@ -1,2 +1,2 @@
-default[:praga][:staging][:path] = "/opt/solr-5.2.1/server/solr"
-default[:praga][:staging][:core_name] = "similar_candidates"
+default[:similar_candidates][:staging][:path] = "/opt/solr-5.2.1/server/solr"
+default[:similar_candidates][:staging][:core_name] = "similar_candidates"
--- solr-config/attributes/similar_moderation.rb
+++ solr-config/attributes/similar_moderation.rb
@@ -1,2 +1,2 @@
-default[:praga][:staging][:path] = "/opt/solr-5.2.1/server/solr"
-default[:praga][:staging][:core_name] = "similar_moderation"
+default[:similar_moderation][:staging][:path] = "/opt/solr-5.2.1/server/solr"
+default[:similar_moderation][:staging][:core_name] = "similar_moderation"
