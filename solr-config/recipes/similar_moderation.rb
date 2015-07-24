892374f1507e62a5f60a590d13ec9ae2db7b65ec
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-07-24 14:36:13 -0300

    new core attributes

--- solr-config/recipes/similar_moderation.rb
+++ solr-config/recipes/similar_moderation.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |app_name, deploy|
   env = deploy[:rails_env]
 
-  remote_directory "#{node[:similar_moderation][env][:path]}/#{node[:praga][env][:core_name]}" do
+  remote_directory "#{node[:similar_moderation][env][:path]}/#{node[:similar_moderation][env][:core_name]}" do
     files_mode '0640'
     files_owner 'deploy'
     mode '0770'
