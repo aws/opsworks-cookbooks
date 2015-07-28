0c9dcef6f084cb38e6c283669e73fc25065f557e
John Jarvis <johnjarv@amazon.com>
2015-07-28 14:12:22 +0200

    Bind mount check will not halt the chef run
    After a post commit review we decided to not halt
    the chef run if the bind mount check fails (though failures are not
    expected)

--- deploy/definitions/opsworks_deploy_dir.rb
+++ deploy/definitions/opsworks_deploy_dir.rb
@@ -6,9 +6,18 @@ define :opsworks_deploy_dir do
 
   if node[:opsworks_initial_setup].attribute?(:bind_mounts)
     node[:opsworks_initial_setup][:bind_mounts][:mounts].each do |dir, source|
-      execute "Check for bind mounts before creating the deploy directory" do
-        command "grep -q #{dir} /proc/mounts"
-        retries 10
+      bash "Check for bind mounts before creating the deploy directory" do
+        code <<-EOH
+          retries=3
+          until grep -q #{dir} /proc/mounts; do
+            if [[ $(( retries-- )) -lt 1 ]]; then
+              echo "Bind mount check failed for #{dir}"
+              exit 1
+            fi
+            sleep 2
+          done
+        EOH
+        ignore_failure true
       end
     end
   end
