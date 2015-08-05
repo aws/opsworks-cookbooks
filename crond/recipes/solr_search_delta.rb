30c3a05b80343405995780f4fa0290655968a6b6
ramonPimentel <ramonpimentel92@gmail.com>
2015-08-05 18:34:14 -0300

    ajuste do cron solr-search-delta

--- crond/recipes/solr_search_delta.rb
+++ crond/recipes/solr_search_delta.rb
@@ -7,6 +7,6 @@ cron "solr-search-delta" do
   mailto "ti@estantevirtual.com.br"
   home "/opt/solr-5.2.1/server"
   command %w{
-    wget -q "http://127.0.0.1:8983/solr/search/dataimport?&optimze=true&command=delta-import&wt=json"
+    wget -q "http://127.0.0.1:8983/solr/search/dataimport?&optimize=true&command=delta-import&wt=json"
   }.join(' ')
 end
