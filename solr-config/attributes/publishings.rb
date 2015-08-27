73d91323ffe749f647c9b68a8d5dcec00ba556f4
Andre Barros (Jason) <asilva@estantevirtual.com.br>
2015-08-27 13:25:15 -0300

    add novo core publishings

---
+++ solr-config/attributes/publishings.rb
@@ -0,0 +1,5 @@
+default[:publishings][:staging][:path] = "/opt/solr-5.2.1/server/solr"
+default[:publishings][:staging][:core_name] = "publishings"
+
+default[:publishings][:production][:path] = "/opt/solr-5.2.1/server/solr"
+default[:publishings][:production][:core_name] = "publishings"
