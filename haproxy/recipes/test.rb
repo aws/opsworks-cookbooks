d957efdf848dfef1e01f3ef5e2db920b31c70ff2
ao-dexter <dexter@athleticoffice.com>
2015-10-06 13:14:38 -0500

    Create test.rb

---
+++ haproxy/recipes/test.rb
@@ -0,0 +1,18 @@
+service "haproxy" do
+  supports :restart => true, :status => true, :reload => true
+  action :nothing # only define so that it can be restarted if the config changed
+end
+
+template "/etc/haproxy/haproxy.cfg" do
+  cookbook "haproxy"
+  source "haproxytest.cfg.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, "service[haproxy]"
+end
+
+execute "echo 'checking if HAProxy is not running - if so start it'" do
+  not_if "pgrep haproxy"
+  notifies :start, "service[haproxy]"
+end
