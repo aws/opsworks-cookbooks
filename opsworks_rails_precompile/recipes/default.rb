cac8256594bd5a7925592702df0e3044515ce3cf
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-10-07 16:45:17 +1100

    add rake task to reindex

--- opsworks_rails_precompile/recipes/default.rb
+++ opsworks_rails_precompile/recipes/default.rb
@@ -10,4 +10,11 @@ node[:deploy].each do |application, deploy|
     command 'bundle exec rake assets:precompile'
     environment 'RAILS_ENV' => rails_env
   end
+
+  execute "rake searchkick:reindex:all" do
+    cwd current_path
+    user 'deploy'
+    command "bundle exec rake searchkick:reindex:all"
+    environment 'RAILS_ENV' => rails_env
+  end
 end
