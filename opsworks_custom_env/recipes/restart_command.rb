2253d20a7809c477711adf26bbd53ab800504dc8
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-10-08 17:46:30 +1100

    fix restart command

--- opsworks_custom_env/recipes/restart_command.rb
+++ opsworks_custom_env/recipes/restart_command.rb
@@ -1,12 +1,8 @@
 node[:deploy].each do |application, deploy|
 
-  execute "restart Rails app #{application} for custom env" do
+  execute "restart app #{application} for custom env" do
     cwd deploy[:current_path]
-    if node[:opsworks][:rails_stack][:name].eql? "apache_passenger"
-      command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
-    elsif node[:opsworks][:rails_stack][:recipe].eql? "nginx_unicorn"
-      command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
-    end
+    command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart" q
     user deploy[:user]
 
     action :nothing
