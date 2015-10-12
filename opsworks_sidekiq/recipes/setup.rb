2f8b1cf900b326b60561d8408d260679268299d2
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-10-13 10:46:05 +1100

    revert environment variables

--- opsworks_sidekiq/recipes/setup.rb
+++ opsworks_sidekiq/recipes/setup.rb
@@ -67,8 +67,7 @@ node[:deploy].each do |application, deploy|
       variables(deploy: deploy,
                 application: application,
                 workers: workers,
-                syslog: node[:sidekiq][application][:syslog],
-                environment: OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]))
+                syslog: node[:sidekiq][application][:syslog])
       notifies :reload, resources(service: 'monit'), :immediately
     end
 
