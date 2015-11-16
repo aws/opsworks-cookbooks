8492896338cb3acadc796f472c6f9eefd212036b
Pedro Rosanes <prosanes@gmail.com>
2015-11-16 17:34:11 -0200

    Atualiza receita solr library

--- solr-config/attributes/library.rb
+++ solr-config/attributes/library.rb
@@ -1,5 +1,7 @@
-default[:library][:staging][:path] = "/opt/solr-5.2.1/server/solr"
+default[:library][:staging][:solr_version] = "5.2.1"
+default[:library][:staging][:path] = "/opt/solr-#{default[:library][:staging][:solr_version]}/server/"
 default[:library][:staging][:core_name] = "library"
 
-default[:library][:production][:path] = "/opt/solr-5.2.1/server/solr"
+default[:library][:production][:solr_version] = "5.2.1"
+default[:library][:production][:path] = "/opt/solr-#{default[:library][:staging][:solr_version]}/server/"
 default[:library][:production][:core_name] = "library"
