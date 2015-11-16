b336cc87e6029cd043a589b85f7a25fa93efd538
Pedro Rosanes <prosanes@gmail.com>
2015-11-16 17:59:52 -0200

    Atualiza receita solr library

--- solr-config/recipes/library.rb
+++ solr-config/recipes/library.rb
@@ -6,21 +6,21 @@ node[:deploy].each do |app_name, deploy|
 
   Chef::Log.info(env)
 
-  remote_directory "#{node[:library][env][:path]}/#{node[:library][env][:core_name]}" do
+  remote_directory "#{node['library'][env]['path']}/#{node['library'][env]['core_name']}" do
     files_mode '0640'
     files_owner 'deploy'
     mode '0770'
     owner 'deploy'
     recursive true
-    source "cores/#{node[:library][env][:core_name]}"
+    source "cores/#{node['library'][env]['core_name']}"
   end
 
-  template "#{node[:library][env][:path]}/#{node[:library][env][:core_name]}/core.properties" do
+  template "#{node['library'][env]['path']}/#{node['library'][env]['core_name']}/core.properties" do
     source "cores/core.properties.erb"
     owner deploy[:user]
     group 'www-data'
     mode 0440
-    variables({ :name => node[:library][env][:core_name]})
+    variables({ :name => node['library'][env]['core_name']})
   end
 
 
