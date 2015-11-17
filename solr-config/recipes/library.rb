79559a74fef6de7f5f7f01f760f907a71ed73820
Pedro Rosanes <prosanes@gmail.com>
2015-11-17 16:39:15 -0200

    Templating solr library config

--- solr-config/recipes/library.rb
+++ solr-config/recipes/library.rb
@@ -23,6 +23,18 @@ node[:deploy].each do |app_name, deploy|
     variables({ :name => node['library'][env]['core_name']})
   end
 
+  template "#{node['library'][env]['path']}/#{node['library'][env]['core_name']}/conf/data-config.xml" do
+    source "cores/#{node['library'][env]['core_name']}/data-config.xml.erb"
+    owner deploy[:user]
+    group 'www-data'
+    mode 0440
+    variables({ :db_url => node['library'][env]['database_url'],
+                :db_name => node['library'][env]['database_name'],
+                :db_url => node['library'][env]['database_url'],
+                :db_user => node['library'][env]['database_user'],
+                :db_password => node['library'][env]['database_password']}
+             )
+  end
 
   service 'solr' do
     supports :restart => true, :status => true
