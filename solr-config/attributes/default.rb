913bed8d6ec62e5e198a8c6520f69df3100dc777
Pedro Rosanes <prosanes@gmail.com>
2015-12-02 14:01:05 -0200

    Solr init.d default jvm memory

--- solr-config/attributes/default.rb
+++ solr-config/attributes/default.rb
@@ -1,6 +1,6 @@
 default['default']['staging']['solr_version'] = "5.2.1"
 default['default']['staging']['root'] = "/opt/solr-#{default['default']['staging']['solr_version']}/server/"
-default['default']['staging']['solr_java_mem'] = '-m 10g'
+default['default']['staging']['solr_java_mem'] = '-m 512m'
 
 
 default['default']['production']['solr_version'] = "5.2.1"
