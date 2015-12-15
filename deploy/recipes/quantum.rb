177fc996231dc8695a680846cea1385cb20e9d7f
Antonio Ganci <a.ganci@fit2you.it>
2015-12-15 12:10:43 +0100

    added secrets.yml for quantum

--- deploy/recipes/quantum.rb
+++ deploy/recipes/quantum.rb
@@ -2,8 +2,22 @@ include_recipe 'deploy'
 Chef::Log.level = :debug
 
 node[:deploy].each do |application, deploy|
+
   template "#{deploy[:deploy_to]}/shared/config/amazon.yml" do
     source 'quantum/amazon.yml.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :quantum_settings => node[:quantum_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source 'quantum/secrets.yml.erb'
     mode '0660'
     owner deploy[:user]
     group deploy[:group]
