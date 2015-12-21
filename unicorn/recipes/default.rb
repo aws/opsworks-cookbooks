e0ae2c7cf3d3ad54275aaf305fb7bdfc833a72af
Bruce Li <libruce@amazon.com>
2015-12-21 16:58:39 +0100

    Install fixed rack version for unicorn

--- unicorn/recipes/default.rb
+++ unicorn/recipes/default.rb
@@ -1,5 +1,6 @@
 ruby_block "ensure only our unicorn version is installed by deinstalling any other version" do
   block do
-    ensure_only_gem_version('unicorn', node[:unicorn][:version])
+    ensure_only_gem_version("rack", node[:unicorn][:rack_version])
+    ensure_only_gem_version("unicorn", node[:unicorn][:version])
   end
 end
