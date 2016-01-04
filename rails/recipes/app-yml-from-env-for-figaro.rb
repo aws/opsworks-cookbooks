afa64a71ecf14ce59b97df7fd6ed6042b30c3446
Pedro Rosanes <prosanes@gmail.com>
2016-01-04 17:30:45 -0200

    Recipe que cria arquivo usado pela gem Figaro, com variaveis de ambiente do opsworks

---
+++ rails/recipes/app-yml-from-env-for-figaro.rb
@@ -0,0 +1,22 @@
+rails_env = new_resource.environment["RAILS_ENV"]
+dotenv_contents = ''
+
+node[:deploy].each do |application, deploy|
+  # this should be used with figaro gem
+  dotenv_path = "#{release_path}/config/application.yml"
+  if deploy[:application_type] == 'rails'
+    # Dotenv file with sensitive data stored in the OpsWorks' layer.
+    deploy[:environment].to_hash.each do |key, value|
+      dotenv_contents << "#{key}: #{value}\n"
+    end
+
+    file(dotenv_path) do
+      mode('0660')
+      owner(deploy[:user])
+      group(deploy[:group])
+      content(dotenv_contents)
+      action(:create)
+    end
+
+  end
+end
