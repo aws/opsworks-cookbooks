4e555ec78ef1d86cd44e31d779d8f61e381e237d
rhenter <henter.r@gmail.com>
2016-01-08 16:37:59 -0200

    Receita para instalar o cliente do Cloudwatch

---
+++ awslogs/recipes/config.rb
@@ -0,0 +1,14 @@
+
+rails_env = new_resource.environment["RAILS_ENV"]
+
+Chef::Log.info("RAILS_ENV ${rails_env}")
+
+
+template "/tmp/cwlogs.cfg" do
+  cookbook "logs"
+  source "cwlogs.cfg.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(:srvlog => node[:srvlog] )
+end
