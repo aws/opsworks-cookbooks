8cbf39e396c20d1b5f901fbfe2136994aca2e21e
Antonio Ganci <a.ganci@fit2you.it>
2016-01-13 17:39:08 +0100

    renamed template

--- deploy/recipes/quantum.rb
+++ deploy/recipes/quantum.rb
@@ -4,12 +4,12 @@ Chef::Log.level = :debug
 node[:deploy].each do |application, deploy|
 
   template "#{deploy[:deploy_to]}/shared/config/aws.yml" do
-    source 'quantum/amazon.yml.erb'
+    source 'quantum/aws.yml.erb'
     mode '0660'
     owner deploy[:user]
     group deploy[:group]
     variables(
-        :quantum_settings => node[:quantum_settings]
+      :quantum_settings => node[:quantum_settings]
     )
     only_if do
       File.exists?("#{deploy[:deploy_to]}/shared/config")
