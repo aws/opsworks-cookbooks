e6caf07f4e4f385e88c8d82c5c3dee35877b0359
Florian Eitel <eitel@amazon.com>
2016-01-13 15:19:59 +0100

    Fix regex for s3 virtual host style addressing to match new eu-central addressing

--- scm_helper/libraries/s3.rb
+++ scm_helper/libraries/s3.rb
@@ -6,7 +6,7 @@ module OpsWorks
       def self.parse_uri(uri)
         uri = URI.parse(uri)
         uri_path_components = uri.path.split("/").reject{|p| p.empty?}
-        virtual_host_match = uri.host.match(/\A(.+)\.s3(?:-(?:ap|eu|sa|us)-.+-\d)?\.amazonaws\.com/i)
+        virtual_host_match = uri.host.match(/\A(.+)\.s3(?:[-.](?:ap|eu|sa|us)-(?:.+-)\d|-external-1)?\.amazonaws\.com/i)
         if virtual_host_match
           # virtual-hosted-style: http://bucket.s3.amazonaws.com or http://bucket.s3-aws-region.amazonaws.com
           bucket = virtual_host_match[1]
