6a2bf5aba5e38f520ca5c2d62c29611d08dc9a22
tortitommaso <torti.tommaso@gmail.com>
2016-01-18 16:57:21 +0100

    added mail configuration for quantum notification

--- deploy/recipes/quantum_notification.rb
+++ deploy/recipes/quantum_notification.rb
@@ -3,7 +3,7 @@ Chef::Log.level = :debug
 
 node[:deploy].each do |application, deploy|
 
-  template "#{deploy[:deploy_to]}/shared/config/shoryuken.yml.erb" do
+  template "#{deploy[:deploy_to]}/shared/config/shoryuken.yml" do
     source 'quantum_notification/shoryuken.yml.erb'
     mode '0660'
     owner deploy[:user]
@@ -15,4 +15,18 @@ node[:deploy].each do |application, deploy|
       File.exists?("#{deploy[:deploy_to]}/shared/config")
     end
   end
+
+  template "#{deploy[:deploy_to]}/shared/config/mail.yml" do
+    source 'quantum_notification/mail.yml.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :quantum_notification_settings => node[:quantum_notification_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
 end
