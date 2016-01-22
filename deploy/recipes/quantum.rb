d2757faf6863004eec947a43eecfb3bfae512a70
nakedmoon <tore.andrea@gmail.com>
2016-01-22 19:26:57 +0100

    Devise Configuration

--- deploy/recipes/quantum.rb
+++ deploy/recipes/quantum.rb
@@ -16,19 +16,6 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/devise.rb" do
-    source 'quantum/devise.rb.erb'
-    mode '0660'
-    owner deploy[:user]
-    group deploy[:group]
-    variables(
-        :quantum_settings => node[:quantum_settings]
-    )
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config")
-    end
-  end
-
   template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
     source 'quantum/secrets.yml.erb'
     mode '0660'
@@ -45,6 +32,20 @@ node[:deploy].each do |application, deploy|
   execute "create initializiers directory in shared/config" do
     command "mkdir -p #{deploy[:deploy_to]}/shared/config/initializers/"
   end
+
+  template "#{deploy[:deploy_to]}/shared/config/initializers/devise.rb" do
+    source 'quantum/devise.rb.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :quantum_settings => node[:quantum_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config/initializers")
+    end
+  end
+
 
   template "#{deploy[:deploy_to]}/shared/config/initializers/aws.rb" do
     source 'quantum/aws.rb.erb'
