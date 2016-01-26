5c69e196158a902e594b8e8bcb47ef3217f25b9c
Rafael Ribeiro <rrs.demolay@gmail.com>
2016-01-26 15:45:39 -0200

    Fixing Attributes

--- puma/attributes/default.rb
+++ puma/attributes/default.rb
@@ -2,5 +2,6 @@ default[:puma][:recipe] = "puma::default"
 default[:puma][:needs_reload] = true
 default[:puma][:service] = 'puma'
 default[:puma][:restart_command] = '../../shared/scripts/puma clean-restart'
+default[:sleep_before_restart] = 0
 
 include_attribute "deploy::deploy"
