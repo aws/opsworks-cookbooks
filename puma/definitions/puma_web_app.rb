04e78a1d9821fa0c9277b8001addbff0510bf019
Rafael Ribeiro <rrs.demolay@gmail.com>
2016-02-01 19:48:12 -0200

    Update puma_web_app.rb

--- puma/definitions/puma_web_app.rb
+++ puma/definitions/puma_web_app.rb
@@ -4,7 +4,7 @@ define :puma_web_app do
 
   nginx_web_app deploy[:application] do
     docroot deploy[:absolute_document_root]
-    server_name deploy[:domains].first unless deploy[:domains].empty?
+    server_name deploy[:domains].first unless deploy[:domains].nil?
     server_aliases deploy[:domains][1, deploy[:domains].size] unless deploy[:domains][1, deploy[:domains].size].empty?
     rails_env deploy[:rails_env]
     mounted_at deploy[:mounted_at]
