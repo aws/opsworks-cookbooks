dae1b1d75e1ae44f6a90d6ad6c37b678e4332ab1
Florian Eitel <eitel@amazon.com>
2016-02-04 17:28:35 +0100

    Add compatibility with chef-sugar for detecting compile_time method

--- s3_file/recipes/dependencies.rb
+++ s3_file/recipes/dependencies.rb
@@ -1,4 +1,5 @@
 chef_gem 'rest-client' do
   version node['s3_file']['rest-client']['version']
   action :install
+  compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
 end
