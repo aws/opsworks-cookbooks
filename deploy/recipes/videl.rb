de84bf8c9143344cb6497d1d5f00b5801c50147d
Antonio Ganci <a.ganci@fit2you.it>
2016-04-01 15:35:54 +0200

    updated videl configuration

--- deploy/recipes/videl.rb
+++ deploy/recipes/videl.rb
@@ -6,7 +6,7 @@ node[:deploy].each do |application, deploy|
   execute "updating crontab" do
     user deploy[:user]
     cwd "#{deploy[:deploy_to]}/current"
-    command "bundle exec whenever -w"
+    command "bundle exec whenever -w -s environment=#{deploy[:env]}"
     action :run
   end
 
@@ -16,7 +16,8 @@ node[:deploy].each do |application, deploy|
     owner deploy[:user]
     group deploy[:group]
     variables(
-        :videl_settings => node[:settings]
+      :videl_settings => node[:settings],
+      :videl_env => deploy[:env]
     )
     only_if do
       File.exists?("#{deploy[:deploy_to]}/shared/config")
