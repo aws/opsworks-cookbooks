45b1aa2e535f0ef3c6fe5d17b27dd22d2d6e417e
nakedmoon <tore.andrea@gmail.com>
2016-04-29 16:25:08 +0200

    Use Project Bundler

--- deploy/recipes/ruby.rb
+++ deploy/recipes/ruby.rb
@@ -13,15 +13,15 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  # execute "installing bundle" do
-  #   command "sudo gem install bundler"
-  #   action :run
-  # end
+  execute "installing bundle" do
+    command "sudo gem install bundler -v=1.10.6"
+    action :run
+  end
 
   execute "running bundle" do
     user deploy[:user]
     cwd "#{deploy[:deploy_to]}/current"
-    command "./bin/bundler install --path vendor"
+    command "bundle install --path vendor"
     action :run
   end
 end
