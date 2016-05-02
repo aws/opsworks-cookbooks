66a0d050177915d23e816a502f123fba6c126a95
Daniel Huesch <huesch@amazon.com>
2016-05-02 10:24:24 -0700

    Bump opsworks-ruby 2.2 to 2.2.5

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -21,7 +21,7 @@ case node["opsworks"]["ruby_version"]
 when "2.2"
   default[:ruby][:major_version] = '2'
   default[:ruby][:minor_version] = '2'
-  default[:ruby][:patch_version] = '4'
+  default[:ruby][:patch_version] = '5'
   default[:ruby][:pkgrelease]    = '1'
 
   default[:ruby][:full_version] = [node[:ruby][:major_version], node[:ruby][:minor_version]].join(".")
