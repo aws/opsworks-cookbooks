1e07a4c79d26a77f92e798366a81a713158d52c1
Rafael Manoel <rafael.manoel@gmail.com>
2016-05-11 11:36:10 -0300

    arruma o agendamento no crontab para a indexacao

--- solr-config/recipes/recommendations.rb
+++ solr-config/recipes/recommendations.rb
@@ -30,7 +30,6 @@ node[:deploy].each do |app_name, deploy|
     mode 0440
     variables({ :db_url => node['recommendations'][env]['database_url'],
                 :db_name => node['recommendations'][env]['database_name'],
-                :db_url => node['recommendations'][env]['database_url'],
                 :db_user => node['recommendations'][env]['database_user'],
                 :db_password => node['recommendations'][env]['database_password']}
              )
@@ -40,7 +39,7 @@ node[:deploy].each do |app_name, deploy|
     owner 'root'
     variables({ :cron_delta_import => node['recommendations'][env]['cron_delta_import'] })
     mode '0755'
-    source 'solr_recommendation_delta_import_cron.sh.erb'
+    source 'solr_recommendations_delta_import_cron.sh.erb'
   end
 
   service 'solr' do
