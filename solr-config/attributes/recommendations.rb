9086ea9f5fe904fe7679a11f3b0929a94d077165
Rafael Manoel <rafael.manoel@gmail.com>
2016-05-24 15:04:36 -0300

    altera o cron de indexacao do core recommendations

--- solr-config/attributes/recommendations.rb
+++ solr-config/attributes/recommendations.rb
@@ -2,7 +2,7 @@ default['recommendations']['staging']['solr_version'] = "5.3.2-SNAPSHOT"
 default['recommendations']['staging']['path'] = "/opt/solr-#{default['recommendations']['staging']['solr_version']}/server/solr/"
 default['recommendations']['staging']['core_name'] = "recommendations"
 default['recommendations']['staging']['database_url'] = "dev.virtualshelf.net"
-default['recommendations']['staging']['cron_delta_import'] = '*/5 * * * *'
+default['recommendations']['staging']['cron_delta_import'] = '15 */3 * * *'
 default['recommendations']['staging']['database_name'] = "estantevirtual"
 default['recommendations']['staging']['database_user'] = "rme-staging"
 default['recommendations']['staging']['database_password'] = "NVY96Qb6ub"
@@ -14,7 +14,7 @@ default['recommendations']['production']['database_url'] = "192.168.100.250"
 default['recommendations']['production']['database_name'] = "estantevirtual"
 default['recommendations']['production']['database_user'] = "rme_prod"
 default['recommendations']['production']['database_password'] = "9PkFHBGXfBXP"
-default['recommendations']['production']['cron_delta_import'] = '*/45 * * * *'
+default['recommendations']['production']['cron_delta_import'] = '15 * * * *'
 
 
 default[:logrotate][:rotate] = 2
