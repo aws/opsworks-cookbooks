d74b4ec7e201c61fcc1de4d766496b863fe860cd
Antonio Ganci <a.ganci@fit2you.it>
2016-06-13 12:24:58 +0200

    added hurricane recipe

---
+++ deploy/recipes/hurricane.rb
@@ -0,0 +1,66 @@
+include_recipe 'deploy'
+Chef::Log.level = :debug
+
+node[:deploy].each do |application, deploy|
+
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source 'hurricane/secrets.yml.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_settings => node[:hurricane_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+  execute "create initializiers directory in shared/config" do
+    command "mkdir -p #{deploy[:deploy_to]}/shared/config/initializers/"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/initializers/rollbar.rb" do
+    source 'hurricane/rollbar.rb.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_settings => node[:hurricane_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config/initializers")
+    end
+  end
+
+  execute "create initializiers directory in app/views/shared" do
+    command "mkdir -p #{deploy[:deploy_to]}/shared/app/views/shared/"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/app/views/shared/_rollbar.js.erb" do
+    source 'hurricane/_rollbar.js.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_settings => node[:hurricane_settings],
+        :hurricane_env => deploy[:env]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/app/views/shared")
+    end
+  end
+
+  current_path = deploy[:current_path]
+
+  execute "restart Server" do
+    cwd deploy[:current_path]
+    command "sleep #{deploy[:sleep_before_restart]} && #{node[:opsworks][:rails_stack][:restart_command]}"
+    action :run
+
+    only_if do
+      File.exists?(deploy[:current_path])
+    end
+  end
+
+end
