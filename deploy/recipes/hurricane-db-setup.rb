71ba58a7ee59498b7545ef361565b20fef4edcab
Tommaso Torti <torti.tommaso@gmail.com>
2016-06-29 10:58:09 +0200

    hurrican db setup added

---
+++ deploy/recipes/hurricane-db-setup.rb
@@ -0,0 +1,38 @@
+include_recipe 'deploy'
+Chef::Log.level = :debug
+
+node[:deploy].each do |application, deploy|
+
+  rails_env = deploy[:rails_env]
+  current_path = deploy[:current_path]
+
+  Chef::Log.info("Stopping Unicorn...")
+
+  execute "stop unicorn" do
+    command "#{deploy[:deploy_to]}/shared/scripts/unicorn stop"
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/scripts/unicorn")
+    end
+  end
+
+  Chef::Log.info("Rails Db Setup...")
+
+  execute 'bin/rake db:drop db:create db:migrate agreement:create_all' do
+    cwd current_path
+    user 'deploy'
+    command 'bin/rake db:drop db:create db:migrate agreement:create_all'
+    environment 'RAILS_ENV' => rails_env
+  end
+
+  Chef::Log.info("Start Unicorn...")
+
+  execute "start unicorn" do
+    command "#{deploy[:deploy_to]}/shared/scripts/unicorn start"
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/scripts/unicorn")
+    end
+  end
+
+
+
+end
