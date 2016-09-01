752767fc7d2668cba4a8b1e78d26f015a278602a
Tommaso Torti <torti.tommaso@gmail.com>
2016-09-01 17:49:57 +0200

    lumen pdf installation test

--- deploy/recipes/lumen-pdftk-setup.rb
+++ deploy/recipes/lumen-pdftk-setup.rb
@@ -10,4 +10,12 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
   end
 
+  yum_package 'libgcj'
+
+  execute "download & install pdftk" do
+    command "cd /tmp; wget https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-2.02-1.x86_64.rpm; rpm -ivh pdftk-2.02-1.x86_64.rpm"
+  end
+
+
+
 end
