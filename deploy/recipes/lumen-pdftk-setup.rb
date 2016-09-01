a8291f2b9c0540b2c9d927a6ecc07f402c30c780
Tommaso Torti <torti.tommaso@gmail.com>
2016-09-01 17:33:21 +0200

    lumen pdftk setup test

--- deploy/recipes/lumen-pdftk-setup.rb
+++ deploy/recipes/lumen-pdftk-setup.rb
@@ -3,7 +3,7 @@ Chef::Log.level = :debug
 
 node[:deploy].each do |application, deploy|
 
-  template "/etc/yum.repos.d/" do
+  template "/etc/yum.repos.d/public-yum-el5.repo" do
     source 'lumen/public-yum-el5.repo'
     mode '0660'
     owner deploy[:user]
