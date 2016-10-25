321014c4a61bda9844f078b4f0ef1596ea73c806
nakedmoon <tore.andrea@gmail.com>
2016-10-25 12:10:51 +0200

    Remove Huricane Multiple Server Restarts

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -10,4 +10,4 @@
 #
 #normal[:opsworks][:deploy_user][:shell] = '/bin/zsh'
 #normal[:opsworks][:deploy_user][:user] = 'deploy'
-normal[:opsworks][:rails_stack][:restart_command] = "../../shared/scripts/unicorn restart!"
+#normal[:opsworks][:rails_stack][:restart_command] = "../../shared/scripts/unicorn restart!"
