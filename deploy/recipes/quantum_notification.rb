3be2f5e49a8d202e0c249f048ae325403fb99b0d
nakedmoon <tore.andrea@gmail.com>
2016-11-08 17:03:37 +0100

    God Recipe

--- deploy/recipes/quantum_notification.rb
+++ deploy/recipes/quantum_notification.rb
@@ -45,16 +45,17 @@ node[:deploy].each do |application, deploy|
   end
 
   execute "god start notification service" do
-    Chef::Log.debug("executing => 'bundle exec god -c #{god_notification_file}' from #{deploy[:current_path]}")
     user deploy[:user]
+    group deploy[:group]
     cwd deploy[:current_path]
-    command "bundle exec god -c #{god_notification_file} -D > god.log"
+    command "bundle exec god -c #{god_notification_file}"
     action :run
-    #not_if 'bundle exec god status notification', :cwd => deploy[:current_path], :user => deploy[:user]
+    #not_if 'bundle exec god status notification', :cwd => deploy[:current_path], :user => deploy[:user], :group => deploy[:group]
   end
 
   execute "restart god notification service" do
     user deploy[:user]
+    group deploy[:group]
     cwd deploy[:current_path]
     command "bundle exec god restart notification"
     action :run
