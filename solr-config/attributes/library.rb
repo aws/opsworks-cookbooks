a2e160efea397dd708f980cf7555bfbb4576a84d
Pedro Rosanes <prosanes@gmail.com>
2016-11-10 11:35:13 -0200

    Adiciona delta import de delete

--- solr-config/attributes/library.rb
+++ solr-config/attributes/library.rb
@@ -7,6 +7,8 @@ default['library']['staging']['database_user'] = "librarysg"
 default['library']['staging']['database_password'] = "library-sg"
 default['library']['staging']['cron_delta_import'] = '*/5 * * * *'
 default['library']['staging']['cron_full_import'] = '0 2 * * *'
+default['library']['staging']['cron_delta_import'] = '0,5,10,15,20,25,30,35,40,45,50,55 * * * *'
+default['library']['staging']['cron_delta_delete'] = '2,7,12,17,22,27,32,37,42,47,52,57 * * * *'
 
 default['library']['production']['solr_version'] = "5.3.2-SNAPSHOT"
 default['library']['production']['path'] = "/opt/solr-#{default['library']['staging']['solr_version']}/server/"
@@ -16,7 +18,8 @@ default['library']['production']['database_name'] = "library_production"
 default['library']['production']['database_user'] = "libraryprod"
 default['library']['production']['database_password'] = "password"
 default['library']['production']['cron_delta_import'] = '*/45 * * * *'
-default['library']['production']['cron_full_import'] = '0 2 * * *'
+default['library']['production']['cron_delta_import'] = '0,5,10,15,20,25,30,35,40,45,50,55 * * * *'
+default['library']['production']['cron_delta_delete'] = '2,7,12,17,22,27,32,37,42,47,52,57 * * * *'
 
 
 default[:logrotate][:rotate] = 2
