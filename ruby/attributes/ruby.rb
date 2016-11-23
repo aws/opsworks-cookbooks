8d99e95ca26b9d91d8feba98b8ccf3c6b4181d0b
Daniel Huesch <huesch@amazon.com>
2016-11-23 17:53:48 +0000

    Bump opsworks-ruby 2.2 to 2.2.6
    cr https://code.amazon.com/reviews/CR-45571

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -37,7 +37,7 @@ when "2.2"
   if _platform == "amazon" && _platform_version < "2015.03"
     default[:ruby][:patch_version] = '3'
   else
-    default[:ruby][:patch_version] = '5'
+    default[:ruby][:patch_version] = "6"
   end
   default[:ruby][:pkgrelease]    = '1'
 
