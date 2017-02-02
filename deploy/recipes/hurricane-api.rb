6116d9f657ade4f8e4cc0bc5146de84948806840
nakedmoon <tore.andrea@gmail.com>
2017-02-02 21:49:46 +0100

    hurricane-api recipies

---
+++ deploy/recipes/hurricane-api.rb
@@ -0,0 +1,93 @@
+include_recipe 'deploy'
+Chef::Log.level = :debug
+
+node[:deploy].each do |application, deploy|
+  
+  execute "updating crontab" do
+    user deploy[:user]
+    cwd "#{deploy[:deploy_to]}/current"
+    command "bundle exec whenever -w -s environment=#{deploy[:env]}"
+    action :run
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    source 'hurricane-api/secrets.yml.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_api_settings => node[:hurricane_api_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/slimpay.yml" do
+    source 'hurricane-api/slimpay.yml.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_api_settings => node[:hurricane_api_settings],
+        :env => deploy[:env]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+
+  execute "create environments directory in shared/config" do
+    command "mkdir -p #{deploy[:deploy_to]}/shared/config/environments/"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/environments/#{deploy[:env]}.rb" do
+    source "hurricane-api/environment_config.rb.erb"
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_api_settings => node[:hurricane_api_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config/environments")
+    end
+  end
+
+
+
+  execute "create initializiers directory in shared/config" do
+    command "mkdir -p #{deploy[:deploy_to]}/shared/config/initializers/"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/initializers/rollbar.rb" do
+    source 'hurricane-api/rollbar.rb.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :hurricane_api_settings => node[:hurricane_api_settings]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config/initializers")
+    end
+  end
+
+  execute "create initializiers directory in app/views/shared" do
+    command "mkdir -p #{deploy[:deploy_to]}/shared/app/views/shared/"
+  end
+
+
+  execute "restart Server" do
+    Chef::Log.debug('Restarting Rails Server From Hurricane Script')
+    cwd deploy[:current_path]
+    command "sleep #{deploy[:sleep_before_restart]} && #{node[:opsworks][:rails_stack][:restart_command]}"
+    action :run
+
+    only_if do
+      File.exists?(deploy[:current_path])
+    end
+  end
+
+end
