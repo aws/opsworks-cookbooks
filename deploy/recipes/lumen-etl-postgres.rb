b4e3c228b8a6ca5fc27172a99e1efc2666baae3f
nakedmoon <tore.andrea@gmail.com>
2017-02-16 11:15:46 +0100

    Lumen ETL Postgres 9.6 Repo

---
+++ deploy/recipes/lumen-etl-postgres.rb
@@ -0,0 +1,15 @@
+include_recipe 'deploy'
+Chef::Log.level = :debug
+
+node[:deploy].each do |application, deploy|
+
+  template "/etc/yum.repos.d/pgdg-96-ami201503.repo" do
+    source 'lumen-etl/pgdg-96-ami201503.repo'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+  end
+
+  yum_package 'postgresql96'
+
+end
