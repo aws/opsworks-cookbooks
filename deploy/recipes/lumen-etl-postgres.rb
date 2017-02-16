4bf874ee015d3c92561d4731a098f1a72ab4f8b4
nakedmoon <tore.andrea@gmail.com>
2017-02-16 12:47:08 +0100

    Didaple Postgres9.6 GPG Check

--- deploy/recipes/lumen-etl-postgres.rb
+++ deploy/recipes/lumen-etl-postgres.rb
@@ -6,13 +6,20 @@ node[:deploy].each do |application, deploy|
   unless File.exists?("/etc/yum.repos.d/pgdg-96-ami201503.repo")
     template "/etc/yum.repos.d/pgdg-96-ami201503.repo" do
       source 'lumen_etl/pgdg-96-ami201503.repo'
-      mode '0660'
+      mode '0644'
       owner deploy[:user]
       group deploy[:group]
     end
 
     yum_package 'postgresql96'
     yum_package 'postgresql96-devel'
+
+
+    link '/usr/bin/pg_config' do
+      to '/usr/pgsql-9.6/bin/pg_config'
+    end
+
+
   end
 
 
