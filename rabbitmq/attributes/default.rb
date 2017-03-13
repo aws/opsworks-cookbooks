7ce1b9607a510d34544b7edc02f0396068c72fe7
Bruno Mentges de Carvalho <bmentges@estantevirtual.com.br>
2017-03-13 16:53:54 -0300

    Configure DLX into ha-all policy, removes ha-two, which is not used, to avoid confusion

--- rabbitmq/attributes/default.rb
+++ rabbitmq/attributes/default.rb
@@ -111,12 +111,8 @@ default['rabbitmq']['heartbeat'] = 80
 
 # Example HA policies
 default['rabbitmq']['policies']['ha-all']['pattern'] = '^(?!amq\\.).*'
-default['rabbitmq']['policies']['ha-all']['params'] = { 'ha-mode' => 'all' }
+default['rabbitmq']['policies']['ha-all']['params'] = { 'ha-mode' => 'all', 'dead-letter-exchange' => 'dead_letter_exchange'  }
 default['rabbitmq']['policies']['ha-all']['priority'] = 0
-
-default['rabbitmq']['policies']['ha-two']['pattern'] = "^two\."
-default['rabbitmq']['policies']['ha-two']['params'] = { 'ha-mode' => 'exactly', 'ha-params' => 2 }
-default['rabbitmq']['policies']['ha-two']['priority'] = 1
 
 default['rabbitmq']['disabled_policies'] = []
 
