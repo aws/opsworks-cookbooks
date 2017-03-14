7ba856fe6e4057b0a6f228fbedf0916aa231bfe4
Leandro Lima <leandrolima@Leandros-MacBook-Air.local>
2017-03-14 08:36:07 -0300

    correcao hashes

--- logrotate_truncate/recipes/default.rb
+++ logrotate_truncate/recipes/default.rb
@@ -4,8 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-app_list = node.try(:[],"opsworks").try(:[],"applications")
-path_list = node.try(:[],"logtruncate").try(:[],"paths")
+#app_list = node.try(:[],"opsworks").try(:[],"applications")
+#path_list = node.try(:[],"logtruncate").try(:[],"paths")
+
+app_list = node["opsworks"]["applications"]
+path_list = node["logtruncate"]["paths"]
 
 template '/etc/truncate_logfiles.conf' do
 	source 'truncate_logfiles.conf.erb'
