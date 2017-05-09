2c8857a124958647f348640c4a0d9f65d36a7c07
nakedmoon <tore.andrea@gmail.com>
2017-05-09 12:44:20 +0200

    Update AWS Cookbooks

--- opsworks_cloudwatchlogs/recipes/uninstall.rb
+++ opsworks_cloudwatchlogs/recipes/uninstall.rb
@@ -2,22 +2,13 @@ service "awslogs" do
   action :stop
 end
 
-directory node["cloudwatchlogs"]["home_dir"] do
-  action :delete
-  recursive true
-end
-
-if platform?("amazon")
-  package "awslogs" do
-    action :remove
-  end
-else
-  directory "/opt/aws/cloudwatch" do
+[node["cloudwatchlogs"]["home_dir"], node["cloudwatchlogs"]["state_file_dir"]].each do |dir|
+  directory dir do
     action :delete
     recursive true
   end
+end
 
-  file "/etc/init.d/awslogs" do
-    action :delete
-  end
+file "/etc/init.d/awslogs" do
+  action :delete
 end
