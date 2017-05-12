de4650336597070dccfcec46c0fbeecc20ef8bfa
nakedmoon <tore.andrea@gmail.com>
2017-05-12 19:37:59 +0200

    Remote Counter Configuration

--- deploy/recipes/remote_counter.rb
+++ deploy/recipes/remote_counter.rb
@@ -16,4 +16,22 @@ node[:deploy].each do |application, deploy|
       File.exists?("#{deploy[:deploy_to]}/shared/config")
     end
   end
+
+  template "#{deploy[:deploy_to]}/shared/config/unicorn.rb" do
+    source 'remote_counter/unicorn.rb.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :remote_counter_settings => node[:remote_counter_settings],
+        :working_dir => File.join(deploy[:deploy_to], 'current'),
+        :log_dir => File.join(deploy[:deploy_to], 'shared', 'log'),
+        :pid_dir => File.join(deploy[:deploy_to], 'shared', 'pids')
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+
 end
