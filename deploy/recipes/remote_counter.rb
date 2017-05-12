3d10f85cf45048697bcf86b9575503103abda1e7
nakedmoon <tore.andrea@gmail.com>
2017-05-12 21:17:01 +0200

    Remote Counter Configuration

--- deploy/recipes/remote_counter.rb
+++ deploy/recipes/remote_counter.rb
@@ -25,13 +25,38 @@ node[:deploy].each do |application, deploy|
     variables(
         :remote_counter_settings => node[:remote_counter_settings],
         :working_dir => File.join(deploy[:deploy_to], 'current'),
-        :log_dir => File.join(deploy[:deploy_to], 'shared', 'log'),
-        :pid_dir => File.join(deploy[:deploy_to], 'shared', 'pids')
+        :stderr_path => File.join(deploy[:deploy_to], 'shared', 'log','unicorn.stderr.log'),
+        :stdout_path => File.join(deploy[:deploy_to], 'shared', 'log','unicorn.stdout.log'),
+        :pid_path => File.join(deploy[:deploy_to], 'shared', 'pids','unicorn.pid')
     )
     only_if do
       File.exists?("#{deploy[:deploy_to]}/shared/config")
     end
   end
 
+  execute 'stop_unicorn' do
+    Chef::Log.info("Stop Unicorn....")
+    cwd current_path
+    user 'deploy'
+    command "cat #{File.join(deploy[:deploy_to], 'shared', 'pids','unicorn.pid')} | xargs kill -QUIT"
+    environment 'REMOTE_COUNTER_ENV' => deploy[:env]
+    ignore_failure true
+    notifies :run, 'execute[start_unicorn]', :immediately
+  end
+
+
+  execute 'start_unicorn' do
+    Chef::Log.info("Start Unicorn....")
+    cwd current_path
+    user 'deploy'
+    command "bundle exec unicorn -c #{File.join(deploy[:deploy_to], 'shared', 'config', 'unicorn.rb')} -D"
+    environment 'REMOTE_COUNTER_ENV' => deploy[:env]
+    ignore_failure false
+    action :nothing
+  end
+
+
+
 
 end
+
