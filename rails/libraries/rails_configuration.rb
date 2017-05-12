1fbeeee6132b45989787bd410c15cb2303f3232a
Ramon Ferreira Pimentel <ramonpimentel92@gmail.com>
2017-05-12 10:51:07 -0300

    Fix rails-configuration

--- rails/libraries/rails_configuration.rb
+++ rails/libraries/rails_configuration.rb
@@ -43,7 +43,8 @@ module OpsWorks
         else
   				Chef::Log.info("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && /usr/local/bin/bundle install --path #{app_config[:home]}/.bundler/#{app_name} --without=#{app_config[:ignore_bundler_groups].join(' ')}'")
           Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && /usr/local/bin/bundle install --path #{app_config[:home]}/.bundler/#{app_name} --without=#{app_config[:ignore_bundler_groups].join(' ')}' 2>&1"))
-      end
+        end
+       end
     end
   end
 end
