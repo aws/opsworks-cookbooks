3fc06a8d811a31bdd06b20df6f4db2d9e46b8d7d
nakedmoon <tore.andrea@gmail.com>
2017-05-16 19:47:11 +0200

    Update Unicorn Restart Script

--- deploy/recipes/remote_counter.rb
+++ deploy/recipes/remote_counter.rb
@@ -5,10 +5,47 @@ node[:deploy].each do |application, deploy|
 
   remote_counter_env = deploy[:env]
   current_path = deploy[:current_path]
-  unicorn_pid_path = File.join(deploy[:deploy_to], 'shared', 'pids','unicorn.pid')
-  unicorn_config_path = File.join(deploy[:deploy_to], 'shared', 'config', 'unicorn.rb')
+  unicorn_pid_dir = File.join(deploy[:deploy_to], 'shared', 'pids')
+  unicorn_pid_path = File.join(unicorn_pid_dir,'unicorn.pid')
+  unicorn_config_dir = File.join(deploy[:deploy_to], 'shared', 'config')
+  unicorn_sockets_dir = File.join(deploy[:deploy_to], 'shared', 'sockets')
+  unicorn_log_dir = File.join(deploy[:deploy_to], 'shared', 'log')
+  unicorn_config_path = File.join(unicorn_config_dir, 'shared', 'config', 'unicorn.rb')
 
-  template "#{deploy[:deploy_to]}/shared/config/settings.yml" do
+
+  directory unicorn_pid_dir do
+    mode '0770'
+    owner deploy[:user]
+    group deploy[:group]
+    action :create
+    recursive true
+  end
+
+  directory unicorn_config_dir do
+    mode '0770'
+    owner deploy[:user]
+    group deploy[:group]
+    action :create
+    recursive true
+  end
+
+  directory unicorn_sockets_dir do
+    mode '0770'
+    owner deploy[:user]
+    group deploy[:group]
+    action :create
+    recursive true
+  end
+
+  directory unicorn_log_dir do
+    mode '0770'
+    owner deploy[:user]
+    group deploy[:group]
+    action :create
+    recursive true
+  end
+
+  template File.join(unicorn_config_dir, 'settings.yml') do
     source 'remote_counter/settings.yml.erb'
     mode '0660'
     owner deploy[:user]
@@ -18,37 +55,59 @@ node[:deploy].each do |application, deploy|
         :remote_counter_env => remote_counter_env
     )
     only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config")
+      File.exists?(unicorn_config_dir)
     end
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/unicorn.rb" do
-    source 'remote_counter/unicorn.rb.erb'
+  template File.join(unicorn_config_dir, 'unicorn.conf') do
+    source 'remote_counter/unicorn.conf.erb'
     mode '0660'
     owner deploy[:user]
     group deploy[:group]
     variables(
         :remote_counter_settings => node[:remote_counter_settings],
         :working_dir => current_path,
-        :stderr_path => File.join(deploy[:deploy_to], 'shared', 'log','unicorn.stderr.log'),
-        :stdout_path => File.join(deploy[:deploy_to], 'shared', 'log','unicorn.stdout.log'),
+        :stderr_path => File.join(unicorn_log_dir, 'unicorn.stderr.log'),
+        :stdout_path => File.join(unicorn_log_dir, 'unicorn.stdout.log'),
+        :sockets_path => File.join(unicorn_sockets_dir,'unicorn.sock'),
+        :gemfile_path => File.join(current_path, 'Gemfile'),
         :pid_path => unicorn_pid_path
     )
     only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config")
+      File.exists?(unicorn_config_dir)
     end
   end
 
-  execute 'stop_unicorn' do
-    Chef::Log.info("Stop Unicorn ...")
+  execute 'restart_unicorn' do
+    Chef::Log.info("Restart Unicorn ...")
     cwd current_path
     user 'deploy'
-    command "cat #{unicorn_pid_path} | xargs kill -9"
+    command "kill -USR2 `cat #{unicorn_pid_path}`"
     environment 'REMOTE_COUNTER_ENV' => remote_counter_env
-    ignore_failure true
+    ignore_failure false
     only_if do
-      File.exists?(unicorn_pid_path)
+      unicorn_running?
     end
+  end
+
+  file unicorn_pid_path do
+    Chef::Log.info("Delete stale PID file....")
+    action :delete
+    only_if do
+      unicorn_stale_pid_file?
+    end
+  end
+
+  def unicorn_running?
+    File.exists?(unicorn_pid_path) && (pid = File.read(unicorn_pid_path).chomp) && process_is_running?(pid)
+  end
+
+  def unicorn_stale_pid_file?
+    File.exists?(unicorn_pid_path) && (pid = File.read(unicorn_pid_path).chomp) && !process_is_running?(pid)
+  end
+
+  def process_is_running?(pid)
+    system("ps aux | grep #{pid} | grep -v grep > /dev/null")
   end
 
 
@@ -56,9 +115,12 @@ node[:deploy].each do |application, deploy|
     Chef::Log.info("Start Unicorn ....")
     cwd current_path
     user 'deploy'
-    command "bundle exec unicorn -c #{unicorn_config_path} -D"
+    command "bundle exec unicorn --env #{remote_counter_env} -c #{unicorn_config_path} -D"
     environment 'REMOTE_COUNTER_ENV' => remote_counter_env
     ignore_failure false
+    not_if do
+      unicorn_running?
+    end
   end
 
 
