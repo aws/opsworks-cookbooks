dba75aa1b17922c3f980f12054786740427a52a3
nakedmoon <tore.andrea@gmail.com>
2017-05-17 19:43:04 +0200

    Ruby OpsWorks Stack

--- dependencies/attributes/customize.rb
+++ dependencies/attributes/customize.rb
@@ -9,6 +9,8 @@
 # The following shows how to override the gem binary:
 #
 
-default["opsworks"]["ruby_web_stack"] = {} if node["opsworks"].has_key?("instance") &&
-                                              node["opsworks"]["instance"].has_key?("layers") &&
-                                              node["opsworks"]["instance"]["layers"].include?("ruby_web")
+default["opsworks"]["ruby_web_stack"] = {}  if node["opsworks"].has_key?("instance") &&
+                                               node["opsworks"]["instance"].has_key?("layers") &&
+                                               node["opsworks"]["instance"]["layers"].include?("ruby_web")
+
+
