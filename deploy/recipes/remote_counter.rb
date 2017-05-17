e77641752cb7b327ce5e13001cde48e624ac7007
nakedmoon <tore.andrea@gmail.com>
2017-05-17 23:35:00 +0200

    Ruby OpsWorks Stack

--- deploy/recipes/remote_counter.rb
+++ deploy/recipes/remote_counter.rb
@@ -117,7 +117,7 @@ node[:deploy].each do |application, deploy|
   execute "restart Server" do
     Chef::Log.debug('Restarting Sinatra Server From Remote Counter Script')
     cwd deploy[:current_path]
-    command "sleep #{deploy[:sleep_before_restart]} && #{node[:opsworks][:ruby_unicorn_nginx][:restart_command]}"
+    command "sleep #{deploy[:sleep_before_restart]} && #{node[:opsworks][:ruby_unicorn_nginx_stack][:restart_command]}"
     action :run
 
     only_if do
