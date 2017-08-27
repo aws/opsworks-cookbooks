f9499247fb4a7c50a1fec940a1b4aca545bcb4ad
nakedmoon <tore.andrea@gmail.com>
2017-08-27 17:49:36 +0200

    Templating Lumen ETL Schedule

--- deploy/recipes/lumen_etl.rb
+++ deploy/recipes/lumen_etl.rb
@@ -2,12 +2,6 @@ include_recipe 'deploy'
 Chef::Log.level = :debug
 
 node[:deploy].each do |application, deploy|
-  execute "updating crontab" do
-    user deploy[:user]
-    cwd "#{deploy[:deploy_to]}/current"
-    command "bundle exec whenever -w -s environment=#{deploy[:env]}"
-    action :run
-  end
 
   template "#{deploy[:deploy_to]}/shared/config/settings.yml" do
     source 'lumen_etl/settings.yml.erb'
@@ -22,4 +16,26 @@ node[:deploy].each do |application, deploy|
       File.exists?("#{deploy[:deploy_to]}/shared/config")
     end
   end
+
+  template "#{deploy[:deploy_to]}/shared/config/schedule.rb" do
+    source 'lumen_etl/schedule.rb.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :etl_settings => node[:etl_settings],
+        :etl_env => deploy[:env]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+  execute "updating crontab" do
+    user deploy[:user]
+    cwd "#{deploy[:deploy_to]}/current"
+    command "bundle exec whenever -w -s environment=#{deploy[:env]}"
+    action :run
+  end
+
 end
