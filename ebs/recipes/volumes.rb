5cae49449ad8dec9ed5e0d4151fc5f0f10e876d1
Rashidy <elrashim@amazon.com>
2018-04-10 23:20:17 +0200

    add apt repository required for ubuntu 14 to install nvme-cli
    cr https://code.amazon.com/reviews/CR-1767441

--- ebs/recipes/volumes.rb
+++ ebs/recipes/volumes.rb
@@ -1,3 +1,8 @@
+execute 'nvme-cli-ubuntu-14-ppa' do
+  command 'add-apt-repository ppa:sbates && apt-get update'
+  only_if { EbsVolumeHelpers.nvme_based? && !EbsVolumeHelpers.has_ebs_tooling? && platform?("ubuntu") && node[:platform_version] == "14.04" && node[:ebs][:devices].size != 0 }
+end
+
 package "nvme-cli" do
   only_if { EbsVolumeHelpers.nvme_based? && !EbsVolumeHelpers.has_ebs_tooling? && node[:ebs][:devices].size != 0 }
   retries 2
