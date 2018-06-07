9ce9c0b13e020179de54a6fad0ca05088098c08a
Rafael Ribeiro <rrs.demolay@gmail.com>
2018-06-07 11:59:44 -0300

    include bundle spaceific platform options on application deploy

--- rails/libraries/rails_configuration.rb
+++ rails/libraries/rails_configuration.rb
@@ -36,7 +36,10 @@ module OpsWorks
 
     def self.bundle(app_name, app_config, app_root_path)
       if File.exists?("#{app_root_path}/Gemfile")
-        Chef::Log.info("Gemfile detected. Running bundle install.")
+          Chef::Log.info("Gemfile detected. Running bundle install.")
+          if app_config[:environment]['BUNDLE_SPECIFIC_PLATFORM']
+            Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && /usr/local/bin/bundle config specific_platform true' 2>&1"))
+          end
         if app_config[:environment]['BUNDLE_GITHUB__COM']
   				Chef::Log.info("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && BUNDLE_GITHUB__COM=#{app_config[:environment]['BUNDLE_GITHUB__COM']} /usr/local/bin/bundle install --path #{app_config[:home]}/.bundler/#{app_name} --without=#{app_config[:ignore_bundler_groups].join(' ')}'")
           Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su - #{app_config[:user]} -c 'cd #{app_root_path} && BUNDLE_GITHUB__COM=#{app_config[:environment]['BUNDLE_GITHUB__COM']} /usr/local/bin/bundle install --path #{app_config[:home]}/.bundler/#{app_name} --without=#{app_config[:ignore_bundler_groups].join(' ')}' 2>&1"))
