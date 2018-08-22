2feb1cf0f31887f9cd9500adf6052dbd3ca9a711
Mike Mueller <mikmuel@amazon.com>
2018-08-22 10:43:49 +0200

    Force bundler installation for newer versions of rubygems
    See https://tt.amazon.com/0160441482 and
    https://github.com/rubygems/rubygems/issues/2058
    
    cr https://code.amazon.com/reviews/CR-2977496

--- opsworks_bundler/recipes/default.rb
+++ opsworks_bundler/recipes/default.rb
@@ -5,6 +5,9 @@ if node[:opsworks_bundler][:manage_package]
     package_name "bundler"
     action :install
     version node[:opsworks_bundler][:version]
+    if Gem::Version.new(node[:opsworks_rubygems][:version]) > Gem::Version.new("2.6.14")
+      options "--force"
+    end
   end
 
   # alternative/fallback install of bundler for more robustness
