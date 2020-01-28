c4df404a1b0fd5835fe6544eb974798a17e6dc60
Raihan Mahmud Arman <raihan2006i@gmail.com>
2020-01-28 15:50:19 +0100

    s3 config file template and setup for hurricane-api

--- deploy/recipes/hurricane-api.rb
+++ deploy/recipes/hurricane-api.rb
@@ -58,6 +58,20 @@ node[:deploy].each do |application, deploy|
     end
   end
 
+  template "#{deploy[:deploy_to]}/shared/config/s3.yml" do
+    source 'hurricane-api/s3.yml.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :hurricane_api_settings => node[:hurricane_api_settings],
+      :env => rails_env
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
   template "#{deploy[:deploy_to]}/shared/config/email_checker.yml" do
     source 'hurricane-api/email_checker.yml.erb'
     mode '0660'
