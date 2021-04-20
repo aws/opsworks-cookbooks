32134574a7d752e844858a92b191d4fe497f38bf
Danilo Filippelli <d.filippelli@fit2you.it>
2021-04-20 10:09:42 +0200

    Add missing file

---
+++ deploy/recipes/preprod-environment.rb
@@ -0,0 +1,14 @@
+node[:deploy].each do |application, deploy|
+
+    rails_env = deploy[:rails_env]
+    current_path = deploy[:current_path]
+
+    # hotfix, create preprod symlink on yet created template, so
+    link "#{current_path}/config/environments/#{rails_env}.rb" do
+        to "#{deploy[:deploy_to]}/shared/config/environments/#{rails_env}.rb"
+        mode '0660'
+        owner deploy[:user]
+        group deploy[:group]
+    end
+
+end
