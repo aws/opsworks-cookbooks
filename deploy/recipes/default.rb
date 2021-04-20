7d5c9cc9c1284d34ec07c74e1c90f3c66fdb5f1a
Danilo Filippelli <d.filippelli@fit2you.it>
2021-04-20 09:34:37 +0200

    move declarations

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -2,12 +2,13 @@ include_recipe 'dependencies'
 
 node[:deploy].each do |application, deploy|
 
+  rails_env = deploy[:rails_env]
+  current_path = deploy[:current_path]
+
   opsworks_deploy_user do
     deploy_data deploy
   end
 
-  rails_env = deploy[:rails_env]
-  current_path = deploy[:current_path]
   # hotfix, create preprod symlink on yet created template, so
   link "#{current_path}/config/environments/#{rails_env}.rb" do
     to "#{deploy[:deploy_to]}/shared/config/environments/#{rails_env}.rb"
